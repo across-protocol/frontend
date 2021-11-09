657debdf631fa7cbfee16bb7d577f2d94f11baea
Jason Kiraly <jaykiraly@gmail.com>
2021-11-09 13:56:10 -0800

    sitemap generated. (#46)
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/sitemap-routes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route, Switch } from "react-router";
+
+export default (
+  <Switch>
+    <Route path="/" />
+    <Route path="/about" />
+    <Route path="/pool" />
+  </Switch>
+);
