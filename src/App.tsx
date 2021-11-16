5a9963c23922e24e3c770c7553e8dd0c1ae0a2c5
Jason Kiraly <jaykiraly@gmail.com>
2021-11-16 10:12:44 -0800

    bug: add check for environment variables. (#53)
    * add check for environment variables.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * clarity on message.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
+import assert from "assert";
+
+// App will have errors without these environment variables -- throw an error.
+assert(process.env.REACT_APP_PUBLIC_ONBOARD_API_KEY, "REACT_APP_PUBLIC_ONBOARD_API_KEY must be defined.")
+assert(process.env.REACT_APP_PUBLIC_INFURA_ID, "REACT_APP_PUBLIC_INFURA_ID must be defined.")
 
 function App() {
   return (
