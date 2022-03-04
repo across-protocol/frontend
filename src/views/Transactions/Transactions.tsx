41241211c59cc1bbad8b75038ebdb33413ed10f7
Jason Kiraly <jasonkiraly@Jasons-MacBook-Pro.local>
2022-03-04 10:41:10 -0800

    connect button works
    Signed-off-by: Jason Kiraly <jasonkiraly@Jasons-MacBook-Pro.local>

--- src/views/Transactions/Transactions.tsx
+++ src/views/Transactions/Transactions.tsx
@@ -1,9 +1,25 @@
 import { Wrapper, Title, ConnectButton } from "./Transactions.styles";
+import useTransactionsView from "./useTransactionsView";
+
 const Transactions = () => {
+  const {
+    provider,
+    isConnected,
+    initOnboard
+  } = useTransactionsView()
+
+  const handleClick = () => {
+    if (!provider) {
+      initOnboard();
+    }
+  };
+
   return (
     <Wrapper>
       <Title>Transactions</Title>
-      <ConnectButton>Connect wallet</ConnectButton>
+      {(!isConnected) && (
+          <ConnectButton onClick={handleClick}>Connect Wallet</ConnectButton>
+        )}
     </Wrapper>
   );
 };
--- src/views/Transactions/useTransactionsView.ts
+++ src/views/Transactions/useTransactionsView.ts
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import { useConnection } from "state/hooks";
+import { onboard } from "utils";
 
 export default function useTransactionsView() {
   const [transactions] = useState([]);
+  const { provider, chainId, isConnected } = useConnection();
+  const { init } = onboard;
 
   return {
     transactions,
+    provider,
+    chainId,
+    isConnected,
+    initOnboard: init
   };
 }
