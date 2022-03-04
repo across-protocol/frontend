327bc3e4fbb30406676a62d9fab4dbca69adc641
Tulun <jaykiraly@gmail.com>
2022-03-04 12:38:44 -0800

    basic render of table.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/views/Transactions/Transactions.tsx
+++ src/views/Transactions/Transactions.tsx
@@ -1,12 +1,83 @@
 import { Wrapper, Title, ConnectButton } from "./Transactions.styles";
 import useTransactionsView from "./useTransactionsView";
+import Table, { ICell, IRow } from "components/Table/Table";
+
+const hc: ICell[] = [
+  {
+    size: "sm",
+    value: "Deposit time",
+  },
+  {
+    size: "sm",
+    value: "Status",
+  },
+  {
+    size: "sm",
+    value: "Filled %",
+  },
+  {
+    size: "sm",
+    value: "Source",
+  },
+  {
+    size: "sm",
+    value: "Destination",
+  },
+  {
+    size: "sm",
+    value: "Asset",
+  },
+  {
+    size: "sm",
+    value: "Amount",
+  },
+  {
+    size: "sm",
+    value: "Deposit tx",
+  },
+];
+
+const rows: IRow[] = [
+  {
+    cells: [
+      {
+        size: "sm",
+        value: "Feb 5th",
+      },
+      {
+        size: "sm",
+        value: "Filled",
+      },
+      {
+        size: "sm",
+        value: "100%",
+      },
+      {
+        size: "sm",
+        value: "Arbitrum",
+      },
+      {
+        size: "sm",
+        value: "Ethereum",
+      },
+      {
+        size: "sm",
+        value: "UMA",
+      },
+      {
+        size: "sm",
+        value: "5000",
+      },
+      {
+        size: "sm",
+        value: "0x123...",
+      },
+    ],
+  },
+];
 
 const Transactions = () => {
-  const {
-    provider,
-    isConnected,
-    initOnboard
-  } = useTransactionsView()
+  const { provider, isConnected, initOnboard } = useTransactionsView();
 
   const handleClick = () => {
     if (!provider) {
@@ -17,11 +88,10 @@ const Transactions = () => {
   return (
     <Wrapper>
       <Title>Transactions</Title>
-      {(!isConnected) && (
-          <ConnectButton onClick={handleClick}>Connect Wallet</ConnectButton>
-        )}
-
-        
+      {!isConnected && (
+        <ConnectButton onClick={handleClick}>Connect Wallet</ConnectButton>
+      )}
+      <Table headerCells={hc} rows={rows} />
     </Wrapper>
   );
 };
