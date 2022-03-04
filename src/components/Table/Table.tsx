fb4fe2ea6967f25704fa02496abad4b49e0258bf
Tulun <jaykiraly@gmail.com>
2022-03-04 12:29:41 -0800

    remove var
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from "react";
+import  { FC, ReactElement } from "react";
 import { TableWrapper, Row, Body, HeadRow, Cell, Title } from "./Table.styles";
 
 export interface TableProps {
