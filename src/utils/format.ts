eb914d03c924ad5519d6d5d3e2fc39a1a17f7bb5
Tulun <jaykiraly@gmail.com>
2022-03-08 14:46:35 -0800

    Fixed empty return docs; return string early in shorteString
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -13,7 +13,7 @@ export function isValidString(s: string | null | undefined | ""): s is string {
  * @param address valid web3 address
  * @param delimiter string to put between your split string, eg: "...", "---"
  * @param numChars number of characters to keep on each part of the string
- * @returns
+ * @returns string. Formatted version of address param.
  */
 
 export function shortenAddress(
@@ -32,13 +32,15 @@ export function shortenAddress(
  * @param str string to shorten
  * @param delimiter string to put between your split string, eg: "...", "---"
  * @param numChars number of characters to keep on each part of the string
- * @returns
+ * @returns string. Formatted version of str param.
  */
 export function shortenString(
   str: string,
   delimiter: string,
   numChars: number
-) {
+): string {
+  // Cannot shorten this string, force early return.
+  if (str.length < 2 * numChars) return str;
   return `${str.substring(0, numChars)}${delimiter}${str.substring(
     str.length - numChars,
     str.length
