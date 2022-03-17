095a79f6fa49831c93c08b0213cf11c1797cbd30
Tulun <jaykiraly@gmail.com>
2022-03-17 10:19:30 -0700

    remove defaults
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -1,6 +1,7 @@
 import { onboard } from "utils";
 import { FC } from "react";
 import { useConnection, useETHBalance } from "state/hooks";
+
 import {
   DEFAULT_FROM_CHAIN_ID,
   CHAINS,
@@ -45,11 +46,15 @@ const Wallet: FC<Props> = ({ setOpenSidebar }) => {
   return (
     <Wrapper onClick={() => setOpenSidebar(true)}>
       <Info>
-        <div>
-          {formatEther(balance ?? "0")}{" "}
-          {CHAINS[chainId ?? 1].nativeCurrency.symbol}
-        </div>
-        <div>{CHAINS[chainId ?? 1].name}</div>
+        {chainId && (
+          <>
+            <div>
+              {formatEther(balance ?? "0")}{" "}
+              {CHAINS[chainId].nativeCurrency.symbol}
+            </div>
+            <div>{CHAINS[chainId].name}</div>
+          </>
+        )}
       </Info>
       <Account>{shortenAddress(account ?? "", "...", 4)}</Account>
     </Wrapper>
