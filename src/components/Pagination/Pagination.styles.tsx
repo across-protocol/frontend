545b40de3dc66a280fbd1dc1d902218317d93141
Tulun <jaykiraly@gmail.com>
2022-03-30 16:16:03 -0700

    basic Pagination stubs.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/components/Pagination/Pagination.styles.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  text-align: center;
+`;
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,7 @@
+import { Wrapper } from "./Pagination.styles";
+
+const Pagination = () => {
+  <Wrapper></Wrapper>;
+};
+
+export default Pagination;
