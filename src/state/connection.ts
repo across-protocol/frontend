3971f6ecb12b43b0b12468a277ff1bd2eb6af8e1
David <david@umaproject.org>
2022-05-16 15:50:17 -0400

    fix: clear account if wallet locks
    Signed-off-by: David <david@umaproject.org>

--- src/state/connection.ts
+++ src/state/connection.ts
@@ -37,7 +37,7 @@ const connectionSlice = createSlice({
   reducers: {
     update: (state, action: PayloadAction<Update>) => {
       const { account, chainId, provider, signer } = action.payload;
-      state.account = account ? getAddress(account) : state.account;
+      state.account = account ? getAddress(account) : undefined;
       state.provider = provider ?? state.provider;
       // theres a potential problem with this: if onboard says a signer is undefined, we default them back
       // to the previous signer. This means we get out of sync with onboard and could have serious consequences.
