cf59da19387245a01ca79f8f541324414a9ad10a
Jason Kiraly <jaykiraly@gmail.com>
2022-06-21 12:52:09 -0700

    feat: Referral UI stub (#140)
    * image
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * stub
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * header subheader
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * referral url
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * twitter
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * center text
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * footer wip
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * proper comp
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * fixed footer
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * adjusted stubbed url
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * both texts
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,69 @@
+import {
+  Wrapper,
+  LinkFooter,
+  Link,
+  LogoFooter,
+  LinkText,
+  AccentLink,
+  PoweredByUMA,
+} from "./Footer.styles";
+import { ReactComponent as DiscordLogo } from "assets/disc-logo.svg";
+import { ReactComponent as TwitterLogo } from "assets/icon-twitter.svg";
+
+const NAV_LINKS = [
+  {
+    key: "faq",
+    name: "FAQ",
+    url: "",
+    icon: "",
+  },
+  {
+    key: "docs",
+    name: "Docs",
+    url: "https://docs.across.to/bridge/",
+    icon: "",
+  },
+  {
+    key: "discord",
+    name: "",
+    url: "https://discord.gg/across",
+    icon: DiscordLogo,
+  },
+  {
+    key: "twitter",
+    name: "",
+    url: "https://twitter.com/AcrossProtocol",
+    icon: TwitterLogo,
+  },
+];
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <LinkFooter>
+        {NAV_LINKS.map((link) => (
+          <Link
+            key={link.key}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {link.icon ? <link.icon /> : null}
+            <LinkText>{link.name}</LinkText>
+          </Link>
+        ))}
+      </LinkFooter>
+      <LogoFooter>
+        <AccentLink
+          href="https://umaproject.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <PoweredByUMA />
+        </AccentLink>
+      </LogoFooter>
+    </Wrapper>
+  );
+};
+
+export default Footer;
--- src/views/Rewards/comp/index.ts
+++ src/views/Rewards/comp/index.ts
@@ -1,3 +1,4 @@
 export { default as RewardBreakdown } from "./RewardBreakdown";
 export { default as RewardHero } from "./RewardHero";
 export { default as RewardTable } from "./RewardTable";
+export { default as RewardReferral } from "./RewardReferral";
