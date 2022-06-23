b1a35d1bee0ea470d4f22921068c4f34186d1e01
Matt Rice <matthewcrice32@gmail.com>
2022-06-23 17:53:03 -0400

    feat: add return values to give time estimates for relayer liquidity (#143)

--- api/utils.js
+++ api/utils.js
@@ -1,4 +1,8 @@
-const { HubPool__factory } = require("@across-protocol/contracts-v2");
+const {
+  HubPool__factory,
+  ERC20__factory,
+  SpokePool__factory,
+} = require("@across-protocol/contracts-v2");
 const sdk = require("@across-protocol/sdk-v2");
 const ethers = require("ethers");
 
@@ -38,7 +42,7 @@ const getTokenDetails = async (provider, l1Token, l2Token, chainId) => {
     hubPool,
     chainId: event.args.destinationChainId.toNumber(),
     l1Token: event.args.l1Token,
-    l2Token: event.args.l2Token,
+    l2Token: event.args.destinationToken,
   };
 };
 
@@ -86,6 +90,93 @@ const getRelayerFeeDetails = (l1Token, amount, destinationChainId) => {
   return relayFeeCalculator.relayerFeeDetails(amount, tokenSymbol);
 };
 
+const providerCache = {};
+
+const getProvider = (_chainId) => {
+  const chainId = _chainId.toString();
+  if (!providerCache[chainId]) {
+    switch (chainId.toString()) {
+      case "1":
+        providerCache[chainId] = infuraProvider("mainnet");
+        break;
+      case "10":
+        providerCache[chainId] = infuraProvider("optimism-mainnet");
+        break;
+      case "137":
+        providerCache[chainId] = infuraProvider("polygon-mainnet");
+        break;
+      case "288":
+        providerCache[chainId] = bobaProvider();
+        break;
+      case "42161":
+        providerCache[chainId] = infuraProvider("arbitrum-mainnet");
+        break;
+      default:
+        throw new Error(`Invalid chainId provided: ${chainId}`);
+    }
+  }
+  return providerCache[chainId];
+};
+
+const getSpokePool = (_chainId) => {
+  const chainId = _chainId.toString();
+  const provider = getProvider(chainId);
+  switch (chainId.toString()) {
+    case "1":
+      return SpokePool__factory.connect(
+        "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381",
+        provider
+      );
+    case "10":
+      return SpokePool__factory.connect(
+        "0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9",
+        provider
+      );
+    case "137":
+      return SpokePool__factory.connect(
+        "0x69B5c72837769eF1e7C164Abc6515DcFf217F920",
+        provider
+      );
+    case "288":
+      return SpokePool__factory.connect(
+        "0xBbc6009fEfFc27ce705322832Cb2068F8C1e0A58",
+        provider
+      );
+    case "42161":
+      return SpokePool__factory.connect(
+        "0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C",
+        provider
+      );
+    default:
+      throw new Error(`Invalid chainId provided: ${chainId}`);
+  }
+};
+
+const isRouteEnabled = (fromChainId, toChainId, fromToken) => {
+  const spokePool = getSpokePool(fromChainId.toString());
+  return spokePool.enabledDepositRoutes(fromToken, toChainId.toString());
+};
+
+const getBalance = (chainId, token, account) => {
+  return ERC20__factory.connect(token, getProvider(chainId)).balanceOf(account);
+};
+
+const maxBN = (...arr) => {
+  return [...arr].sort((a, b) => {
+    if (b.gt(a)) return 1;
+    if (a.gt(b)) return -1;
+    return 0;
+  })[0];
+};
+
+const minBN = (...arr) => {
+  return [...arr].sort((a, b) => {
+    if (a.gt(b)) return 1;
+    if (b.gt(a)) return -1;
+    return 0;
+  })[0];
+};
+
 module.exports = {
   getTokenDetails,
   isString,
@@ -95,4 +186,9 @@ module.exports = {
   bobaProvider,
   getRelayerFeeDetails,
   maxRelayFeePct,
+  getProvider,
+  getBalance,
+  maxBN,
+  minBN,
+  isRouteEnabled,
 };
