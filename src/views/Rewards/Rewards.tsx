4ab37bf22c0849841c43e49d0019a10bae1a67c2
Tulun <jaykiraly@gmail.com>
2022-06-24 14:43:02 -0700

    remove unneeded renders for now
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/views/Rewards/Rewards.tsx
+++ src/views/Rewards/Rewards.tsx
@@ -1,36 +1,11 @@
 import { Wrapper } from "./Rewards.styles";
-import {
-  RewardBreakdown,
-  RewardHero,
-  RewardTable,
-  RewardReferral,
-} from "./comp";
-import createAllPoolsTableJSX, {
-  headers as allPoolsHeaders,
-} from "./comp/RewardTable/createMyPoolsTableJSX";
-
-import createMyPoolsTableJSX, {
-  headers as myPoolsHeaders,
-} from "./comp/RewardTable/createAllPoolsTableJSX";
+import { RewardBreakdown, RewardReferral } from "./comp";
 import Footer from "components/Footer";
 
 const Rewards = () => {
-  const rowsAllPools = createAllPoolsTableJSX();
-  const rowsMyPools = createMyPoolsTableJSX();
   return (
     <Wrapper>
-      <RewardHero />
       <RewardBreakdown />
-      <RewardTable
-        title="My pools"
-        headers={allPoolsHeaders}
-        rows={rowsAllPools}
-      />
-      <RewardTable
-        title="All pools"
-        headers={myPoolsHeaders}
-        rows={rowsMyPools}
-      />
       <RewardReferral />
       <Footer />
     </Wrapper>
