a2d4ba8d8b783d69d55ab58cb70d238feae077c4
Tulun <jaykiraly@gmail.com>
2022-06-24 14:45:45 -0700

    stubbed my referrals table
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/views/Rewards/Rewards.tsx
+++ src/views/Rewards/Rewards.tsx
@@ -1,12 +1,17 @@
 import { Wrapper } from "./Rewards.styles";
-import { RewardBreakdown, RewardReferral } from "./comp";
+import { RewardBreakdown, RewardReferral, RewardTable } from "./comp";
 import Footer from "components/Footer";
+import createMyReferralsTableJSX, {
+  headers,
+} from "./comp/RewardTable/createMyReferralsTableJSX";
 
 const Rewards = () => {
+  const rows = createMyReferralsTableJSX();
   return (
     <Wrapper>
       <RewardBreakdown />
       <RewardReferral />
+      <RewardTable title="My referrals" rows={rows} headers={headers} />
       <Footer />
     </Wrapper>
   );
