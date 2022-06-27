6b4e300813f1e6b4f0200f1316377b618e204ecf
Tulun <jaykiraly@gmail.com>
2022-06-27 10:33:59 -0700

    stepper stubs
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/components/Stepper/Stepper.styles.tsx
@@ -0,0 +1,71 @@
+import styled from "@emotion/styled";
+
+export const StyledStepperHeaderItem = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+
+  .step-counter {
+    position: relative;
+    z-index: 5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 35px;
+    height: 35px;
+    padding: 10px;
+    border-radius: 50%;
+    background: #ccc;
+    margin-bottom: 6px;
+  }
+
+  &::after {
+    position: absolute;
+    content: "";
+    border-bottom: 2px solid #ccc;
+    width: 100%;
+    top: 17px;
+    left: 50%;
+    z-index: 2;
+  }
+
+  &.completed {
+    .step-counter {
+      background-color: #4bb543;
+    }
+    &::before {
+      position: absolute;
+      content: "";
+      border-bottom: 2px solid #4bb543;
+      width: 100%;
+      top: 17px;
+      left: -50%;
+      z-index: 3;
+    }
+  }
+
+  &:first-child {
+    &::before {
+      content: none;
+    }
+  }
+
+  &:last-child {
+    &::after {
+      content: none;
+    }
+  }
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+export const StyledStepperHeader = styled.div`
+  margin-top: auto;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
