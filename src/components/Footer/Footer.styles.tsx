6edc37eeb5585aec5b2f7071c4c56bf80c48ad5f
Tulun <jaykiraly@gmail.com>
2022-06-27 15:47:52 -0700

    adjusted footer
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/Footer/Footer.styles.tsx
+++ src/components/Footer/Footer.styles.tsx
@@ -6,6 +6,7 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 0 1.5rem;
+  margin-top: 10vh;
 `;
 const BaseFooter = styled.footer`
   position: sticky;
