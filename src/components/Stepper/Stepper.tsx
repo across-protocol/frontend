1966eaf4c2c082aff0a6cb482a2f4b70384c9bb3
Tulun <jaykiraly@gmail.com>
2022-06-27 11:00:58 -0700

    basic stepper
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,5 +1,19 @@
-const Stepper = () => {
-  return <div />;
+import { StyledStepper } from "./Stepper.styles";
+
+import useStepper from "./useStepper";
+interface Props {
+  numSteps: number;
+}
+const Stepper: React.FC<Props> = ({ numSteps }) => {
+  const { stepItems } = useStepper(numSteps);
+
+  return (
+    <StyledStepper>
+      {stepItems.map((el) => {
+        return el;
+      })}
+    </StyledStepper>
+  );
 };
 
 export default Stepper;
