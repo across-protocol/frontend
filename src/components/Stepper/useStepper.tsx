90b1d20ca02e657b664d5c4e90fe69967108fbce
Tulun <jaykiraly@gmail.com>
2022-06-29 11:17:03 -0700

    added comment; change i = 0 to i = 1 to simplify code.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/Stepper/useStepper.tsx
+++ src/components/Stepper/useStepper.tsx
@@ -3,35 +3,39 @@ import { StyledStepperItem } from "./Stepper.styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
+/* 
+  useStepper is largely for handling the classes need to properly show the lines between number elements.
+  It's rather complicated because of how the stepper needs to lay itself out on the page, and the space between these elements.
+*/
 export default function useStepper(currentStep: number, numSteps: number) {
   const [stepItems, setStepItems] = useState<JSX.Element[]>([]);
   const createStepsItems = useCallback(() => {
     const items: JSX.Element[] = [];
     const midPoint = Math.round(numSteps / 2);
-    for (let i = 0; i < numSteps; i++) {
-      const completed = i + 1 < currentStep;
+    for (let i = 1; i < numSteps; i++) {
+      const completed = i < currentStep;
       let className = completed
         ? "completed"
-        : i + 1 === currentStep
+        : i === currentStep
         ? ""
         : "next-step";
       const isNotStartOrFinalStep =
-        (i + 1 > 1 && i + 1 !== numSteps) || (i + 1 < numSteps && i + 1 !== 1);
+        (i > 1 && i !== numSteps) || (i < numSteps && i !== 1);
 
       if (isNotStartOrFinalStep) {
-        if (i + 1 < midPoint) className = className.concat(" before-middle");
-        if (i + 1 > midPoint) className = className.concat(" after-middle");
-        if (i + 1 === midPoint) className = className.concat(" middle");
+        if (i < midPoint) className = className.concat(" before-middle");
+        if (i > midPoint) className = className.concat(" after-middle");
+        if (i === midPoint) className = className.concat(" middle");
       }
-      if (i + 1 === 1) {
+      if (i === 1) {
         className = className.concat(" start");
       }
-      if (i + 1 === numSteps) className = className.concat(" end");
+      if (i === numSteps) className = className.concat(" end");
 
       const item = (
         <StyledStepperItem className={className} key={i}>
           {!completed ? (
-            <div className="step-counter">{i + 1}</div>
+            <div className="step-counter">{i}</div>
           ) : (
             <div className="step-counter checkmark">
               <FontAwesomeIcon icon={faCheck} />
