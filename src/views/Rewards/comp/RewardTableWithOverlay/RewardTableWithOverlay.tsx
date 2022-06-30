c7cafec2dfd37ff0dd5ab79af145e59a3303b0ad
Tulun <jaykiraly@gmail.com>
2022-06-30 13:02:58 -0700

    fixed overlay positioning
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/views/Rewards/comp/RewardTableWithOverlay/RewardTableWithOverlay.tsx
+++ src/views/Rewards/comp/RewardTableWithOverlay/RewardTableWithOverlay.tsx
@@ -3,16 +3,16 @@ import { useConnection } from "state/hooks";
 import createMyReferralsTableJSX, {
   headers,
 } from "../RewardTable/createMyReferralsTableJSX";
-
+import { Wrapper } from "./RewardTableWithOverlay.styles";
 const RewardTableWithOverlay = () => {
   const { isConnected } = useConnection();
 
   const rows = createMyReferralsTableJSX(isConnected);
   return (
-    <div>
+    <Wrapper>
       {!isConnected ? <ConnectTableOverlay /> : null}
       <RewardTable title="My referrals" rows={rows} headers={headers} />
-    </div>
+    </Wrapper>
   );
 };
 
