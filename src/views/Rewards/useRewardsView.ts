d7e9e3d298c8014f4053bf616d237737ad4d6a38
Tulun <jaykiraly@gmail.com>
2022-07-06 15:07:14 -0700

    Pagination WIP
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/views/Rewards/useRewardsView.ts
+++ src/views/Rewards/useRewardsView.ts
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState, useMemo } from "react";
 import ReactTooltip from "react-tooltip";
 
 import { useConnection } from "state/hooks";
 import { useReferrals } from "hooks/useReferrals";
 import { useReferralSummary } from "hooks/useReferralSummary";
+const DEFAULT_PAGE_SIZE = 5;
 
 export const useRewardsView = () => {
   const { isConnected, account } = useConnection();
   const { referrals } = useReferrals(account || "");
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageSize, setPageSize] = useState(DEFAULT_PAGE_SIZE);
+  const pageSizes = useMemo(() => [5, 10, 25, 50], []);
   const { summary, isLoading: isReferalSummaryLoading } = useReferralSummary(
     account || ""
   );
@@ -22,5 +26,10 @@ export const useRewardsView = () => {
     isConnected,
     account,
     referrals,
+    currentPage,
+    setCurrentPage,
+    pageSize,
+    setPageSize,
+    pageSizes,
   };
 };
