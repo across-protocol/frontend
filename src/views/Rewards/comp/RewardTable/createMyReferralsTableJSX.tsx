931e8122b2a91110c641634d21a2a97fbadce9a5
Tulun <jaykiraly@gmail.com>
2022-07-06 11:57:27 -0700

    Missing icons.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/views/Rewards/comp/RewardTable/createMyReferralsTableJSX.tsx
+++ src/views/Rewards/comp/RewardTable/createMyReferralsTableJSX.tsx
@@ -13,6 +13,11 @@ import {
   StyledUNILogo,
   StyledUSDCLogo,
   ReferralDiv,
+  StyledWETHLogo,
+  StyledDaiLogo,
+  StyledWBTCLogo,
+  StyledUmaLogo,
+  StyledBadgerLogo,
 } from "./RewardTables.styles";
 import { ReactComponent as UserIcon } from "assets/user.svg";
 import { ReactComponent as ArrowUserIcon } from "assets/corner-down-right.svg";
@@ -35,6 +40,16 @@ function determineIcon(symbol: string) {
       return <StyledUSDCLogo />;
     case "UNI":
       return <StyledUNILogo />;
+    case "WBTC":
+      return <StyledWBTCLogo />;
+    case "WETH":
+      return <StyledWETHLogo />;
+    case "DAI":
+      return <StyledDaiLogo />;
+    case "UMA":
+      return <StyledUmaLogo />;
+    case "BADGER":
+      return <StyledBadgerLogo />;
     default:
       return <StyledETHIcon />;
   }
