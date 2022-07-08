7d2ed7b44ed8d02d6973847697051a68dc21cc96
Tulun <jaykiraly@gmail.com>
2022-07-08 11:34:10 -0700

    Reward block stub.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/views/Rewards/comp/RewardMediumBlock/RewardMediumBlock.styles.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 1400px;
+  display: flex;
+  justify-content: center;
+`;
---
+++ src/views/Rewards/comp/RewardMediumBlock/RewardMediumBlock.tsx
@@ -0,0 +1,16 @@
+import { Wrapper } from "./RewardMediumBlock.styles";
+const RewardMediumBlock = () => {
+  return (
+    <Wrapper>
+      <div>
+        <span>
+          For a limited amount of time, all rewards from transfers will be
+          increased by 4x.
+        </span>
+        <span>Read more</span>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default RewardMediumBlock;
