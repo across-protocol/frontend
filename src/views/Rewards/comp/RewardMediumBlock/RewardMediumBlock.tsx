226cc427b181cc03540f2a272b289cf5ed0caef8
amateima <amatei@umaproject.org>
2022-07-14 22:47:25 +0300

    Update multiplier
    Signed-off-by: amateima <amatei@umaproject.org>

--- src/views/Rewards/comp/RewardMediumBlock/RewardMediumBlock.tsx
+++ src/views/Rewards/comp/RewardMediumBlock/RewardMediumBlock.tsx
@@ -6,7 +6,7 @@ const RewardMediumBlock = () => {
     <Wrapper>
       <span>
         For a limited amount of time, all rewards from transfers will be
-        increased by 4x.
+        increased by 3x.
       </span>
       <a href={mediumUrl} target="_blank" rel="noreferrer">
         Read more <ExternalLinkIcon />
