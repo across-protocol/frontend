8c2f0b4ed0f17a6c750cc8f2aaaea89bda8d2ce9
Tulun <jaykiraly@gmail.com>
2022-07-27 14:45:57 -0700

    return val
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/hooks/useReferrer.ts
+++ src/hooks/useReferrer.ts
@@ -5,6 +5,5 @@ export default function useReferrer() {
   // If ref and referrer params exist, prefer referrer param.
   // Not likely to happen but should have a catch if we get a bad link.
   // TODO? Test which of these is a good value?
-  let r = referrer || refParam;
-  return r;
+  return referrer || refParam;
 }
