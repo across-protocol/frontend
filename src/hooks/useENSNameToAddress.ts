7d277ecc64e0d4d52173667c75639d15ec83c491
Tulun <jaykiraly@gmail.com>
2022-08-09 15:43:21 -0700

    further simplification
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/hooks/useENSNameToAddress.ts
+++ src/hooks/useENSNameToAddress.ts
@@ -5,14 +5,14 @@ export default function useENSNameToAddress(
   referrer: string,
   provider: ethers.providers.Provider | undefined
 ) {
-  const [address, setAddress] = useState<string | null | undefined>(null);
+  const [address, setAddress] = useState<string>("");
   const lookupAddress = useCallback(async () => {
     try {
       const resolvedAddress = await provider?.resolveName(referrer);
-      setAddress(resolvedAddress);
+      setAddress(resolvedAddress || "");
     } catch (e) {
       console.log("error resolving name", e);
-      setAddress(null);
+      setAddress("");
     }
   }, [referrer, provider]);
 
