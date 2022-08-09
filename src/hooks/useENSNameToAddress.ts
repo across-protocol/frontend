021a9973ef21e8d0c30479cd50e073db60999595
Tulun <jaykiraly@gmail.com>
2022-08-09 15:52:36 -0700

    refactor
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/hooks/useENSNameToAddress.ts
+++ src/hooks/useENSNameToAddress.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
 export default function useENSNameToAddress(
@@ -6,17 +6,30 @@ export default function useENSNameToAddress(
   provider: ethers.providers.Provider | undefined
 ) {
   const [address, setAddress] = useState<string>("");
-  const lookupAddress = useCallback(async () => {
-    try {
-      const resolvedAddress = await provider?.resolveName(referrer);
-      setAddress(resolvedAddress || "");
-    } catch (e) {
-      console.log("error resolving name", e);
-      setAddress("");
+  useEffect(() => {
+    if (provider && referrer) {
+      lookupAddress(referrer, provider).then(setAddress);
     }
   }, [referrer, provider]);
 
-  if (referrer && provider) lookupAddress();
-
   return address;
+}
+
+/**
+ *
+ * @param referrer
+ * @param provider
+ * @returns Promise<string>, resolves to the address of the ENS name if it exists
+ */
+async function lookupAddress(
+  referrer: string,
+  provider: ethers.providers.Provider
+) {
+  try {
+    const resolvedAddress = await provider.resolveName(referrer);
+    return resolvedAddress || "";
+  } catch (e) {
+    console.log("error resolving name", e);
+    return "";
+  }
 }
