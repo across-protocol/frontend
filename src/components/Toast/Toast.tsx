2abc3bc61d4b21b961ea8b4208d3c4448f0ccd16
Tulun <jaykiraly@gmail.com>
2022-08-11 15:32:26 -0700

    stub values
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+import "./Toast.css";
+
+interface ToastProps {
+  toastList: any[];
+  position: string;
+  autoDelete?: boolean;
+  autoDeleteTime?: number;
+}
+
+const Toast: React.FC<ToastProps> = (props) => {
+  const { toastList, position, autoDelete, autoDeleteTime } = props;
+  const [list, setList] = useState(toastList);
+
+  useEffect(() => {
+    setList([...toastList]);
+  }, [toastList]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (autoDelete && toastList.length && list.length) {
+        deleteToast(toastList[0].id);
+      }
+    }, autoDeleteTime);
+
+    return () => {
+      clearInterval(interval);
+    };
+
+    // eslint-disable-next-line
+  }, [toastList, autoDelete, autoDeleteTime, list]);
+
+  const deleteToast = (id: number) => {
+    const listItemIndex = list.findIndex((e) => e.id === id);
+    const toastListItem = toastList.findIndex((e) => e.id === id);
+    list.splice(listItemIndex, 1);
+    toastList.splice(toastListItem, 1);
+    setList([...list]);
+  };
+
+  return (
+    <>
+      <div className={`notification-container ${position}`}>
+        {list.map((toast, i) => (
+          <div
+            key={i}
+            className={`notification toast ${position}`}
+            style={{ backgroundColor: toast.backgroundColor }}
+          >
+            <button onClick={() => deleteToast(toast.id)}>X</button>
+            <div className="notification-image">
+              <img src={toast.icon} alt="" />
+            </div>
+            <div>
+              <p className="notification-title">{toast.title}</p>
+              <p className="notification-message">{toast.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Toast;
