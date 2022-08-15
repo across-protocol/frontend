0074f78ffa1e77df0d1353acbbadb3361d123e16
Tulun <jaykiraly@gmail.com>
2022-08-15 15:30:50 -0700

    move addToast to useBridge
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/hooks/useReferrer.ts
+++ src/hooks/useReferrer.ts
@@ -2,18 +2,20 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import { useQueryParams } from "./useQueryParams";
 import { useConnection } from "state/hooks";
-import { useToast } from "components/Toast/useToast";
+// import { useToast } from "components/Toast/useToast";
 
 export default function useReferrer() {
   const { provider } = useConnection();
   const { referrer, ref: refParam } = useQueryParams();
-  const { addToast } = useToast();
+  // const { addToast } = useToast();
 
   // Default to referrer if query ref isn't provided.
   const r = refParam || referrer;
 
   const [address, setAddress] = useState<string>("");
+  const [error, setError] = useState<string>("");
   useEffect(() => {
+    setError("");
     if (provider && r) {
       if (ethers.utils.isAddress(r)) {
         return setAddress(r);
@@ -25,11 +27,13 @@ export default function useReferrer() {
           .then((ra) => {
             setAddress(ra || "");
             if (!ra) {
-              addToast({
-                type: "error",
-                title: "Error",
-                body: "Invalid referral ENS name",
-              });
+              setError("Invalid referral ENS name");
+
+              // addToast({
+              //   type: "error",
+              //   title: "Error",
+              //   body: "Invalid referral ENS name",
+              // });
             }
           })
           .catch((e) => {
@@ -37,19 +41,21 @@ export default function useReferrer() {
             console.warn("error resolving name", e);
             setAddress("");
             if (!ethers.utils.isAddress(r)) {
-              addToast({
-                type: "error",
-                title: "Error",
-                body: "Invalid referral address",
-              });
+              setError("Invalid referral address");
+              // addToast({
+              //   type: "error",
+              //   title: "Error",
+              //   body: "Invalid referral address",
+              // });
             }
           });
       } else {
-        return addToast({
-          type: "error",
-          title: "Error",
-          body: "Invalid referral address",
-        });
+        setError("Invalid referral address");
+        // return addToast({
+        //   type: "error",
+        //   title: "Error",
+        //   body: "Invalid referral address",
+        // });
       }
     }
     // eslint-disable-next-line
@@ -57,5 +63,5 @@ export default function useReferrer() {
   // If ref and referrer params exist, prefer referrer param.
   // Not likely to happen but should have a catch if we get a bad link.
   // TODO? Test which of these is a good value?
-  return { referrer: address };
+  return { referrer: address, error };
 }
