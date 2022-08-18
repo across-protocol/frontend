4e26993d88c779b6528e010c5e25b21a29513d04
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-08-18 17:24:28 -0400

    improve: refactor view name and correct the route (#227)

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,7 +6,7 @@ import {
   About,
   Transactions,
   Rewards,
-  PoolRewards,
+  Staking,
   Claim,
 } from "views";
 import { Header, SuperHeader, Banner, Sidebar } from "components";
@@ -79,7 +79,7 @@ const Routes: React.FC = () => {
         <Route exact path="/about" component={About} />
         <Route exact path="/rewards" component={Rewards} />
         <Route exact path="/rewards/claim" component={Claim} />
-        <Route path="/rewards/:poolId" component={PoolRewards} />
+        <Route exact path="/rewards/staking/:poolId" component={Staking} />
         <Route path="/" component={Send} />
       </Switch>
       <Toast position="top-right" />
