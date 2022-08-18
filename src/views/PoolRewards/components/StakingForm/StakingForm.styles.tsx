12f6c8b2448e4da8a79283b7bf30c9da237d3fca
Tulun <jaykiraly@gmail.com>
2022-08-18 13:58:59 -0700

    scaffold of StakingForm
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/views/PoolRewards/components/StakingForm/StakingForm.styles.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 24px 0px 0px;
+  gap: 24px;
+  background-color: #34353b;
+  border: 1px solid #3e4047;
+  border-radius: 10px;
+  box-sizing: border-box;
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+`;
---
+++ src/views/PoolRewards/components/StakingForm/StakingForm.tsx
@@ -0,0 +1,6 @@
+import { Wrapper } from "./StakingForm.styles";
+export const StakingForm = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default StakingForm;
