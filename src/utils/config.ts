28b8c5b4609886c9220f1cb08914963aa953bb5a
Tulun <jaykiraly@gmail.com>
2022-08-18 15:20:34 -0700

    removed toLowerCase and added correct checksum
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -158,9 +158,7 @@ export class ConfigClient {
   // this has a chance to mix up eth/weth which can be a problem. prefer token by symbol.
   getTokenInfoByAddress(chainId: number, address: string): Token {
     const tokens = this.getTokenList(chainId);
-    const token = tokens.find(
-      (token) => token.address.toLowerCase() === address.toLowerCase()
-    );
+    const token = tokens.find((token) => token.address === address);
     assert(
       token,
       `Token not found on chain: ${chainId} and address ${address}`
