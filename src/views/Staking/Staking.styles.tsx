c443c0eb1a6ab39810bcd4b8c3248147e8d13ae0
James Morris <jaamorris@cs.stonybrook.edu>
2022-08-19 14:29:18 -0400

    improve: update breakpoint for mobile wrapper

--- src/views/Staking/Staking.styles.tsx
+++ src/views/Staking/Staking.styles.tsx
@@ -3,5 +3,10 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   background-color: transparent;
   max-width: 600px;
-  margin: 67px auto 27px;
+  margin: 64px auto 86px;
+
+  @media (max-width: 576px) {
+    margin: 16px auto 122px;
+    width: 404px;
+  }
 `;
