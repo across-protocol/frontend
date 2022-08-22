4e1e2c74f2f927ccc02af46513c3228190ebea52
Tulun <jaykiraly@gmail.com>
2022-08-22 13:42:00 -0700

    starting v2 utility.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ src/utils/onboardV2.ts
@@ -0,0 +1,42 @@
+import Onboard from "@web3-onboard/core";
+import injectedModule from "@web3-onboard/injected-wallets";
+import { ethers } from "ethers";
+
+const MAINNET_RPC_URL = "https://mainnet.infura.io/v3/<INFURA_KEY>";
+
+const injected = injectedModule();
+
+const onboard = Onboard({
+  wallets: [injected],
+  chains: [
+    {
+      id: "0x1",
+      token: "ETH",
+      label: "Ethereum Mainnet",
+      rpcUrl: MAINNET_RPC_URL,
+    },
+  ],
+});
+
+const wallets = await onboard.connectWallet();
+
+console.log(wallets);
+
+if (wallets[0]) {
+  // create an ethers provider with the last connected wallet provider
+  const ethersProvider = new ethers.providers.Web3Provider(
+    wallets[0].provider,
+    "any"
+  );
+
+  const signer = ethersProvider.getSigner();
+
+  // send a transaction with the ethers provider
+  const txn = await signer.sendTransaction({
+    to: "0x",
+    value: 100000000000000,
+  });
+
+  const receipt = await txn.wait();
+  console.log(receipt);
+}
