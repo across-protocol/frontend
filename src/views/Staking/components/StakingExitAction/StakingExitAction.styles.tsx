e199e2bdd31d879ef977195b2ee6e81c59c08110
James Morris <jaamorris@cs.stonybrook.edu>
2022-08-22 12:35:44 -0400

    improve: consolidate query size into constant

--- src/views/Staking/components/StakingExitAction/StakingExitAction.styles.tsx
+++ src/views/Staking/components/StakingExitAction/StakingExitAction.styles.tsx
@@ -2,13 +2,15 @@ import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const INTERNAL_BREAKPOINT = "(max-width: 576px)";
+
 export const Wrapper = styled.div`
   margin: 0px 0px 24px 23px;
   display: flex;
   align-items: center;
   justify-content: start;
 
-  @media (max-width: 576px) {
+  @media ${INTERNAL_BREAKPOINT} {
     margin: 0px 0px 17px 19px;
   }
 `;
@@ -16,7 +18,7 @@ export const Wrapper = styled.div`
 export const ExitIcon = styled(FontAwesomeIcon)`
   color: #9daab2;
   font-size: 16px;
-  @media (max-width: 576px) {
+  @media ${INTERNAL_BREAKPOINT} {
     font-size: 17px;
   }
 `;
@@ -28,7 +30,7 @@ export const Logo = styled.img`
   width: 32px;
 
   margin: 0px 12px 0px 18px;
-  @media (max-width: 576px) {
+  @media ${INTERNAL_BREAKPOINT} {
     margin-right: 8px;
     height: 24px;
     width: 24px;
@@ -43,7 +45,7 @@ export const Text = styled.p`
   color: #e0f3ff;
 
   font-size: 22px;
-  @media (max-width: 576px) {
+  @media ${INTERNAL_BREAKPOINT} {
     font-size: 18px;
   }
 `;
