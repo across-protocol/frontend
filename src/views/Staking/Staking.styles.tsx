f73da1badef2369b71c6ef2d72c05ddfc4d1f116
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-08-23 17:01:45 -0400

    feat: add exit functionality from staking page (#230)
    * feat: scaffold pool staking/unstaking page (#224)
    
    * feat: begin scaffolding of PoolRewards view
    
    * improve: set base links to active if in subpath
    
    * improve: add wrapper stylization
    
    * fix: revert subpath logic
    
    * improve: refactor view name and correct the route (#227)
    
    * feat: scaffold pool staking/unstaking page (#224)
    
    * feat: begin scaffolding of PoolRewards view
    
    * improve: set base links to active if in subpath
    
    * improve: add wrapper stylization
    
    * fix: revert subpath logic
    
    * improve: refactor view name and correct the route (#227)
    
    * rebase; fixed exports.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * improve: update breakpoint for mobile wrapper
    
    * feat: add staking exit page
    
    * improve: provide attribute for specifying logo URI
    
    * improve: update prop for pool name
    
    * nit: change preview to show figma specific info
    
    * improve: update margin to further align with figma
    
    * improve: consolidate query size into constant
    
    * fix: prefer const to unchanging variable
    
    * improve: prefer constant object over custom value
    
    * fix: reorder the order of the staking components
    
    * improve: set cursor to a pointer for exit button
    
    * improve: add link to the entire exit widget
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    Co-authored-by: Tulun <jaykiraly@gmail.com>

--- src/views/Staking/Staking.styles.tsx
+++ src/views/Staking/Staking.styles.tsx
@@ -1,7 +1,13 @@
 import styled from "@emotion/styled";
+import { QUERIESV2 } from "utils";
 
 export const Wrapper = styled.div`
   background-color: transparent;
   max-width: 600px;
-  margin: 67px auto 27px;
+  margin: 64px auto 86px;
+
+  @media ${QUERIESV2.sm} {
+    margin: 16px auto 122px;
+    width: 404px;
+  }
 `;
