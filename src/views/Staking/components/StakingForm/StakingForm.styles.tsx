c96bfbf480a43be24c06cf25626d7d0be4150c84
James Morris <jaamorris@cs.stonybrook.edu>
2022-08-25 10:51:44 -0400

    refactor: remove unused styles

--- src/views/Staking/components/StakingForm/StakingForm.styles.tsx
+++ src/views/Staking/components/StakingForm/StakingForm.styles.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import { SecondaryButtonWithoutShadow as UnstyledButton } from "components/Buttons";
 import ProgressBar from "components/ProgressBar";
 import { ReactComponent as UnstyedUsdcLogo } from "assets/icons/usdc-24.svg";
 import { ReactComponent as UnstyledArrowIcon } from "assets/icons/arrow-16.svg";
@@ -44,82 +43,11 @@ export const Tab = styled.div<ITab>`
   cursor: pointer;
 `;
 
-export const InputRow = styled.div`
-  display: flex;
-  width: calc(100% - 48px);
-  margin: 0 auto;
-  gap: 16px;
-  border-bottom: 1px solid #3e4047;
-  padding-bottom: 24px;
-  flex-direction: row;
-  @media ${QUERIESV2.sm} {
-    flex-direction: column;
-  }
-`;
-
 export const UsdcLogo = styled(UnstyedUsdcLogo)``;
 
 export const InputWrapper = styled.div`
   flex-grow: 8;
   position: relative;
-`;
-
-export const Input = styled.input`
-  height: 64px;
-  padding: 9px 64px;
-  width: 100%;
-  background: #2d2e33;
-  border: 1px solid #4c4e57;
-  border-radius: 32px;
-  color: #e0f3ff;
-`;
-
-export const MaxButton = styled(UnstyledButton)`
-  height: 24px;
-  font-family: "Barlow";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  letter-spacing: 0.04em;
-  text-transform: uppercase;
-  color: #c5d5e0;
-  position: absolute;
-  right: 24px;
-  top: 20px;
-  padding: 8px 10px 10px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #4c4e57;
-  border-radius: 24px;
-  &:hover {
-    color: #e0f3ff;
-    border-color: #e0f3ff;
-  }
-`;
-
-export const ButtonWrapper = styled.div`
-  flex-grow: 1;
-`;
-
-interface IStakeButton {
-  valid: boolean;
-}
-export const StakeButton = styled(UnstyledButton)<IStakeButton>`
-  background: #6cf9d8;
-  padding: 0px 40px;
-  height: 64px;
-  color: #2d2e33;
-  opacity: ${({ valid }) => (valid ? 1 : 0.25)};
-  @media ${QUERIESV2.sm} {
-    text-align: center;
-    width: 100%;
-    height: 40px;
-    padding: 0px 20px;
-    border-radius: 20px;
-  }
 `;
 
 export const StakeInfo = styled.div`
