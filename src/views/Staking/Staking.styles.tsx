21cdbe1939afca925c37cd19cd8947bce3f15c2c
James Morris <jaamorris@cs.stonybrook.edu>
2022-08-25 09:31:56 -0400

    fix: enable smooth transition of width prop

--- src/views/Staking/Staking.styles.tsx
+++ src/views/Staking/Staking.styles.tsx
@@ -3,13 +3,16 @@ import { QUERIESV2 } from "utils";
 
 export const Wrapper = styled.div`
   background-color: transparent;
+
   max-width: 600px;
-  margin: 67px auto 27px;
+  width: calc(100% - 24px);
+
+  margin: 60px auto 20px;
   display: flex;
   flex-direction: column;
   gap: 16px;
+
   @media ${QUERIESV2.sm} {
-    margin: 16px auto 122px;
-    max-width: 404px;
+    width: 100%;
   }
 `;
