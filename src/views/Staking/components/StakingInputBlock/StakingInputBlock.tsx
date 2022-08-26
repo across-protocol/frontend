d2bae88146b0e1481056b4a9b27231df06ac177e
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-08-26 17:20:53 -0400

    feat: add disconnected view to staking reward page (#239)
    * feat: include prop to display full width button
    
    * nit: update staking form to showcase change
    
    * feat: add a simple function to encapsulate ternary
    
    * fix: remove utility function
    
    * improve: add on-click handler to buttons
    
    * improve: consolidated component input types
    
    * feat: connected wallet connection and onboard init
    
    * nit: use implicit key/value pair for creating return object
    
    * feat: add functional ternary closure builder
    
    * improve: rename and prefer util closure builder
    
    * improve: define variable types more accurately

--- src/views/Staking/components/StakingInputBlock/StakingInputBlock.tsx
+++ src/views/Staking/components/StakingInputBlock/StakingInputBlock.tsx
@@ -10,23 +10,27 @@ import { capitalizeFirstLetter } from "utils/format";
 import { StyledComponent } from "@emotion/styled";
 import { Theme } from "@emotion/react";
 
+type StylizedSVGComponent = StyledComponent<
+  React.SVGProps<SVGSVGElement> & {
+    title?: string | undefined;
+  } & {
+    children?: React.ReactNode;
+  } & {
+    theme?: Theme | undefined;
+  },
+  {},
+  {}
+>;
+
 interface Props {
   value: string;
   setValue: React.Dispatch<React.SetStateAction<string>>;
   valid: boolean;
   buttonText: string;
-  Logo: StyledComponent<
-    React.SVGProps<SVGSVGElement> & {
-      title?: string | undefined;
-    } & {
-      children?: React.ReactNode;
-    } & {
-      theme?: Theme | undefined;
-    },
-    {},
-    {}
-  >;
+  Logo: StylizedSVGComponent;
   maxValue: string;
+  omitInput?: boolean;
+  onClickHandler: () => void;
 }
 
 const StakingInputBlock: React.FC<Props> = ({
@@ -36,22 +40,30 @@ const StakingInputBlock: React.FC<Props> = ({
   buttonText,
   Logo,
   maxValue,
+  omitInput,
+  onClickHandler,
 }) => {
   return (
     <InputRow>
-      <InputWrapper>
-        <Logo />
-        <Input
-          placeholder="Enter amount"
-          value={value}
-          type="text"
-          onChange={(e) => setValue(e.target.value)}
-          valid={!value || valid}
-        />
-        <MaxButton onClick={() => setValue(maxValue ?? "")}>Max</MaxButton>
-      </InputWrapper>
+      {!omitInput && (
+        <InputWrapper>
+          <Logo />
+          <Input
+            placeholder="Enter amount"
+            value={value}
+            type="text"
+            onChange={(e) => setValue(e.target.value)}
+            valid={!value || valid}
+          />
+          <MaxButton onClick={() => setValue(maxValue ?? "")}>Max</MaxButton>
+        </InputWrapper>
+      )}
       <ButtonWrapper>
-        <StakeButton valid={valid}>
+        <StakeButton
+          valid={valid}
+          fullWidth={omitInput}
+          onClick={onClickHandler}
+        >
           {capitalizeFirstLetter(buttonText)}
         </StakeButton>
       </ButtonWrapper>
