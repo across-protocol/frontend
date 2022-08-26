896289c719d5392618caba70c5fee060795546cf
James Morris <jaamorris@cs.stonybrook.edu>
2022-08-26 11:26:17 -0400

    feat: add footer

--- src/views/Staking/Staking.tsx
+++ src/views/Staking/Staking.tsx
@@ -1,20 +1,24 @@
 import { Wrapper } from "./Staking.styles";
 import { StakingReward, StakingForm, StakingExitAction } from "./components";
 import { useStakingView } from "./useStakingView";
+import Footer from "components/Footer";
 
 const Staking = () => {
   const { amountOfRewardsClaimable, poolName, exitLinkURI, poolLogoURI } =
     useStakingView();
   return (
-    <Wrapper>
-      <StakingExitAction
-        poolName={poolName}
-        exitLinkURI={exitLinkURI}
-        poolLogoURI={poolLogoURI}
-      />
-      <StakingForm />
-      <StakingReward maximumClaimableAmount={amountOfRewardsClaimable} />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <StakingExitAction
+          poolName={poolName}
+          exitLinkURI={exitLinkURI}
+          poolLogoURI={poolLogoURI}
+        />
+        <StakingForm />
+        <StakingReward maximumClaimableAmount={amountOfRewardsClaimable} />
+      </Wrapper>
+      <Footer />
+    </>
   );
 };
 
