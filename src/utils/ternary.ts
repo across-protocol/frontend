aee378cba01e6bf27d1bb51b945d9eea4be76aaf
James Morris <jaamorris@cs.stonybrook.edu>
2022-08-29 13:15:45 -0400

    fix: make functional ternary more generic

--- src/utils/ternary.ts
+++ src/utils/ternary.ts
@@ -1,21 +1,19 @@
-import { ReactElement } from "react";
-
 /**
  * Creates a reusable ternary operation function.
  * @param expression The logical expression to test the ternary. For example, the variable A in -> A ? B : C
  * @param fallbackValue The Else value in a ternary. For example, the variable C in -> A ? B : C
  * @returns A function that emulates a ternary operation. This closure takes the truthy return and evaluates the ternary.
  */
-export function repeatableTernaryBuilder(
+export function repeatableTernaryBuilder<Type>(
   expression: boolean,
-  fallbackValue: ReactElement
+  fallbackValue: Type
 ) {
   /**
    * Represents a ternary operation.
    * @param value The returned in a ternary if the expression is true. For example, the variable B in -> A ? B : C
    * @returns `value` if `expression` is true, else `fallbackValue`
    */
-  function closure(value: ReactElement): ReactElement {
+  function closure(value: Type): Type {
     return expression ? value : fallbackValue;
   }
   return closure;
