ef38a4ee4252a836120541eae061931eb3faff26
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-08-29 11:41:51 -0400

    feat: add uniform layout component for new views (#241)
    * feat: add layout to include footer
    
    * improve: export layout on index
    
    * fix: utilize 100vh to overcome sticky header bug

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,6 +10,7 @@ export { default as SuperHeader } from "./SuperHeader";
 export { default as Stepper } from "./Stepper";
 export { default as Banner } from "./Banner";
 export { default as Sidebar } from "./Sidebar";
+export { default as LayoutV2 } from "./LayoutV2";
 
 export * from "./Box";
 export * from "./Buttons";
