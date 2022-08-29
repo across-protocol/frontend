52be0aaf5d02437ead198308509848db1e2ad74c
James Morris <jaamorris@cs.stonybrook.edu>
2022-08-29 12:08:18 -0400

    fix: include types to remove error

--- src/views/Staking/types.ts
+++ src/views/Staking/types.ts
@@ -1,10 +1,26 @@
+import { BigNumberish } from "ethers";
+import { Theme } from "@emotion/react";
+import { StyledComponent } from "@emotion/styled";
+
+export type StylizedSVG = StyledComponent<
+  React.SVGProps<SVGSVGElement> & {
+    title?: string | undefined;
+  } & {
+    children?: React.ReactNode;
+  } & {
+    theme?: Theme | undefined;
+  },
+  {},
+  {}
+>;
+
 type GenericStakingComponentProps = {
   isConnected: boolean;
   walletConnectionHandler: () => void;
 };
 
 export type StakingRewardPropType = GenericStakingComponentProps & {
-  maximumClaimableAmount: number;
+  maximumClaimableAmount: BigNumberish;
 };
 
 export type StakingFormPropType = GenericStakingComponentProps & {};
