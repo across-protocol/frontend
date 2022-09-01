a897a77e2c749dffe5503844e00147e4573de2ed
Tulun <jaykiraly@gmail.com>
2022-09-01 16:16:40 -0700

    Waits from bridge.cy removed.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- cypress/e2e/bridge.cy.ts
+++ cypress/e2e/bridge.cy.ts
@@ -1,6 +1,5 @@
 describe("bridge", () => {
   it("render in initial state", () => {
-    cy.wait(5000);
     cy.visit("/");
     cy.dataCy("connect-wallet").should("be.visible");
     cy.dataCy("send").should("be.disabled");
@@ -8,7 +7,6 @@ describe("bridge", () => {
 
   it("display balance of 'from' asset ", () => {
     cy.connectInjectedWallet("connect-wallet");
-    cy.wait(5000);
     cy.dataCy("balance").should("be.visible");
   });
 
@@ -21,15 +19,12 @@ describe("bridge", () => {
   });
 
   it("Clicks send and submits a tx and shows the success page", () => {
-    cy.wait(6000);
     cy.dataCy("send").click();
-    cy.wait(5000);
     cy.dataCy("transaction-submitted").should("be.visible");
   });
 
   it("Closes the success page and goes back to main bridge", () => {
     cy.dataCy("bridge-success-button").click();
-    cy.wait(1000);
     cy.dataCy("bridge-amount-input").should("be.visible");
   });
 });
