bc81f65b1847c266fd481575959a2c57d856c4aa
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-01 10:46:25 -0400

    fix: update fixed point division

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { BigNumber, ethers } from "ethers";
 import { Fee } from "./bridge";
 import { ChainId } from "./constants";
+import { parseUnits } from "./format";
 
 export type APIEndpoint = "limits" | "suggested-fees";
 type EndpointMethods = "get" | "post";
@@ -44,13 +45,13 @@ export async function suggestedFeesApiCall(
   });
 
   const relayFeePct = BigNumber.from(result["relayFeePct"]);
-  const relayFeeTotal = amount.mul(relayFeePct);
+  const relayFeeTotal = amount.mul(relayFeePct).div(parseUnits("1", 18));
 
   const capitalFeePct = BigNumber.from(result["capitalFeePct"]);
-  const capitalFeeTotal = amount.mul(capitalFeePct);
+  const capitalFeeTotal = amount.mul(capitalFeePct).div(parseUnits("1", 18));
 
   const relayGasFeePct = BigNumber.from(result["relayGasFeePct"]);
-  const relayGasFeeTotal = amount.mul(relayGasFeePct);
+  const relayGasFeeTotal = amount.mul(relayGasFeePct).div(parseUnits("1", 18));
 
   console.log(relayGasFeePct.toString());
   console.log(relayGasFeeTotal.toString());
