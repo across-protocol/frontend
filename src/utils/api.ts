c7e29e237e3c689a693cadeac2fec741fff43d25
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-02 09:40:50 -0400

    improve: defer call to axios directly

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,39 +4,6 @@ import { Fee } from "./bridge";
 import { ChainId } from "./constants";
 
 /**
- * A current list of API endpoints supported by this function
- */
-export type APIEndpoint = "limits" | "suggested-fees";
-type EndpointMethods = "get" | "post";
-
-/**
- * Creates a call to the Across Serverless API to the specified endpoint
- * @param endpoint The endpoint to make an HTTP request to
- * @param payload The content of the information transmitted to the endpoint. Note: query params if this method is a GET and data body if this method is POST/PUT
- * @returns The JSON/text content of the API call
- */
-export async function apiCall(
-  endpoint: APIEndpoint,
-  payload: any
-): Promise<any> {
-  let method: EndpointMethods = "get";
-  switch (endpoint) {
-    case "limits":
-    case "suggested-fees":
-      method = "get";
-      break;
-  }
-  const axiosConfig = {
-    [method === "get" ? "params" : "data"]: payload,
-  };
-  const url = `/api/${endpoint}`;
-  const response = await axios.get(url, axiosConfig);
-  const data = response.data;
-
-  return data;
-}
-
-/**
  * Creates an HTTP call to the `suggested-fees` API endpoint
  * @param amount The amount of fees to calculate
  * @param originToken The ERC20 token address from the origin chain
@@ -53,13 +20,15 @@ export async function suggestedFeesApiCall(
   relayerCapitalFee: Fee;
   isAmountTooLow: boolean;
 }> {
-  const result = await apiCall("suggested-fees", {
-    token: originToken,
-    destinationChainId: toChainid,
-    amount: amount.toString(),
-    skipAmountLimit: true,
+  const response = await axios.get(`/api/suggested-fees`, {
+    params: {
+      token: originToken,
+      destinationChainId: toChainid,
+      amount: amount.toString(),
+      skipAmountLimit: true,
+    },
   });
-
+  const result = response.data;
   const relayFeePct = BigNumber.from(result["relayFeePct"]);
   const relayFeeTotal = BigNumber.from(result["relayFeeTotal"]);
 
