23ab3ce12c19bdfded651c6dec973a669a39c835
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-09-08 16:47:46 -0400

    feat: add title section to landing page (#265)
    * feat: add title card
    
    * fix: update padding to improve 1:1 with figma
    
    * feat: add link text

--- src/views/PreLaunchAirdrop/components/TitleSection.tsx
+++ src/views/PreLaunchAirdrop/components/TitleSection.tsx
@@ -1,64 +1,32 @@
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import { ReactComponent as ArrowIcon } from "assets/icons/arrow-right-16.svg";
-import AcrossLogoWithRings from "assets/ring.png";
-import React from "react";
-import { ButtonV2 } from "components";
 
-type TitleSectionProps = {
-  isConnected?: boolean;
-};
-
-const TitleSection: React.FC<TitleSectionProps> = ({ isConnected }) => {
+const TitleSection = () => {
   return (
     <Wrapper>
-      <StyledAcrossLogo src={AcrossLogoWithRings} />
-      <HeaderWrapper>
-        <PageHeader>ACX is about to launch.</PageHeader>
-        <PageSubHeader>
-          As a community member you will have the opportunity to make your claim
-          for a piece of the official ACX airdrop.
-        </PageSubHeader>
-      </HeaderWrapper>
-
-      <ButtonWrapper>
-        {!isConnected && (
-          <StyledButton size="lg">Connect to check eligibility</StyledButton>
-        )}
-        <EligibilityLink to={"/"}>
-          <InnerLinkText>Airdrop Details</InnerLinkText>
-          <StyledArrowIcon />
-        </EligibilityLink>
-      </ButtonWrapper>
+      <PageHeader>ACX is about to launch.</PageHeader>
+      <PageSubHeader>
+        The token launch is almost here and as a community member you will have
+        the opportunity to make your claim for a piece of the official airdrop.
+      </PageSubHeader>
+      <EligibilityLink to={"/"}>
+        <InnerLinkText>Read about airdrop eligibility</InnerLinkText>
+        <StyledArrowIcon />
+      </EligibilityLink>
     </Wrapper>
   );
 };
-export default React.memo(TitleSection);
+export default TitleSection;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 32px;
+  gap: 16px;
   max-width: 560px;
   width: 100%;
   text-align: center;
-`;
-
-const HeaderWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  gap: 16px;
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  text-align: center;
-  gap: 32px;
 `;
 
 const PageHeader = styled.h1`
@@ -102,18 +70,4 @@ const StyledArrowIcon = styled(ArrowIcon)`
   & * {
     stroke: #e0f3ff;
   }
-`;
-
-const StyledAcrossLogo = styled.img`
-  margin: -60px;
-`;
-
-const StyledButton = styled(ButtonV2)`
-  width: 293px;
-  height: 64px;
-
-  background: linear-gradient(264.97deg, #6cf9d8 24.16%, #c4fff1 61.61%);
-  box-shadow: 0px 0px 24px rgba(109, 250, 217, 0.25);
-  border-radius: 32px;
-  padding: 0px 40px;
 `;
