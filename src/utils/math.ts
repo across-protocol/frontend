864141a0fe9c4ef2257cf525f0913ea194dfd049
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-11 13:12:26 -0400

    refactor: create range utility function

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -18,3 +18,39 @@ export function safeDivide(numerator: BigNumber, divisor: BigNumber) {
   }
   return numerator.div(divisor);
 }
+
+/**
+ * Tests whether a BigNumberish is within range of a maximum and minimum
+ * @param value The value to test the range
+ * @param inclusive Whether this range is inclusive or exclusive of the range
+ * @param minimum The minimum of the range
+ * @param maximum The maximum of the range
+ * @param parser An optional parser to parse the `value` into a BigNumber. Defaults to `BigNumber.from`
+ * @returns `true` if `value` is valid and within range of the `maximum` and `minimum`
+ */
+export function isNumericWithinRange(
+  value: BigNumberish,
+  inclusive: boolean,
+  minimum?: BigNumberish,
+  maximum?: BigNumberish,
+  parser: (arg0: string) => BigNumber = BigNumber.from
+): boolean {
+  if (!isNumberEthersParseable(value)) return false;
+  const valueBN = parser(value.toString());
+  const min = (v: BigNumberish) => (inclusive ? valueBN.gte(v) : valueBN.gt(v));
+  const max = (v: BigNumberish) => (inclusive ? valueBN.lte(v) : valueBN.lt(v));
+
+  if (maximum && minimum) {
+    // Both min and max are defined
+    return min(minimum) && max(maximum);
+  } else if (!maximum && minimum) {
+    // minimum is only defined
+    return min(minimum);
+  } else if (maximum && !minimum) {
+    // maximum is only defined
+    return max(maximum);
+  } else {
+    // no range is defined. value is always within range of -inf, inf
+    return true;
+  }
+}
