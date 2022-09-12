ac689b540ea07403c84ae2adcd631c7c28816a08
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-09-12 16:14:27 -0400

    feat: :sparkles: create skeleton for airdrop cards (#266)
    * feat: create view for the pre-launch airdrop page (#251)
    
    * feat: add background img from figma file
    
    * refactor: create prop to make header transparent
    
    * feat: add basic prelaunch view
    
    * improve: add mobile layout to base mockup
    
    * nit: remove placeholder text
    
    * refactor: use a background layer
    
    * feat: add title section to landing page (#265)
    
    * feat: add title card
    
    * fix: update padding to improve 1:1 with figma
    
    * feat: add link text
    
    * feat: add eligible pill
    
    * nit: memoize title section
    
    * feat: add card icon with variable states
    
    * feat: add card skeleton
    
    * nit: add sample airdrop icon
    
    * improve: fix background in place
    
    * feat: add sample cards
    
    * feat: add storybook integration
    
    * fix: add global styling to storybook
    
    * fix: add storybook integration
    
    * fix: update z-index for storybook

--- src/views/PreLaunchAirdrop/PreLaunchAirdrop.tsx
+++ src/views/PreLaunchAirdrop/PreLaunchAirdrop.tsx
@@ -1,10 +1,14 @@
+import styled from "@emotion/styled";
 import Footer from "components/Footer";
 import { TitleSection } from "./components";
+import { ReactComponent as IIcon } from "assets/sample-airdrop-icon.svg";
+
 import {
   BackgroundLayer,
   ContentWrapper,
   Wrapper,
 } from "./PreLaunchAirdrop.styles";
+import AirdropCard from "./components/AirdropCard";
 
 const PreLaunchAirdrop = () => {
   return (
@@ -12,10 +16,53 @@ const PreLaunchAirdrop = () => {
       <BackgroundLayer />
       <ContentWrapper>
         <TitleSection />
+        <TestDiv>
+          <AirdropCard
+            Icon={TestIcon}
+            check={"undetermined"}
+            title="Across Community Member"
+            description="Community members can check eligibility for the ACX airdrop by connecting their Discord account to an Ethereum wallet."
+          />
+          <AirdropCard
+            Icon={TestIcon}
+            check={"undetermined"}
+            title="Across Liquidity Provider"
+            description="Community members can check eligibility for the ACX airdrop by connecting their Discord account to an Ethereum wallet."
+            externalLink="/"
+          />
+          <AirdropCard
+            Icon={TestIcon}
+            check={"eligible"}
+            title="Across Community Member"
+            description="Community members can check eligibility for the ACX airdrop by connecting their Discord account to an Ethereum wallet."
+          />
+          <AirdropCard
+            Icon={TestIcon}
+            check={"ineligible"}
+            title="Across Community Member"
+            description="Community members can check eligibility for the ACX airdrop by connecting their Discord account to an Ethereum wallet."
+          />
+          <AirdropCard
+            Icon={TestIcon}
+            title="Across Community Member"
+            description="Community members can check eligibility for the ACX airdrop by connecting their Discord account to an Ethereum wallet."
+          />
+        </TestDiv>
       </ContentWrapper>
       <Footer />
     </Wrapper>
   );
 };
+
+const TestIcon = styled(IIcon)`
+  height: 200px;
+  width: 200px;
+`;
+const TestDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 64px;
+  padding: 64px 0;
+`;
 
 export default PreLaunchAirdrop;
