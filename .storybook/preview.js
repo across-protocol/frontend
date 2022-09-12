a0a194ba57fd7fe01e20ad33c94c3b21ebf4879d
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-09-12 16:14:27 -0400

    feat: :sparkles: create skeleton for airdrop cards (#266)
    * feat: create view for the pre-launch airdrop page (#251)
    
    * feat: add background img from figma file
    
    * refactor: create prop to make header transparent
    
    * feat: add basic prelaunch view
    
    * improve: add mobile layout to base mockup
    
    * nit: remove placeholder text
    
    * refactor: use a background layer
    
    * feat: add title section to landing page (#265)
    
    * feat: add title card
    
    * fix: update padding to improve 1:1 with figma
    
    * feat: add link text
    
    * feat: add eligible pill
    
    * nit: memoize title section
    
    * feat: add card icon with variable states
    
    * feat: add card skeleton
    
    * nit: add sample airdrop icon
    
    * improve: fix background in place
    
    * feat: add sample cards
    
    * feat: add storybook integration
    
    * fix: add global styling to storybook
    
    * fix: add storybook integration
    
    * fix: update z-index for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
 import { Buffer } from "buffer";
+import { addDecorator } from "@storybook/react";
+import { GlobalStyles } from "components";
+
 window.Buffer = Buffer;
+addDecorator(s => <><GlobalStyles />{s()}</>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
