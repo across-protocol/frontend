a62c9afbc4b51aa030bedf7ad978f7ea8572df42
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-12 18:40:30 -0400

    fix(storybook): narrow scope of preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { Buffer } from "buffer";
 import { addDecorator } from "@storybook/react";
-import { GlobalStyles } from "components";
+import { default as GlobalStyles } from "components/GlobalStyles/GlobalStyles";
 
 window.Buffer = Buffer;
 addDecorator(s => <><GlobalStyles />{s()}</>);
