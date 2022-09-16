04d0a6c1e35e196b93d0340bee51de1ffe56e7d8
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-09-16 15:49:02 +0200

    fixup

--- src/hooks/useIsInCenterOfViewport.ts
+++ src/hooks/useIsInCenterOfViewport.ts
@@ -10,17 +10,9 @@ export function useIsInCenterOfViewport(ref: any) {
         {
           /**
            * This rootMargin creates a horizontal line vertically centered
-           * that will help trigger an intersection at that the very point.
+           * that will help trigger an intersection at that y point.
            */
           rootMargin: "-50% 0% -50% 0%",
-
-          /**
-           * This is the default so you could remove it.
-           * I just wanted to leave it here to make it more explicit
-           * as this threshold is the only one that works with the above
-           * rootMargin
-           */
-          threshold: 0,
         }
       ),
     []
