fdca54061bebbf44fea480047a5ca84a3c419fc9
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-16 15:46:23 -0400

    fix: only allow one component to render at a time
    Thanks to @tulun for this catch!

--- src/views/PreLaunchAirdrop/PreLaunchAirdrop.tsx
+++ src/views/PreLaunchAirdrop/PreLaunchAirdrop.tsx
@@ -5,23 +5,32 @@ import {
   ContentWrapper,
   Wrapper,
 } from "./PreLaunchAirdrop.styles";
-import usePreLaunchAirdrop, { FlowSelector } from "./usePreLaunchAirdrop";
+import usePreLaunchAirdrop from "./usePreLaunchAirdrop";
 import TravellerFlow from "./components/TravellerFlow";
 import { SplashFlow } from "./components/SplashFlow";
 import { EligibilityFlow } from "./components/EligibilityFlow";
 const PreLaunchAirdrop = () => {
   const { activePageFlow } = usePreLaunchAirdrop();
 
-  const pageFlowOptions: Record<FlowSelector, JSX.Element> = {
-    eligibility: <EligibilityFlow />,
-    traveller: <TravellerFlow />,
-    splash: <SplashFlow />,
-  };
+  let activePageComponent: JSX.Element;
+  switch (activePageFlow) {
+    case "eligibility":
+      activePageComponent = <EligibilityFlow />;
+      break;
+    case "traveller":
+      activePageComponent = <TravellerFlow />;
+      break;
+    case "splash":
+      activePageComponent = <SplashFlow />;
+      break;
+    default:
+      activePageComponent = <></>;
+  }
 
   return (
     <Wrapper>
       <BackgroundLayer />
-      <ContentWrapper>{pageFlowOptions[activePageFlow]}</ContentWrapper>
+      <ContentWrapper>{activePageComponent}</ContentWrapper>
       <Footer />
     </Wrapper>
   );
