db7450de0dcf5a6beeafab8ebef2fa9269492678
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-20 17:51:13 -0400

    improve: change style of discord hook

--- src/hooks/useDiscord.ts
+++ src/hooks/useDiscord.ts
@@ -1,8 +1,35 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { discordClientId } from "utils";
+import { useQueryParams } from "./useQueryParams";
+
+const STORAGE_KEY = "discord_JWT";
 
 export function useDiscord() {
+  const params = useQueryParams();
+
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [discordJWT, setDiscordJWT] = useState<string | undefined>(undefined);
+  const [hasOAuthToken, setHasOAuthToken] = useState<boolean | undefined>(
+    undefined
+  );
+  const [discordOAuthToken, setDiscordOAuthToken] = useState<
+    string | undefined
+  >(undefined);
+
+  const authenticate = useCallback((jwt: string) => {
+    setDiscordJWT(jwt);
+    setIsAuthenticated(true);
+    window.localStorage.setItem(STORAGE_KEY, jwt);
+  }, []);
+
+  const unauthenticate = useCallback(() => {
+    setDiscordJWT(undefined);
+    setIsAuthenticated(false);
+    window.localStorage.removeItem(STORAGE_KEY);
+  }, []);
+
   const redirectToAuth = useCallback(() => {
+    unauthenticate();
     const queryString = new URLSearchParams({
       client_id: discordClientId,
       redirect_uri: `${window.location.origin}/auth/discord`,
@@ -11,9 +38,23 @@ export function useDiscord() {
     }).toString();
     const url = `https://discord.com/api/oauth2/authorize?${queryString}`;
     window.location.replace(url);
-  }, []);
+  }, [unauthenticate]);
+
+  useEffect(() => {
+    const storedJWT = window.localStorage.getItem(STORAGE_KEY);
+    setIsAuthenticated(Boolean(storedJWT));
+    setDiscordJWT((jwt) => (jwt ? jwt : storedJWT) ?? undefined);
+    setDiscordOAuthToken(params["code"] || undefined);
+    setHasOAuthToken(typeof params["code"] !== "undefined");
+  }, [params]);
 
   return {
     redirectToAuth,
+    authenticate,
+    unauthenticate,
+    isAuthenticated,
+    discordJWT,
+    discordOAuthToken,
+    hasOAuthToken,
   };
 }
