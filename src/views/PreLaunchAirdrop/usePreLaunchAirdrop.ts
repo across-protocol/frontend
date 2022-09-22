456c887899e32072129470734bcaddcf9c73440b
James Morris <jaamorris@cs.stonybrook.edu>
2022-09-22 19:28:57 -0400

    improve: add new exports to use in plaap

--- src/views/PreLaunchAirdrop/usePreLaunchAirdrop.ts
+++ src/views/PreLaunchAirdrop/usePreLaunchAirdrop.ts
@@ -1,9 +1,12 @@
+import { useOnboard } from "hooks/useOnboard";
 import { useState } from "react";
 
 export type FlowSelector = "splash" | "traveller" | "info";
 
 export default function usePreLaunchAirdrop() {
   const [activePageFlow, setActivePageFlow] = useState<FlowSelector>("splash");
+
+  const { isConnected, connect, account } = useOnboard();
 
   return {
     activePageFlow,
@@ -12,5 +15,11 @@ export default function usePreLaunchAirdrop() {
     switchToSplash: () => setActivePageFlow("splash"),
     switchToTraveller: () => setActivePageFlow("traveller"),
     switchToInfo: () => setActivePageFlow("info"),
+
+    isConnected,
+    connectWalletHandler: () => connect(),
+    account,
+
+    linkWalletHandler: async () => {},
   };
 }
