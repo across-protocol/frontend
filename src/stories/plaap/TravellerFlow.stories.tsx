654deda87a2b9e4c5ae921f847a9e24b73d8f677
Tulun <jaykiraly@gmail.com>
2022-09-23 16:00:38 -0700

    argTypes
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/stories/plaap/TravellerFlow.stories.tsx
+++ src/stories/plaap/TravellerFlow.stories.tsx
@@ -9,7 +9,14 @@ export default {
   title: "TravellerFlow",
   component: TravellerFlow,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {},
+  argTypes: {
+    account: {
+      type: "string",
+    },
+    setActivePageFlow: {
+      type: "function",
+    },
+  },
 } as ComponentMeta<typeof TravellerFlow>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
