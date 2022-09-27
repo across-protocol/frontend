ae7c5a6557f6006d41817d5555ad7027760b5b8e
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-09-26 20:49:50 -0400

    fix(plaap): provides a set of bug fixes to plaap ui flow (#301)
    * fix: :ambulance: isolate mocking code into a separate axios instance
    
    Without calling `axios.create()` the `MockAdapter` will update the default behavior of axios. This will cause all axios calls globally to run through the mock router. This change allows the mocked utility to be scoped only to the axios client on this mock adapter
    
    * feat: :sparkles: add constant to determine if code is in storybook
    
    This update adds a constant variable that detects whether or not this code is being run inside a storybook instance
    
    * fix: :bug: disable onboard useEffect hook when inside storybook
    
    Storybook is faiing to unsubscribe from the onboard useEffect block. This change detects if the code is running within storybook and proactively disables it

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -739,3 +739,5 @@ export const QUERIESV2 = {
   sm: breakpoint(576),
   tb: breakpoint(1024),
 };
+
+export const insideStorybookRuntime = Boolean(process.env.STORYBOOK);
--- src/utils/serverless-api/mock-adapter.ts
+++ src/utils/serverless-api/mock-adapter.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 
+const isolatedAxiosInstance = axios.create();
+
 // This sets the mock adapter on the default instance
-export { axios };
-export default new MockAdapter(axios);
+export { isolatedAxiosInstance as axios };
+export default new MockAdapter(isolatedAxiosInstance);
