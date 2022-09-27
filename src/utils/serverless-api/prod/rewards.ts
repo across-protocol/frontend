46ea54cd6f56cb56ad09fa5eaa39fa8cc0a47f4f
Tulun <jaykiraly@gmail.com>
2022-09-27 14:15:04 -0700

    fix: async / await; simplify fake jwt; log error.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/utils/serverless-api/prod/rewards.ts
+++ src/utils/serverless-api/prod/rewards.ts
@@ -2,17 +2,23 @@ import axios from "axios";
 import { RewardsApiInterface } from "../types";
 import { rewardsApiUrl } from "utils";
 
-export default function rewardsApiCall(
+export default async function rewardsApiCall(
   address: string,
   jwt?: string
 ): Promise<RewardsApiInterface> {
-  return axios
-    .get(`${rewardsApiUrl}/airdrop/rewards?address=${address}`, {
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-    .then((res) => {
-      return res.data as RewardsApiInterface;
-    });
+  try {
+    const response = await axios.get(
+      `${rewardsApiUrl}/airdrop/rewards?address=${address}`,
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return {} as RewardsApiInterface;
+  }
 }
--- src/views/PreLaunchAirdrop/api/getPrelaunchRewards.ts
+++ src/views/PreLaunchAirdrop/api/getPrelaunchRewards.ts
@@ -1,8 +1,8 @@
 import getApiEndpoint from "utils/serverless-api";
 import { mockServerlessAPI } from "utils";
 
-const TEST_JWT =
-  "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..IFJ1oL_VAa-nnhEX.HmM1L9g8uBlrSggZeQr8f-cpIslh8rTnVe1HVXxf0Kgkm6GO-TmdDxokln4vG1udByCTgTAOvTAWVDcvyJy4L_tTxSLwhoaEBIYnN_G3lZXg4JYen7__7qqdI2oZOLTJ0iSVGoL4Ro_TvWe_ZPLTLugx-Figkr20pabxUorw7yw8mqWCLDuWR56ZpH94RUGN5CjRTs0z1FXPac0TKo_Q-gtLcmnpwj383A.ZYkHwm56_n5GHvB-7y6Tag";
+const TEST_JWT = "eyTESTING123";
+
 export default async function getPrelaunchRewards(
   address: string,
   jwt?: string
