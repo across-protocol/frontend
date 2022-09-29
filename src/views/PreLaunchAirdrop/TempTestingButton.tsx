748ef6460ad199beeecdaa9e4949e1b181b46d79
Tulun <jaykiraly@gmail.com>
2022-09-29 10:41:36 -0700

    min form
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/views/PreLaunchAirdrop/TempTestingButton.tsx
+++ src/views/PreLaunchAirdrop/TempTestingButton.tsx
@@ -1,28 +1,35 @@
 import { ButtonV2 } from "components";
 import axios from "axios";
 import { rewardsApiUrl } from "utils";
+import { useState } from "react";
 
 // This resets the dev wallet to ineligible state.
 const TempTestingButton = () => {
+  const [value, setValue] = useState("");
   return (
-    <ButtonV2
-      style={{ width: "200px", margin: "0 auto" }}
-      size="md"
-      onClick={() => {
-        axios
-          .patch(`${rewardsApiUrl}/airdrop/rewards/wallet-rewards`, {
-            walletAddress: "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D",
-            earlyUserRewards: "0",
-            liquidityProviderRewards: "0",
-            welcomeTravellerRewards: "0",
-          })
-          .then((res) => {
-            console.log("success?", res);
-          });
-      }}
-    >
-      Reset Dev Wallet (testing)
-    </ButtonV2>
+    <div style={{ width: "200px", margin: "0 auto" }}>
+      <label>Address</label>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <ButtonV2
+        style={{ width: "200px", margin: "0 auto" }}
+        size="md"
+        onClick={() => {
+          axios
+            .patch(`${rewardsApiUrl}/airdrop/rewards/wallet-rewards`, {
+              walletAddress: value,
+              earlyUserRewards: "0",
+              liquidityProviderRewards: "0",
+              welcomeTravellerRewards: "0",
+            })
+            .then((res) => {
+              console.log("success?", res);
+              setValue("");
+            });
+        }}
+      >
+        Reset Dev Wallet (testing)
+      </ButtonV2>
+    </div>
   );
 };
 
