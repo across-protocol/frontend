8b4050a7c586be05ef8f3d13c0629068f3db1dff
Tulun <jaykiraly@gmail.com>
2022-10-03 11:12:14 -0700

    first chunk of format tests.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/utils/tests/format.test.ts
+++ src/utils/tests/format.test.ts
@@ -1,5 +1,12 @@
-import { isValidString, shortenAddress } from "../format";
-
+import {
+  isValidString,
+  shortenAddress,
+  shortenString,
+  shortenTransactionHash,
+} from "../format";
+const VALID_ADDRESS = "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828";
+const TX_HASH =
+  "0xe5a0c976ca4d09ce6ea034101e78b1a6a9536940cb8f246e7b54d1fe16b8c125";
 describe("#format functions", () => {
   it("isValidString returns true when string is passed in", () => {
     expect(isValidString("xasdfas")).toBeTruthy();
@@ -10,7 +17,27 @@ describe("#format functions", () => {
     expect(isValidString(undefined)).toBeFalsy();
   });
 
-  it("shortAddress should return empty string when empty string is passed in", () => {
+  // TODO: shortenAddress should probably be refactored to check for
+  // address validity. Will adjust when deal with jest import issues.
+  it("shortenAddress should return empty string when empty string is passed in", () => {
     expect(shortenAddress("", "....", 4)).toEqual("");
+  });
+
+  it("shortenAddress should shorten string when a valid address is passed in", () => {
+    expect(shortenAddress(VALID_ADDRESS, "....", 4)).toEqual("0x04....F828");
+    expect(shortenAddress(VALID_ADDRESS, "...", 3)).toEqual("0x0...828");
+  });
+
+  it("shortenString should return original string if string length is less than 2 * numChars.", () => {
+    expect(shortenString("x", "---", 2)).toEqual("x");
+    expect(shortenString("yyyy", "---", 3)).toEqual("yyyy");
+  });
+
+  it("shortenString should return a shortened string if str.length is greater than 2 * numChars", () => {
+    expect(shortenString("xxxxyyyy", "--", 2)).toEqual("xx--yy");
+  });
+
+  it("shortenTransactionHash should shorten to first 5 chars + ...", () => {
+    expect(shortenTransactionHash(TX_HASH)).toEqual("0xe5a...");
   });
 });
