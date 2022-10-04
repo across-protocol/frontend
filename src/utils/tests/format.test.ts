bfaab7e192559bd149e127d74d646dcbd39026ea
Tulun <jaykiraly@gmail.com>
2022-10-04 09:29:18 -0700

    move to describe blocks
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/utils/tests/format.test.ts
+++ src/utils/tests/format.test.ts
@@ -8,7 +8,8 @@ import {
 const VALID_ADDRESS = "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828";
 const TX_HASH =
   "0xe5a0c976ca4d09ce6ea034101e78b1a6a9536940cb8f246e7b54d1fe16b8c125";
-describe("#format functions", () => {
+
+describe("#isValidString", () => {
   it("isValidString returns true when string is passed in", () => {
     expect(isValidString("xasdfas")).toBeTruthy();
   });
@@ -17,7 +18,9 @@ describe("#format functions", () => {
     expect(isValidString(null)).toBeFalsy();
     expect(isValidString(undefined)).toBeFalsy();
   });
+});
 
+describe("#shortenAddress", () => {
   // TODO: shortenAddress should probably be refactored to check for
   // address validity. Will adjust when deal with jest import issues.
   it("shortenAddress should return empty string when empty string is passed in", () => {
@@ -28,7 +31,9 @@ describe("#format functions", () => {
     expect(shortenAddress(VALID_ADDRESS, "....", 4)).toEqual("0x04....F828");
     expect(shortenAddress(VALID_ADDRESS, "...", 3)).toEqual("0x0...828");
   });
+});
 
+describe("#shortenString", () => {
   it("shortenString should return original string if string length is less than 2 * numChars.", () => {
     expect(shortenString("x", "---", 2)).toEqual("x");
     expect(shortenString("yyyy", "---", 3)).toEqual("yyyy");
@@ -37,11 +42,15 @@ describe("#format functions", () => {
   it("shortenString should return a shortened string if str.length is greater than 2 * numChars", () => {
     expect(shortenString("xxxxyyyy", "--", 2)).toEqual("xx--yy");
   });
+});
 
+describe("#shortenTransactionHash", () => {
   it("shortenTransactionHash should shorten to first 5 chars + ...", () => {
     expect(shortenTransactionHash(TX_HASH)).toEqual("0xe5a...");
   });
+});
 
+describe("#smallNumberFormatter", () => {
   it("smallNumberFormatter should show at least 1 digit for numbers < 1 and round up to 3 sig digs", () => {
     expect(smallNumberFormatter(0.01235)).toEqual("0.0124");
   });
