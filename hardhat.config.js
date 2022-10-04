106e4e5183fff5ed9f668456013ac428a67f7624
Tulun <jaykiraly@gmail.com>
2022-10-04 14:56:17 -0700

    toWeiSafe works
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+const mainnetFork = {
+  url: `${process.env.JSON_RPC_PROVIDER}`,
+  blockNumber: 13582625,
+};
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1,
+      forking: mainnetFork,
+      accounts: {
+        count: 2,
+      },
+    },
+  },
+};
