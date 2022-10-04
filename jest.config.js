e65f56c5323996afe8c17faf5ad374f7c5ba04a6
Jason Kiraly <jaykiraly@gmail.com>
2022-10-04 09:59:12 -0700

    feat: Add Stories for widgets. (#342)
    * test: check to ensure that airdrop is transparent
    
    * Format tests
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * first chunk of format tests.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * Modal story.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * Better Modal Book
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * Alert story.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * bouncing loader.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * dot stepper
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * remove temp
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * move to describe blocks
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * remove extraneous comments
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    Co-authored-by: James Morris <jaamorris@cs.stonybrook.edu>

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
