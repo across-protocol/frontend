be5551be15a091edeeeb9bc21ed3bcf3ff56223c
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-10-06 12:08:27 -0400

    fix: update text (#348)

--- src/views/PreLaunchAirdrop/components/MoreInfoFlow/MoreInfoFlow.tsx
+++ src/views/PreLaunchAirdrop/components/MoreInfoFlow/MoreInfoFlow.tsx
@@ -52,15 +52,14 @@ export function MoreInfoFlow({ onClickBack }: Props) {
             hideBoxShadow
           >
             <CardTextDescription>
-              Active bridge users who have not used Across prior to the Across
-              Referral Program (July 18, 2022) identified by the Bridge Traveler
-              program. These users need to complete a bridge transfer on Across
-              ahead of the ACX token launch to become eligible. The amount of
-              ACX initially committed to this program is 10MM, but this amount
-              will double to 20MM if a participation rate of 30% or more is
-              achieved. Users who complete a bridge transfer will share these
-              tokens with some allocation variability depending on past bridge
-              activity.
+              Active bridge users identified by the Bridge Traveler program who
+              have not used Across prior to September 1, 2022. These users need
+              to complete a bridge transfer on Across ahead of the ACX token
+              launch to become eligible. The amount of ACX initially committed
+              to this program is 10MM, but this amount will double to 20MM if a
+              participation rate of 30% or more is achieved. Users who complete
+              a bridge transfer will share these tokens with some allocation
+              variability depending on past bridge activity.
             </CardTextDescription>
           </AirdropCard>,
           <AirdropCard
