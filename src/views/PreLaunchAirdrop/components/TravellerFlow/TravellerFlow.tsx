95f0fccb0a7b3f1b8e7fb09f66f4934d03e9ca51
James Morris <96435344+james-a-morris@users.noreply.github.com>
2022-10-10 12:14:38 -0400

    fix: allow about link to open in new tab (#354)
    * fix: have about link target its own tab
    
    * fix: remove unneeded code

--- src/views/PreLaunchAirdrop/components/TravellerFlow/TravellerFlow.tsx
+++ src/views/PreLaunchAirdrop/components/TravellerFlow/TravellerFlow.tsx
@@ -16,7 +16,6 @@ import {
 import { ButtonV2 } from "components/Buttons/ButtonV2";
 import useTravellerFlow from "./useTravellerFlow";
 import { setAccountSeenWelcomeTravellerFlow } from "utils/localStorage";
-import { Link } from "react-router-dom";
 
 const TRAVELLER_FLOW_DATA = [
   {
@@ -39,7 +38,11 @@ const TRAVELLER_FLOW_DATA = [
         >
           UMA's Optimistic Oracle
         </a>
-        . Learn more about Across <Link to="/about">here</Link>.
+        . Learn more about Across{" "}
+        <a href="/about" target="_blank" rel="noreferrer">
+          here
+        </a>
+        .
       </>
     ),
   },
