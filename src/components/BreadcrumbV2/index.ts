3fc081f0e2087476c11863d08881dfe4248fce8f
James Morris <jaamorris@cs.stonybrook.edu>
2022-10-12 12:05:16 -0400

    feat: add breadcrumb component

---
+++ src/components/BreadcrumbV2/index.ts
@@ -0,0 +1,1 @@
+export { default } from "./BreadcrumbV2";
---
+++ src/components/BreadcrumbV2/useBreadcrumb.ts
@@ -0,0 +1,23 @@
+import { useLocation } from "react-router";
+
+export function useBreadcrumb() {
+  const location = useLocation();
+  const routes = location.pathname
+    .split("/")
+    .filter((r) => r)
+    .reduce((prev, curr) => {
+      prev.push({ path: `${prev[prev.length - 1] ?? ""}/${curr}`, name: curr });
+      return prev;
+    }, [] as { path: string; name: string }[]);
+  if (routes.length === 0) {
+    routes.push({ path: "/", name: "Home" });
+  }
+  const ancestorRoutes = routes.length > 0 ? routes.slice(0, -1) : [];
+  const currentRoute = routes[routes.length - 1];
+
+  return {
+    routes,
+    ancestorRoutes,
+    currentRoute,
+  };
+}
