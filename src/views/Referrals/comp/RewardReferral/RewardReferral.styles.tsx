739efb0d776feb8e85cd38787190133b1a8ccbfb
James Morris <jaamorris@cs.stonybrook.edu>
2022-10-18 15:49:09 -0400

    improve: update layout of referral page

--- src/views/Referrals/comp/RewardReferral/RewardReferral.styles.tsx
+++ src/views/Referrals/comp/RewardReferral/RewardReferral.styles.tsx
@@ -1,18 +1,15 @@
 import styled from "@emotion/styled";
-import { ButtonV2 } from "components/Buttons";
+import { ButtonV2, SecondaryButtonV2 } from "components/Buttons";
 import { ReactComponent as ReferralSVG } from "assets/icons/rewards/referral-within-star.svg";
-import { ReactComponent as CopyIcon16 } from "assets/icons/copy-16.svg";
-import { ReactComponent as CopyIcon24 } from "assets/icons/copy-24.svg";
 import { ReactComponent as II } from "assets/icons/info-16.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { QUERIESV2 } from "utils";
+import { Text } from "components/Text";
 
 export const Wrapper = styled.div`
   display: flex;
   gap: 24px 24px;
 
-  @media screen and (max-width: 1024px) {
+  @media ${QUERIESV2.tb.andDown} {
     flex-direction: column;
   }
 `;
@@ -33,10 +30,6 @@ export const ReferralLinkBlock = styled(ReferralRowBlock)`
   overflow: clip;
   flex: 0 0 340px;
 
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
   @media ${QUERIESV2.tb.andDown} {
     flex: 0;
     flex-grow: 0;
@@ -44,7 +37,6 @@ export const ReferralLinkBlock = styled(ReferralRowBlock)`
 `;
 
 export const InnerReferralLinkBlock = styled.div`
-  flex: 0 0 340px;
   padding: 32px 24px;
   display: flex;
   flex-direction: column;
@@ -52,14 +44,16 @@ export const InnerReferralLinkBlock = styled.div`
   align-items: center;
   z-index: 1;
 
-  @media screen and (max-width: 1024px) {
+  height: 100%;
+
+  @media ${QUERIESV2.tb.andDown} {
     width: 100%;
     padding: 48px 24px 24px;
     border: none;
     background-color: transparent;
   }
 
-  @media screen and (max-width: 428px) {
+  @media ${QUERIESV2.sm.andDown} {
     padding: 0 16px;
   }
 `;
@@ -86,8 +80,15 @@ export const ReferralTierBlock = styled(ReferralRowBlock)`
   padding: ${24 / 16}rem;
   flex-grow: 2;
 
-  @media screen and (max-width: 428px) {
+  overflow: clip;
+
+  display: flex;
+  flex-direction: column;
+  gap: ${24 / 16}rem;
+
+  @media ${QUERIESV2.sm.andDown} {
     padding: ${16 / 16}rem;
+    gap: ${16 / 16}rem;
   }
 `;
 
@@ -99,7 +100,7 @@ export const Header = styled.h2`
   color: #e0f3ff;
   text-align: center;
 
-  @media screen and (max-width: 428px) {
+  @media ${QUERIESV2.sm.andDown} {
     font-size: ${22 / 16}rem;
     line-height: ${26 / 16}rem;
   }
@@ -112,9 +113,35 @@ export const SubHeader = styled.h3`
   font-weight: 400;
   color: #c5d5e0;
 
-  @media screen and (max-width: 428px) {
+  @media ${QUERIESV2.sm.andDown} {
     font-size: ${14 / 16}rem;
     line-height: ${18 / 16}rem;
+  }
+`;
+
+export const TierButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+  gap: 8px;
+`;
+
+export const ClaimButton = styled(SecondaryButtonV2)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  & p {
+    font-weight: 500;
+    color: #f9d26c;
+  }
+  border-color: #f9d26c;
+
+  @media ${QUERIESV2.sm.andDown} {
+    padding: 8px 16px;
   }
 `;
 
@@ -124,134 +151,34 @@ export const TierSmHeader = styled.h4`
   font-weight: 400;
   color: #9daab2;
 
-  @media screen and (max-width: 428px) {
+  @media ${QUERIESV2.sm.andDown} {
     font-size: ${14 / 16}rem;
     line-height: ${18 / 16}rem;
   }
 `;
 
 export const TierHeader = styled.div`
-  margin: 4px 0 0;
   color: #e0f3ff;
   font-size: ${26 / 16}rem;
   line-height: ${32 / 16}rem;
   font-weight: 400;
 
-  @media screen and (max-width: 428px) {
+  @media ${QUERIESV2.sm.andDown} {
     font-size: ${22 / 16}rem;
     line-height: ${26 / 16}rem;
   }
 `;
 
-export const ReferralUrl = styled.button`
-  margin: 0 0 8px;
-  width: 100%;
+export const TierWrapper = styled.div`
   display: flex;
-  justify-content: inherit;
-  align-items: center;
-  padding: ${18 / 16}rem ${18 / 16}rem;
-  font-size: ${18 / 16}rem;
-  line-height: ${28 / 16}rem;
-  font-weight: 400;
-  text-align: center;
-  color: #9daab2;
-  background-color: #2d2e33;
-  border: 1px solid #4c4e57;
-  border-radius: 32px;
-  outline: none;
-  cursor: pointer;
-
-  @media screen and (max-width: 1024px) {
-    padding: ${18 / 16}rem ${24 / 16}rem;
-    max-width: fit-content;
-    margin: 0;
-  }
-
-  @media screen and (max-width: 428px) {
-    padding: ${10 / 16}rem ${16 / 16}rem;
-    font-size: ${16 / 16}rem;
-    line-height: ${20 / 16}rem;
-  }
-
-  :hover {
-    border: 1px solid #e0f3ff;
-  }
+  flex-direction: column;
+  gap: 8px;
 `;
 
 export const StyledReferralLogo = styled(ReferralSVG)`
   margin: 0 auto;
   height: 64px;
   width: 64px;
-`;
-
-export const TierInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 0 0;
-  border-top: 1px solid #3e4047;
-  flex-wrap: wrap;
-
-  @media (max-width: 568px) {
-    flex-direction: column;
-  }
-
-  @media (max-width: 428px) {
-    padding: 4px 0 0;
-  }
-`;
-
-export const TierInfoItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding-top: 12px;
-  width: 40%;
-  color: #9daab2;
-  font-size: ${16 / 16}rem;
-  line-height: ${20 / 16}rem;
-  font-weight: 400;
-
-  &:nth-of-type(2n) {
-    width: 60%;
-    justify-content: flex-end;
-    color: #e0f3ff;
-  }
-
-  @media (max-width: 568px) {
-    width: 100% !important;
-
-    &:nth-of-type(2n) {
-      flex-direction: row-reverse;
-      padding-top: 4px;
-    }
-
-    &:nth-of-type(2n + 1) {
-      padding-top: 16px;
-    }
-  }
-
-  @media (max-width: 428px) {
-    font-size: ${14 / 16}rem;
-    line-height: ${18 / 16}rem;
-  }
-`;
-
-export const LightGrayItemText = styled.span<{ margin?: number }>`
-  color: #9daab2;
-  margin: ${({ margin }) => (margin ? `0 ${margin}px` : 0)};
-`;
-
-export const WarningInfoItem = styled(TierInfoItem)`
-  color: #f9d26c !important;
-`;
-
-export const RewardsInfo = styled.span`
-  display: flex;
-  align-items: center;
-  margin: 0 12px;
-
-  svg path {
-    stroke: #f9d26c;
-  }
 `;
 
 export const ReferralLinkButtonsRow = styled.div`
@@ -261,11 +188,11 @@ export const ReferralLinkButtonsRow = styled.div`
   flex-wrap: wrap;
   gap: 16px 24px;
 
-  @media screen and (max-width: 1024px) {
+  @media ${QUERIESV2.tb.andDown} {
     margin-top: ${32 / 16}rem;
   }
 
-  @media screen and (max-width: 428px) {
+  @media ${QUERIESV2.sm.andDown} {
     flex-direction: column;
     align-items: center;
     margin-top: ${24 / 16}rem;
@@ -273,101 +200,114 @@ export const ReferralLinkButtonsRow = styled.div`
 `;
 
 export const ConnectButton = styled(ButtonV2)`
-  @media screen and (max-width: 428px) {
+  @media ${QUERIESV2.sm.andDown} {
     font-size: 14px;
     line-height: 18px;
     padding: 11px 16px;
   }
 `;
 
-export const CopyIconDesktop = styled(CopyIcon24)`
-  display: block;
-  margin-left: 8px;
+export const InfoIcon = styled(II)`
+  cursor: pointer;
+`;
 
-  path {
-    stroke: var(--color-primary);
+export const ArrowSeparator = styled.span`
+  margin-top: -4px;
+  color: #9daab2;
+  flex-shrink: 0;
+`;
+
+export const StatsInfoSegment = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 16px;
+
+  overflow: clip;
+
+  @media ${QUERIESV2.sm.andDown} {
+    margin: -16px 0;
+    gap: 0px;
   }
+`;
 
-  @media (max-width: 1024px) {
-    margin-left: 16px;
+export const StatsInfoRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 0px;
+
+  width: 100%;
+
+  @media ${QUERIESV2.sm.andDown} {
+    padding: 16px 0;
   }
+`;
 
-  @media (max-width: 428px) {
+export const StatsTitleIconTooltipWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 6px;
+`;
+
+export const StatsTitleIconWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 12px;
+
+  & svg {
+    @media ${QUERIESV2.sm.andDown} {
+      height: ${16 / 16}rem;
+      width: ${16 / 16}rem;
+    }
+  }
+`;
+
+export const StatsValueWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 8px;
+
+  height: 100%;
+`;
+
+export const StatsGrayTextDesktop = styled(Text)`
+  color: #9daab2;
+  display: inline-block;
+  @media ${QUERIESV2.sm.andDown} {
     display: none;
   }
 `;
 
-export const CopyIconMobile = styled(CopyIcon16)`
-  display: none;
-  margin-left: 16px;
-
-  path {
-    stroke: var(--color-primary);
-  }
-
-  @media (max-width: 428px) {
-    display: block;
-  }
-`;
-
-const CM = styled(FontAwesomeIcon)`
-  margin-left: 8px;
-
-  @media (max-width: 1024px) {
-    margin-left: 16px;
-  }
-
-  path {
-    fill: var(--color-primary);
-  }
-`;
-
-export const CopyCheckmark = () => <CM icon={faCheckCircle} />;
-
-export const InfoIcon = styled(II)`
-  margin-left: 8px;
-  cursor: pointer;
-`;
-
-export const InlineTooltipWrapper = styled.div`
-  display: inline-block;
-`;
-
-export const ExternalLink = styled.a`
-  display: flex;
-  align-items: center;
-  font-size: ${16 / 16}rem;
-  line-height: ${20 / 16}rem;
-  font-weight: 500;
-  text-decoration: none;
-  color: #e0f3ff;
-  transition: opacity 0.1s;
-  cursor: pointer;
-
-  svg {
-    path {
-      fill: #e0f3ff;
-    }
-  }
-
-  &:hover {
-    opacity: 0.8;
-  }
-
-  @media (max-width: 428px) {
-    font-size: ${14 / 16}rem;
-    line-height: ${18 / 16}rem;
-  }
-`;
-
-export const ArrowSeparator = styled.span`
-  line-height: 8px;
-  margin: 4px 8px 0;
+export const StatsGrayTextMobile = styled(StatsGrayTextDesktop)`
   color: #9daab2;
-  transform: rotate(180deg);
+  display: none;
+  @media ${QUERIESV2.sm.andDown} {
+    display: inline-block;
+  }
+`;
 
-  @media (max-width: 568px) {
-    margin: 0 8px 0;
-    transform: rotate(0);
+export const StatsWhiteText = styled(Text)`
+  color: #e0f3ff;
+`;
+
+export const MobileDivider = styled.div`
+  width: 200%;
+  margin-left: -50%;
+  height: 1px;
+  background: #3e4047;
+
+  display: none;
+  @media ${QUERIESV2.sm.andDown} {
+    display: block;
   }
 `;
--- src/views/Referrals/comp/RewardReferral/RewardReferral.tsx
+++ src/views/Referrals/comp/RewardReferral/RewardReferral.tsx
@@ -1,5 +1,4 @@
-import { useMemo } from "react";
-import { ethers } from "ethers";
+import React, { useMemo } from "react";
 import CopyReferralLink from "components/CopyReferralLink";
 import {
   Wrapper,
@@ -11,17 +10,24 @@ import {
   ReferralTierBlock,
   TierSmHeader,
   TierHeader,
-  TierInfo,
-  TierInfoItem,
-  LightGrayItemText,
-  WarningInfoItem,
   ConnectButton,
   InfoIcon,
-  ArrowSeparator,
-  RewardsInfo,
   ReferralLinkBlockBannerWrapper,
   InnerReferralLinkBlock,
   StyledReferralCopyLink,
+  TierWrapper,
+  StatsInfoSegment,
+  StatsInfoRow,
+  StatsTitleIconTooltipWrapper,
+  StatsTitleIconWrapper,
+  StatsGrayTextDesktop,
+  StatsGrayTextMobile,
+  StatsValueWrapper,
+  ArrowSeparator,
+  StatsWhiteText,
+  MobileDivider,
+  TierButtonWrapper,
+  ClaimButton,
 } from "./RewardReferral.styles";
 
 import { ReactComponent as PurpleBanner } from "assets/bg-banners/purple-card-banner.svg";
@@ -30,7 +36,15 @@ import { PopperTooltip } from "components/Tooltip";
 import { ExternalLink } from "components/ExternalLink";
 import StepperWithTooltips from "../StepperWithTooltips";
 import { useConnection } from "hooks";
+import { formatEther, formatNumberMaxFracDigits, rewardTiers } from "utils";
 
+import { ReactComponent as WalletIcon } from "assets/icons/wallet-24.svg";
+import { ReactComponent as TransferIcon } from "assets/icons/transfer-24.svg";
+import { ReactComponent as GraphIcon } from "assets/icons/graph-24.svg";
+import { ReactComponent as IncreaseIcon } from "assets/icons/increase-24.svg";
+import { ReactComponent as TrophyIcon } from "assets/icons/trophy-24.svg";
+import { repeatableTernaryBuilder } from "utils/ternary";
+import { Text } from "components/Text";
 interface Props {
   isConnected: boolean;
   referrer: string | undefined;
@@ -116,126 +130,150 @@ const ReferralTierComponent: React.FC<{
 }> = ({ referralsSummary, isConnected }) => {
   const rewardsAmount = useMemo(() => {
     if (referralsSummary.rewardsAmount) {
-      return Number(
-        ethers.utils.formatUnits(referralsSummary.rewardsAmount, 18)
-      ).toFixed(4);
+      return formatEther(referralsSummary.rewardsAmount);
     }
     return 0;
   }, [referralsSummary.rewardsAmount]);
 
+  const nextTier = tiers[referralsSummary.tier + 1];
+
+  const nextTierTernary = repeatableTernaryBuilder<
+    { arrow: boolean; value: string | React.ReactHTMLElement<any> } | undefined
+  >(!!nextTier, undefined);
+
+  const datum = [
+    {
+      Icon: WalletIcon,
+      title: { desktop: "Referee Wallets", mobile: "Wallets" },
+      primaryText: String(referralsSummary.referreeWallets),
+      tooltip: {
+        title: "Referee wallets",
+        description: "Lorem ipsum",
+      },
+    },
+    {
+      Icon: TransferIcon,
+      title: { desktop: "Transfers", mobile: "Transfers" },
+      primaryText: `${referralsSummary.transfers} transfer${
+        referralsSummary.transfers !== 1 ? "s" : ""
+      }`,
+      secondaryText: nextTierTernary({
+        arrow: true,
+        value: `${
+          nextTier.referrals - referralsSummary.referreeWallets
+        } to next tier`,
+      }),
+      tooltip: {
+        title: "Transfers",
+        description: "Lorem ipsum",
+      },
+    },
+    {
+      Icon: GraphIcon,
+      title: { desktop: "Volume from Transfers", mobile: "Volume" },
+      primaryText: `$${formatNumberMaxFracDigits(referralsSummary.volume)}`,
+      secondaryText: nextTierTernary({
+        arrow: true,
+        value: `$${formatNumberMaxFracDigits(
+          nextTier.volume - referralsSummary.volume
+        )} to next tier`,
+      }),
+      tooltip: {
+        title: "Volume from Transfers",
+        description: "Lorem ipsum",
+      },
+    },
+    {
+      Icon: IncreaseIcon,
+      title: { desktop: "Referral Rate", mobile: "Rate" },
+      primaryText: `${Math.floor(
+        referralsSummary.referralRate * 0.75 * 100
+      )}% referral rate`,
+      secondaryText: nextTierTernary({
+        arrow: false,
+        value: `${Math.floor(
+          referralsSummary.referralRate * 0.25 * 100
+        )}% for referee`,
+      }),
+      tooltip: {
+        title: "Referral Rate",
+        description: "Lorem ipsum",
+      },
+    },
+    {
+      Icon: TrophyIcon,
+      title: { desktop: "Total Rewards", mobile: "Rewards" },
+      primaryText: `${rewardsAmount} ACX`,
+      tooltip: {
+        title: "Total Rewards",
+        description: "Lorem ipsum",
+      },
+    },
+  ];
+
   return (
     <ReferralTierBlock>
-      <TierSmHeader>Current referral tier</TierSmHeader>
-      <TierHeader>{tiers[referralsSummary.tier].name}</TierHeader>
+      <TierButtonWrapper>
+        <TierWrapper>
+          <TierHeader>{tiers[referralsSummary.tier].name}</TierHeader>
+          <TierSmHeader>Current referral tier</TierSmHeader>
+        </TierWrapper>
+        {isConnected && (
+          <ClaimButton size="lg">
+            <Text>Claim rewards</Text>
+          </ClaimButton>
+        )}
+      </TierButtonWrapper>
       <StepperWithTooltips
         currentStep={referralsSummary.tier - 1}
         numSteps={5}
-        tooltips={[
-          {
-            title: "Copper tier",
-            titleSecondary: "40% referral rate",
-            body: "Starting tier with no requirements to join.",
-          },
-          {
-            title: "Bronzer tier",
-            titleSecondary: "50% referral rate",
-            body: "Requires over $50,000 of bridge volume or 3 unique referral transfers.",
-          },
-          {
-            title: "Silver tier",
-            titleSecondary: "60% referral rate",
-            body: "Requires over $100,000 of bridge volume or 5 unique referral transfers.",
-          },
-          {
-            title: "Gold tier",
-            titleSecondary: "70% referral rate",
-            body: "Requires over $250,000 of bridge volume or 10 unique referral transfers.",
-          },
-          {
-            title: "Platinum tier",
-            titleSecondary: "80% referral rate",
-            body: "Requires over $500,000 of bridge volume or 20 unique referral transfers.",
-          },
-        ]}
+        tooltips={rewardTiers}
       />
-      <TierInfo>
-        <TierInfoItem>Referee wallets</TierInfoItem>
-        <TierInfoItem>{referralsSummary.activeRefereesCount}</TierInfoItem>
-        <TierInfoItem>
-          Unique referral transfers
-          <PopperTooltip
-            title="Total referee wallets"
-            body="Number of unique wallets that have used your referral link."
-            placement="bottom-start"
-          >
-            <InfoIcon />
-          </PopperTooltip>
-        </TierInfoItem>
-        <TierInfoItem>
-          {referralsSummary.tier < 5 && (
-            <>
-              <LightGrayItemText>
-                {`${
-                  tiers[referralsSummary.tier + 1].referrals -
-                  referralsSummary.referreeWallets
-                } to next tier`}
-              </LightGrayItemText>
-              <ArrowSeparator>&rarr;</ArrowSeparator>
-            </>
-          )}
-          {referralsSummary.referreeWallets}
-        </TierInfoItem>
-        <TierInfoItem>Transfers</TierInfoItem>
-        <TierInfoItem>{`${referralsSummary.transfers}`}</TierInfoItem>
-        <TierInfoItem>Volume from transfers</TierInfoItem>
-        <TierInfoItem>
-          {referralsSummary.tier < 5 && (
-            <>
-              <LightGrayItemText>
-                {`${(
-                  tiers[referralsSummary.tier + 1].volume -
-                  referralsSummary.volume
-                ).toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                })} to next tier`}
-              </LightGrayItemText>
-              <ArrowSeparator>&rarr;</ArrowSeparator>
-            </>
-          )}
-          {referralsSummary.volume.toLocaleString("en-US", {
-            style: "currency",
-            currency: "USD",
-          })}
-        </TierInfoItem>
-        <TierInfoItem>Referral rate</TierInfoItem>
-        <TierInfoItem>
-          <LightGrayItemText margin={8}>{`${
-            referralsSummary.referralRate * 100 * 0.25
-          }% for referee`}</LightGrayItemText>
-          {`${referralsSummary.referralRate * 100 * 0.75}%`}
-        </TierInfoItem>
-        <TierInfoItem>Rewards from transfers</TierInfoItem>
-        {isConnected ? (
+      <MobileDivider />
+      <StatsInfoSegment>
+        {datum.map((stat, ind) => (
           <>
-            <WarningInfoItem>
-              <RewardsInfo>
-                Not claimable yet
-                <PopperTooltip
-                  title={"Not claimable"}
-                  body={"The ACX token is not live yet."}
-                  placement="bottom"
-                >
-                  <InfoIcon />
-                </PopperTooltip>
-              </RewardsInfo>
-              {`~${rewardsAmount} ACX`}
-            </WarningInfoItem>
+            {ind !== 0 && <MobileDivider />}
+            <StatsInfoRow key={stat.title.desktop}>
+              <StatsTitleIconTooltipWrapper>
+                <StatsTitleIconWrapper>
+                  <stat.Icon />
+                  <StatsGrayTextDesktop size="lg">
+                    {stat.title.desktop}
+                  </StatsGrayTextDesktop>
+                  <StatsGrayTextMobile size="lg">
+                    {stat.title.mobile}
+                  </StatsGrayTextMobile>
+                </StatsTitleIconWrapper>
+                {stat.tooltip && (
+                  <PopperTooltip
+                    title={stat.tooltip.title}
+                    body={stat.tooltip.description}
+                    placement="bottom-start"
+                  >
+                    <InfoIcon />
+                  </PopperTooltip>
+                )}
+              </StatsTitleIconTooltipWrapper>
+              <StatsValueWrapper>
+                {stat.secondaryText && (
+                  <>
+                    <StatsGrayTextDesktop size="lg">
+                      {stat.secondaryText.value}
+                    </StatsGrayTextDesktop>
+                    {stat.secondaryText.arrow && (
+                      <ArrowSeparator>
+                        <StatsGrayTextDesktop size="lg">←</StatsGrayTextDesktop>
+                      </ArrowSeparator>
+                    )}
+                  </>
+                )}
+                <StatsWhiteText size="lg">{stat.primaryText}</StatsWhiteText>
+              </StatsValueWrapper>
+            </StatsInfoRow>
           </>
-        ) : (
-          <TierInfoItem>-</TierInfoItem>
-        )}
-      </TierInfo>
+        ))}
+      </StatsInfoSegment>
     </ReferralTierBlock>
   );
 };
