96c88de159cab5817e808baa6496a350eb9782ee
James Morris <jaamorris@cs.stonybrook.edu>
2022-10-19 12:59:04 -0400

    improve: hoist table to component library

--- src/views/Referrals/comp/index.ts
+++ src/views/Referrals/comp/index.ts
@@ -1,6 +1,6 @@
 export { default as RewardBreakdown } from "./RewardBreakdown";
 export { default as RewardHero } from "./RewardHero";
-export { default as RewardTable } from "./RewardTable";
+export { default as RewardTable } from "../../../components/RewardTable";
 export { default as RewardReferral } from "./RewardReferral";
 export { default as ConnectTableOverlay } from "./ConnectTableOverlay";
 export { default as RewardTableWithOverlay } from "./RewardTableWithOverlay";
