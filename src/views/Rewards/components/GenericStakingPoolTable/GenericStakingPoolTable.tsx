2bd6523f9acb6ab4439c871e8b53357597e50461
James Morris <jaamorris@cs.stonybrook.edu>
2022-10-19 18:35:54 -0400

    improve: add formatter

--- src/views/Rewards/components/GenericStakingPoolTable/GenericStakingPoolTable.tsx
+++ src/views/Rewards/components/GenericStakingPoolTable/GenericStakingPoolTable.tsx
@@ -1,10 +1,46 @@
-import styled from "@emotion/styled";
+import styled, { StyledComponent } from "@emotion/styled";
 import RewardTable from "components/RewardTable";
-import createAllPoolsTableJSX from "../PoolTable/createAllPoolsTableJSX";
-import { headers } from "./formatter";
+import { StyledETHIcon } from "components/RewardTable/RewardTables.styles";
+import { BigNumberish } from "ethers";
+import { formatRow, headers } from "./formatter";
 
-const GenericStakingPoolTable = () => {
-  const rows = createAllPoolsTableJSX();
+export type GenericStakingPoolRowData = {
+  logo: StyledComponent<any>;
+  poolName: string;
+  multiplier: number;
+
+  rewardAPY: BigNumberish;
+  baseAPY: BigNumberish;
+  rewards: BigNumberish;
+
+  ageOfCapital: number;
+
+  rewardFormatter: (wei: BigNumberish) => string;
+  lpTokenFormatter: (wei: BigNumberish) => string;
+};
+
+type GenericStakingPoolTableType = {
+  poolData?: GenericStakingPoolRowData[];
+};
+
+const SAMPLE_DATA: GenericStakingPoolRowData[] = [
+  {
+    poolName: "ETH",
+    logo: StyledETHIcon,
+    multiplier: 2,
+    rewardAPY: "2",
+    baseAPY: "2",
+    rewardFormatter: () => "4",
+    lpTokenFormatter: () => "4",
+    rewards: "3",
+    ageOfCapital: 3,
+  },
+];
+
+const GenericStakingPoolTable = ({
+  poolData = SAMPLE_DATA,
+}: GenericStakingPoolTableType) => {
+  const rows = poolData.map((datum) => formatRow(datum));
   return (
     <Wrapper>
       <RewardTable scrollable={true} rows={rows} headers={headers} />
--- src/views/Rewards/components/GenericStakingPoolTable/formatter.tsx
+++ src/views/Rewards/components/GenericStakingPoolTable/formatter.tsx
@@ -1,5 +1,22 @@
 import styled from "@emotion/styled";
 import { BaseHeadCell } from "components/Table";
+import { Text } from "components/Text";
+import { PopperTooltip } from "components/Tooltip";
+import { ReactComponent as II } from "assets/icons/info-16.svg";
+import { GenericStakingPoolRowData } from "./GenericStakingPoolTable";
+import { IRow } from "components/Table/Table";
+
+type RowData = GenericStakingPoolRowData;
+
+const flexBasisLengths = [
+  172, // Pool
+  232, // Staked LP Token
+  168, // Multiplier
+  144, // Reward APY
+  152, // Age of Capital
+  112, // Rewards
+  236, // Button
+];
 
 const rawHeader = [
   "Pool",
@@ -29,41 +46,79 @@ const rawHeader = [
   "",
 ];
 
-export const headers = rawHeader.map((header) => ({
-  value: <BaseHeadCell>HI</BaseHeadCell>,
-}));
-
 const Cell = styled(BaseHeadCell)<{ length: number }>`
   flex: 0 0 ${({ length }) => length}px;
 `;
 
-//   {
-//     value: <AssetHeadCell>Pool</AssetHeadCell>,
-//   },
-//   {
-//     value: "Staked LP Tokens",
-//   },
-//   {
-//     value: (
-//       <>
-//         Multiplier <CircleInfo />
-//       </>
-//     ),
-//   },
-//   {
-//     value: "Reward APY",
-//   },
-//   {
-//     value: (
-//       <>
-//         Age of capital <CircleInfo />
-//       </>
-//     ),
-//   },
-//   {
-//     value: "Rewards",
-//   },
-//   {
-//     value: " ",
-//   },
-// ];
+const InfoIcon = styled(II)`
+  cursor: pointer;
+`;
+
+const HeaderCell = styled(Cell)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 6px;
+`;
+
+const RowCell = styled(Cell)`
+  padding: 20px 16px;
+  background: transparent;
+
+  border-top: 1px solid #3e4047;
+`;
+
+const PoolCell = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 24px;
+`;
+
+export const headers = rawHeader.map((header, idx) => {
+  const text = typeof header === "string" ? header : header.header;
+  const toolTip =
+    typeof header === "string" ? undefined : (
+      <PopperTooltip
+        title={header.tooltip.title}
+        body={header.tooltip.description}
+        placement="bottom-start"
+      >
+        <InfoIcon />
+      </PopperTooltip>
+    );
+
+  return {
+    value: (
+      <HeaderCell length={flexBasisLengths[idx]}>
+        <Text color="white-70" size="lg">
+          {text}
+        </Text>
+        {toolTip}
+      </HeaderCell>
+    ),
+  };
+});
+
+function formatPoolCell(data: RowData) {
+  return (
+    <PoolCell>
+      <data.logo />
+      <Text size="lg" color="white-100">
+        {data.poolName.toUpperCase()}
+      </Text>
+    </PoolCell>
+  );
+}
+
+export function formatRow(data: RowData): IRow {
+  const formatterFns = [formatPoolCell];
+
+  return {
+    cells: formatterFns.map((fn, idx) => ({
+      value: <RowCell length={flexBasisLengths[idx]}>{fn(data)}</RowCell>,
+    })),
+  };
+}
