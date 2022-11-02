508907d4b9dbe8d9d14ee3e9266fa039a0a8811b
James Morris <jaamorris@cs.stonybrook.edu>
2022-11-02 09:48:34 -0400

    docs: add comments

--- src/utils/rewards.ts
+++ src/utils/rewards.ts
@@ -7,8 +7,8 @@ import { safeDivide } from "utils/math";
 import { BigNumber } from "ethers";
 import { parseEther } from "@ethersproject/units";
 import { StakingPool } from "hooks";
-import _ from "lodash";
 import { formattedBigNumberToNumber, parseEtherLike } from "./format";
+import { cloneDeep } from "lodash-es";
 
 export function getBaseRewardsApr(
   baseEmissionRatePerSecond: BigNumber,
@@ -30,26 +30,35 @@ export function getBaseRewardsApr(
   return safeDivide(rewardsPerYear.mul(userStaked), totalStaked.pow(2));
 }
 
+/**
+ * Generates an updated staking value depending on the configuration of an origin StakingPool data resolved from the AcceleratingDistributor contract
+ * @param origin The current configuration of a given staking pool. This data will not be modified
+ * @param lpModification The amount of LP tokens that will either be staked or unstaked
+ * @param modificationType An enumerated value of whether or not the `lpModification` value will be staked or unstaked from the origin
+ * @returns A modified `StakingPool` configuration with an updated APY/Reward configuration depending on the staking action & amount
+ */
 export function deriveNewStakingValues(
   origin: StakingPool,
   lpModification?: BigNumber,
   modificationType?: "stake" | "unstake"
 ): StakingPool | undefined {
+  // If the user does not pass any staking data, return undefined
   if (!lpModification || lpModification.eq(0) || !modificationType) {
     return undefined;
   }
-
-  const updatedPoolData = _.cloneDeep(origin);
+  // Resolve a boolean for if the user is staking or not
   const isStake = modificationType === "stake";
-
-  const currentlyStaked = updatedPoolData.userAmountOfLPStaked.add(
+  // Resolve the new staked amount based on the lpConfiguration/staking Action
+  const currentlyStaked = origin.userAmountOfLPStaked.add(
     lpModification.mul(isStake ? 1 : -1)
   );
+  // Resolve if the user has effectively unstaked under this new configuration
   const noStake = currentlyStaked.eq(0);
-
+  // Update the total amount of time the user has staked weighted on this new
+  // stake/unstake action
   const updatedTimeEstimateInSeconds = noStake
-    ? BigNumber.from(0)
-    : isStake
+    ? BigNumber.from(0) // If the user has no stake, their time is 0
+    : isStake // The user wants to stake
     ? lpModification
         .mul(fixedPointAdjustment)
         .div(currentlyStaked)
@@ -57,17 +66,23 @@ export function deriveNewStakingValues(
         .div(fixedPointAdjustment)
     : BigNumber.from(origin.elapsedTimeSinceAvgDeposit).mul(secondsPerDay); // The user wishes to unstake - the elapsed time does not change
 
+  // Resolve the total amount of time in days that the user would have now staked
   const updatedTimeEstimate = updatedTimeEstimateInSeconds
     .div(secondsPerDay)
     .toNumber();
 
+  // Generate a fractional amount of how much time the user has staked with respect
+  // to the total number of seconds that a stake needs to exist to claim the maximum
+  // reward multiplier
   let fractionOfMaxMultiplier = updatedTimeEstimateInSeconds
     .mul(fixedPointAdjustment)
     .div(origin.secondsToMaxMultiplier);
+  // Cap the fractional percent at 1.0
   fractionOfMaxMultiplier = fractionOfMaxMultiplier.gt(fixedPointAdjustment)
     ? fixedPointAdjustment
     : fractionOfMaxMultiplier;
-
+  // Use the fractional amount to resolve the new multplier that the user has at
+  // receiving additional % on their staking reward APY
   const updatedMultiplier =
     noStake || updatedTimeEstimateInSeconds.eq(0)
       ? parseEther("1")
@@ -76,6 +91,8 @@ export function deriveNewStakingValues(
             .mul(origin.maxMultiplier.sub(fixedPointAdjustment))
             .div(fixedPointAdjustment)
         );
+  // Convert the new multplier into a percentage of it and the
+  // maximum multiplier
   const updatedMultiplierPercentage = origin.maxMultiplier.eq(0)
     ? 0
     : formattedBigNumberToNumber(
@@ -85,13 +102,19 @@ export function deriveNewStakingValues(
           .mul(100)
       );
 
+  // Resolve the new Rewards APY (base stakingAPY * multiplier)
   const updatedRewardsApy = origin.apyData.baseRewardsApy
     .mul(currentlyStaked.gt(0) ? updatedMultiplier : parseEtherLike("1"))
     .div(fixedPointAdjustment);
 
+  // Resolve the new total APY (pool APY + rewards APY)
   const updatedTotalApy = origin.apyData.poolApy.add(updatedRewardsApy);
 
-  const clonedOrigin = _.cloneDeep(origin);
+  // Clone all aspects of the origin so that there's no chance we can
+  // modify the initial input.
+  const clonedOrigin = cloneDeep(origin);
+  // Return the new configuration values with the origin values padded
+  // to fill the rest of the data
   return {
     ...clonedOrigin,
     userAmountOfLPStaked: currentlyStaked,
