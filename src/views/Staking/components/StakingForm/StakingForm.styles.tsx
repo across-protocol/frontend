a0a8991ff601941ad23a61dc3c500b11b98c4930
James Morris <jaamorris@cs.stonybrook.edu>
2022-11-04 12:03:03 -0400

    improve: update mobile padding

--- src/views/Staking/components/StakingForm/StakingForm.styles.tsx
+++ src/views/Staking/components/StakingForm/StakingForm.styles.tsx
@@ -43,6 +43,10 @@ export const Tab = styled.div<ITab>`
   border-bottom: ${(props) =>
     props.active ? "2px solid #e0f3ff" : "1px solid #3E4047"};
   cursor: pointer;
+
+  @media ${QUERIESV2.sm.andDown} {
+    padding: 0 0 12px;
+  }
 `;
 
 export const UsdcLogo = styled(UnstyedUsdcLogo)``;
