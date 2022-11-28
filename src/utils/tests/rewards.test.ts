709eb130876d5aad4f64c972191d77c43c23c2f6
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-11-28 16:26:18 +0100

    test: fix base rewards

--- src/utils/tests/rewards.test.ts
+++ src/utils/tests/rewards.test.ts
@@ -19,12 +19,4 @@ describe("#getBaseRewardsApr", () => {
       getBaseRewardsApr(totalBaseRewardsPerYear, totalStaked)
     ).toMatchObject(parseEtherLike("0.5"));
   });
-
-  test("return user base rewards APR of 50%", () => {
-    const totalStaked = totalBaseRewardsPerYear.mul(2);
-    const userStaked = totalBaseRewardsPerYear; // user share 50%
-    expect(
-      getBaseRewardsApr(totalBaseRewardsPerYear, totalStaked, userStaked)
-    ).toMatchObject(parseEtherLike("0.25"));
-  });
 });
