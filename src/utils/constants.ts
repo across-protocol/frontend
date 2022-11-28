275d068428bde6f693e722a0a689aa798a8fbc5f
Kevin <106104250+kevinuma@users.noreply.github.com>
2022-11-28 07:43:57 -0600

    feat: Add support for ACX (#422)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -407,6 +407,13 @@ export const tokenList: TokenInfoList = [
     logoURI: "/logos/usdt-logo.svg",
     mainnetAddress: getAddress("0xdAC17F958D2ee523a2206206994597C13D831ec7"),
   },
+  {
+    name: "Across Protocol Token",
+    symbol: "ACX",
+    decimals: 18,
+    logoURI: "/logos/acx-logo.png",
+    mainnetAddress: getAddress("0x44108f0223A3C3028F5Fe7AEC7f9bb2E66beF82F"),
+  },
 ];
 
 assert(
