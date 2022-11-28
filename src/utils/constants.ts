abcaf047b77936a666ee15f807dfaa4c5cdce274
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-11-28 18:33:57 -0500

    feat: :sparkles: Add a wrapper function to send a tx with automatic padding

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -722,3 +722,8 @@ export const rewardTiers = [
 
 export const secondsPerYear = across.constants.SECONDS_PER_YEAR;
 export const secondsPerDay = 86400; // 60 sec/min * 60 min/hr * 24 hr/day
+
+export const gasMultiplier = process.env
+  .REACT_SEND_TXN_GAS_ESTIMATION_MULTIPLIER
+  ? Number(process.env.REACT_SEND_TXN_GAS_ESTIMATION_MULTIPLIER)
+  : undefined;
--- src/utils/transactions.ts
+++ src/utils/transactions.ts
@@ -1,4 +1,6 @@
-import { ethers } from "ethers";
+import { Contract, ethers } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+import { fixedPointAdjustment, gasMultiplier } from "./constants";
 
 /**
  * This function takes a raw transaction and a signer and returns the result of signing the transaction.
@@ -20,4 +22,33 @@ export async function sendSignedTransaction(
 ): Promise<ethers.providers.TransactionResponse> {
   const tx = await provider.sendTransaction(signedTx);
   return tx;
+}
+
+/**
+ * Pads the gas estimation by a fixed amount dictated in the `REACT_SEND_TXN_GAS_ESTIMATION_MULTIPLIER` env var
+ * @param contract The contract that this transaction will originate from
+ * @param method The specific call method
+ * @param args The arguments to supply this smart contract call
+ * @returns A completed or failed transaction
+ */
+export async function sendWithPaddedGas<T>(
+  contract: Contract,
+  method: string,
+  args: any
+) {
+  /* If the gas multiplier hasn't been set, run this function as a normal tx */
+  if (!gasMultiplier) {
+    return contract[method](args) as Promise<T>;
+  } else {
+    // Estimate the gas with the provided estimateGas logic
+    const gasEstimation = await contract.estimateGas[method](args);
+    // Factor in the padding
+    const gasToRecommend = gasEstimation
+      .mul(parseEther(String(gasMultiplier)))
+      .div(fixedPointAdjustment);
+    // Call the tx with the padded gas
+    return contract[method](args, {
+      gasLimit: gasToRecommend,
+    }) as Promise<T>;
+  }
 }
