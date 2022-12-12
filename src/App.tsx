b6c41852b7edb15703b2e532b61a10671b9d29fb
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-12-12 17:31:11 -0500

    improve: add use of amplitude

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import { initAmplitude } from "utils/amplitude";
 import Routes from "./Routes";
+
+initAmplitude();
 
 function App() {
   return (
