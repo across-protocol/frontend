eb432e931073e42e4e97aa6ece80425ad7bbbd81
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-12-14 09:25:33 -0500

    improve: switch to static singleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { getAmplitudeLogger } from "utils/amplitude";
+import { AmplitudeEventLogger } from "utils/amplitude";
 import { AmplitudeEvent } from "utils/amplitude/types";
 import Routes from "./Routes";
 
 // Record an event when the application is loaded
-getAmplitudeLogger().recordEvent({
+AmplitudeEventLogger.shared().recordEvent({
   event: AmplitudeEvent.ApplicationLoaded,
   payload: {},
 });
