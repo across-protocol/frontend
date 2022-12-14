0a58acea4a92eb36ec29a7b30497b6d23c1f49b2
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-12-13 19:36:10 -0500

    feat: create type union for amplitude

--- src/utils/amplitude/types.ts
+++ src/utils/amplitude/types.ts
@@ -20,57 +20,170 @@ export enum AmplitudeEvent {
   TransferSigned = "TransferSigned",
   TransferTransactionCompleted = "TransferTransactionCompleted",
 }
-
 interface ApplicationLoadedRecordInterface {
   event: AmplitudeEvent.ApplicationLoaded;
 }
 interface WebVitalsRecordInterface {
   event: AmplitudeEvent.WebVitals;
+  payload: {
+    cumulativeLayoutShift: number;
+  };
 }
 interface ConnectWalletButtonClickedRecordInterface {
   event: AmplitudeEvent.ConnectWalletButtonClicked;
+  payload: {
+    action: string;
+    page: string;
+    section: string;
+    element: string;
+  };
 }
 interface WalletSelectedRecordInterface {
   event: AmplitudeEvent.WalletSelected;
+  payload: {
+    action: string;
+    page: string;
+    element: string;
+    walletType: string;
+  };
 }
 interface WalletConnectTransactionCompletedRecordInterface {
   event: AmplitudeEvent.WalletConnectTransactionCompleted;
+  payload: {
+    isReconnect: boolean;
+    succeeded: boolean;
+    walletAddress: string;
+    walletType: string;
+  };
 }
 interface DisconnectWalletButtonClickedRecordInterface {
   event: AmplitudeEvent.DisconnectWalletButtonClicked;
+  payload: {
+    action: string;
+    page: string;
+    section: string;
+    element: string;
+  };
 }
 interface PageViewedRecordInterface {
   event: AmplitudeEvent.PageViewed;
+  payload: {
+    referrer: string;
+    origin: string;
+    gitCommitHash: string;
+    isInitialPageView: string;
+    path: string;
+  };
 }
 interface FromChainSelectedRecordInterface {
   event: AmplitudeEvent.FromChainSelected;
+  payload: {
+    fromChainId: number;
+    chainName: string;
+  };
 }
 interface ToChainSelectedRecordInterface {
   event: AmplitudeEvent.ToChainSelected;
+  payload: {
+    toChainId: number;
+    chainName: string;
+  };
 }
 interface TokenSelectedRecordInterface {
   event: AmplitudeEvent.TokenSelected;
+  payload: {
+    tokenSymbol: string;
+    tokenListIndex: number;
+    tokenListLength: number;
+  };
 }
 interface MaxTokenAmountClickedRecordInterface {
   event: AmplitudeEvent.MaxTokenAmountClicked;
+  payload: {
+    action: string;
+    page: string;
+    section: string;
+    element: string;
+  };
 }
 interface ToAccountChangedRecordInterface {
   event: AmplitudeEvent.ToAccountChanged;
+  payload: {
+    toWalletAddress: string;
+  };
 }
+
+type TransferRelatedPayload = {
+  tokenSymbol: string;
+  fromAmount: string;
+  fromAmountUsd: string;
+  fromChainId: number;
+  fromChainName: string;
+  toAmount: string;
+  toAmountUsd: string;
+  toChainId: number;
+  toChainName: string;
+  sender: string;
+  recipient: string;
+  isSenderEqRecipient: boolean;
+  capitalFeePct: string;
+  capitalFeeTotal: string;
+  capitalFeeTotalUsd: string;
+  isAmountTooLow: boolean;
+  lpFeePct: string;
+  lpFeeTotal: string;
+  lpFeeTotalUsd: string;
+  relayFeePct: string;
+  relayFeeTotal: string;
+  relayFeeTotalUsd: string;
+  relayGasFeePct: string;
+  relayGasFeeTotal: string;
+  relayGasFeeTotalUsd: string;
+  expectedFillTimeInMinutes: number;
+  totalBridgeFee: string;
+  totalBridgeFeePct: string;
+  totalBridgeFeeUsd: string;
+  quoteTimestamp: number;
+  quoteLatencyMilliseconds: number;
+  routeChainIdFromTo: number;
+  routeChainNameFromTo: string;
+  transferQuoteBlockNumber: number;
+};
+
 interface FeesInfoExpandedRecordInterface {
   event: AmplitudeEvent.FeesInfoExpanded;
+  payload: TransferRelatedPayload;
 }
 interface TransferQuoteRecievedRecordInterface {
   event: AmplitudeEvent.TransferQuoteRecieved;
+  payload: TransferRelatedPayload;
 }
 interface TransferSubmittedRecordInterface {
   event: AmplitudeEvent.TransferSubmitted;
+  payload: TransferRelatedPayload & {
+    transferQuoteBlockNumber: number;
+    referralProgramAddress: string;
+  };
 }
 interface TransferSignedRecordInterface {
   event: AmplitudeEvent.TransferSigned;
+  payload: TransferRelatedPayload & {
+    transactionHash: string;
+    timeFromTransferSubmittedToTransferSignedInMilliseconds: number;
+    referralProgramAddress: string;
+  };
 }
 interface TransferTransactionCompletedRecordInterface {
   event: AmplitudeEvent.TransferTransactionCompleted;
+  payload: TransferRelatedPayload & {
+    transactionHash: string;
+    timeFromTransferSubmittedToTransferSignedInMilliseconds: number;
+    referralProgramAddress: string;
+    transferCompleteTimestamp: number;
+    NetworkFeeNative: string;
+    NetworkFeeNativeToken: string;
+    NetworkFeeUsd: string;
+  };
 }
 
 export type AmplitudeEventLogRecord =
