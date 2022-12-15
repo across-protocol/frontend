906d5411f4f8aea34575398bc2bc41a7fe6b43af
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-12-15 10:01:02 -0500

    fix: rebase issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { ampli } from "./ampli";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
 
-console.log(window.location.origin);
-
 // Record an event when the application is loaded
 ampli.load({
-  environment: amplitudeEnvironment,
+  environment: "production", // Since we are proxying the API, we need to set the environment manually
   client: {
-    apiKey: "ACX_PLACEHOLDER_API_KEY",
+    apiKey: "ACX_PLACEHOLDER_API_KEY", // Since we are proxying the API, we need to set the API key manually
     configuration: {
       serverUrl: `${window.location.origin}/api/instrumentation`,
     },
