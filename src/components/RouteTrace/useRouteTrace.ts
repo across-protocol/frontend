7f90ecbe9328022be6f1b49f45b39595ddc27992
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-12-15 16:20:28 -0500

    feat: instrument on every new page

--- src/components/RouteTrace/useRouteTrace.ts
+++ src/components/RouteTrace/useRouteTrace.ts
@@ -1,25 +1,64 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { currentGitCommitHash, getConfig } from "utils";
+import { ampli } from "../../ampli";
 
 export function useRouteTrace() {
   const location = useLocation();
   const [initialPage, setInitialPage] = useState(true);
-  const [currentPath, setCurrentPath] = useState("");
+  const [path, setPath] = useState("");
 
   useEffect(() => {
-    if (location.pathname !== "" && currentPath !== location.pathname) {
-      setCurrentPath(location.pathname);
+    if (location.pathname !== "" && path !== location.pathname) {
+      setPath(location.pathname);
     }
-  }, [location, currentPath]);
+  }, [location, path]);
 
   useEffect(() => {
-    if (currentPath) {
+    if (path) {
       const referrer = document.referrer;
       const origin = window.location.origin;
-
-      console.log(currentPath, referrer, "|", origin, initialPage);
+      const page = pageLookup[path] ?? "404Page";
+      ampli.pageViewed({
+        path,
+        referrer,
+        origin,
+        isInitialPageView: initialPage,
+        page,
+        gitCommitHash: currentGitCommitHash,
+      });
       setInitialPage(false);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPath]);
+  }, [path]);
 }
+
+const pageLookup: Record<
+  string,
+  | "404Page"
+  | "splashPage"
+  | "bridgePage"
+  | "poolPage"
+  | "rewardsPage"
+  | "transactionsPage"
+  | "stakingPage"
+  | "referralPage"
+  | "airdropPage"
+> = {
+  "/": "splashPage",
+  "/bridge": "bridgePage",
+  "/pool": "poolPage",
+  "/rewards": "rewardsPage",
+  "/rewards/referrals": "referralPage",
+  "/airdrop": "airdropPage",
+  "/transactions": "transactionsPage",
+  ...getConfig()
+    .getPoolSymbols()
+    .reduce(
+      (acc, sym) => ({
+        ...acc,
+        [`/rewards/staking/${sym.toLowerCase()}`]: "stakingPage",
+      }),
+      {}
+    ),
+};
