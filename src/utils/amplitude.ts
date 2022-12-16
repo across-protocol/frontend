0f08495dcaa888d3af79234ae8e6b6ecd8f88ac6
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-12-16 17:01:58 +0100

    feat: add wallet select + connection tx tracking

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,3 +1,6 @@
+import { WalletState } from "@web3-onboard/core";
+import { utils } from "ethers";
+
 import { ampli, ConnectWalletButtonClickedProperties } from "ampli";
 import { pageLookup } from "components/RouteTrace/useRouteTrace";
 
@@ -15,5 +18,39 @@ export function trackConnectWalletButtonClicked(
     element: "connectWalletButton",
     page: getPageValue(),
     section,
+  });
+}
+
+export function trackIfWalletSelected(
+  walletStates: WalletState[],
+  previousConnection?: string | null
+) {
+  // Only track if user explicitly selected a wallet in the web3-onboard modal
+  if (walletStates.length > 0 && !previousConnection) {
+    return ampli.walletSelected({
+      page: getPageValue(),
+      action: "onClick",
+      element: "web3OnboardModal",
+      walletType: walletStates[0].label,
+    });
+  }
+}
+
+export function trackWalletConnectTransactionCompleted(
+  walletStates: WalletState[],
+  previousConnection?: string | null
+) {
+  if (walletStates.length === 0) {
+    return ampli.walletConnectTransactionCompleted({
+      isReconnect: false,
+      succeeded: false,
+    });
+  }
+
+  return ampli.walletConnectTransactionCompleted({
+    isReconnect: Boolean(previousConnection),
+    succeeded: true,
+    walletAddress: utils.getAddress(walletStates[0].accounts[0].address),
+    walletType: walletStates[0].label,
   });
 }
