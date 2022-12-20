8d5e6e358093bf871a39bd940e3d97a38b357d09
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-12-20 12:39:36 -0500

    format: include quote trasaction

--- src/components/SendAction/SendAction.tsx
+++ src/components/SendAction/SendAction.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  formatUnits,
-  receiveAmount,
-  getChainInfo,
-  getToken,
-  getConfirmationDepositTime,
-} from "utils";
+import { formatUnits, getChainInfo } from "utils";
 import { PrimaryButton } from "../Buttons";
 import {
   Wrapper,
@@ -32,8 +26,6 @@ const SendAction: React.FC<Props> = ({ onDeposit }) => {
   const {
     amount,
     fees,
-    tokenSymbol,
-    toChain,
     handleActionClick,
     buttonDisabled,
     isInfoModalOpen,
@@ -42,26 +34,14 @@ const SendAction: React.FC<Props> = ({ onDeposit }) => {
     txPending,
     fromChain,
     txHash,
-    limits,
-    limitsError,
+    showFees,
+    toChainInfo,
+    fromChainInfo,
+    tokenInfo,
+    timeToRelay,
+    amountMinusFees,
+    isWETH,
   } = useSendAction(onDeposit);
-  const showFees = amount.gt(0) && !!fees;
-  const amountMinusFees = showFees ? receiveAmount(amount, fees) : undefined;
-  const toChainInfo = toChain ? getChainInfo(toChain) : undefined;
-  const fromChainInfo = fromChain ? getChainInfo(fromChain) : undefined;
-  const tokenInfo = tokenSymbol ? getToken(tokenSymbol) : undefined;
-  const isWETH = tokenInfo?.symbol === "WETH";
-  let timeToRelay = "loading";
-  if (limits && toChain && fromChain) {
-    timeToRelay = getConfirmationDepositTime(
-      amount,
-      limits,
-      toChain,
-      fromChain
-    );
-  } else if (limitsError) {
-    timeToRelay = "estimation failed";
-  }
 
   return (
     <AccentSection>
@@ -90,7 +70,7 @@ const SendAction: React.FC<Props> = ({ onDeposit }) => {
               <Info>
                 <div>Destination Gas Fee</div>
                 <div>
-                  {showFees
+                  {showFees && fees
                     ? `${formatUnits(
                         fees.relayerGasFee.total,
                         tokenInfo.decimals
--- src/components/SendAction/useSendAction.ts
+++ src/components/SendAction/useSendAction.ts
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSendForm, useBridgeFees, useBridge, useBridgeLimits } from "hooks";
-import { confirmations, bridgeDisabled } from "utils";
+import {
+  confirmations,
+  bridgeDisabled,
+  getToken,
+  formatUnits,
+  fixedPointAdjustment,
+  formatEther,
+  getChainInfo,
+  getConfirmationDepositTime,
+  receiveAmount,
+  formatWeiPct,
+} from "utils";
 import { Deposit } from "views/Confirmation";
 import { useConnection } from "hooks";
+import { ampli } from "ampli";
+import { useCoingeckoPrice } from "hooks/useCoingeckoPrice";
+import { BigNumber } from "ethers";
+import { ConvertDecimals } from "utils/convertdecimals";
 
 export default function useSendAction(
   onDepositConfirmed: (deposit: Deposit) => void
@@ -18,9 +33,116 @@ export default function useSendAction(
     fromChain,
     toChain
   );
+
+  const showFees = amount.gt(0) && !!fees;
+  const amountMinusFees = showFees ? receiveAmount(amount, fees) : undefined;
+  const toChainInfo = toChain ? getChainInfo(toChain) : undefined;
+  const fromChainInfo = fromChain ? getChainInfo(fromChain) : undefined;
+  const tokenInfo = tokenSymbol ? getToken(tokenSymbol) : undefined;
+  let timeToRelay = "loading";
+  if (limits && toChain && fromChain) {
+    timeToRelay = getConfirmationDepositTime(
+      amount,
+      limits,
+      toChain,
+      fromChain
+    );
+  } else if (isError) {
+    timeToRelay = "estimation failed";
+  }
+
+  const tokenPrice = useCoingeckoPrice(
+    selectedRoute?.l1TokenAddress!,
+    "usd",
+    selectedRoute !== undefined
+  );
+  // Create a function that converts a wei amount into a formatted token amount
+  const formatTokens = (wei: BigNumber) =>
+    formatUnits(wei, tokenInfo?.decimals ?? 18);
+  // Create a function that converts a wei amount to a USD equivalent
+  const usdEquivalent = (wei: BigNumber) =>
+    (tokenPrice.data?.price ?? fixedPointAdjustment)
+      .mul(
+        ConvertDecimals(tokenInfo?.decimals ?? 18, 18)(wei ?? BigNumber.from(0))
+      )
+      .div(fixedPointAdjustment);
+  // Create a function that converts a wei amount to a USD equivalent string
+  const usdEquivalentString = (wei: BigNumber) =>
+    formatEther(usdEquivalent(wei));
+  const formatWeiEtherPct = (wei: BigNumber) => formatWeiPct(wei)!.toString();
+
   const { status, hasToApprove, send, approve } = useBridge();
   const { account, connect } = useConnection();
   const [txHash, setTxHash] = useState("");
+
+  // This use effect instruments amplitude when a new quote is received
+  useEffect(() => {
+    // Ensure that we have a quote and fees before instrumenting.
+    if (
+      fees &&
+      selectedRoute &&
+      tokenInfo &&
+      fromChainInfo &&
+      toChainInfo &&
+      toAddress &&
+      account
+    ) {
+      ampli.transferQuoteRecieved({
+        capitalFeePct: formatWeiEtherPct(fees.relayerCapitalFee.pct),
+        capitalFeeTotal: formatTokens(fees.relayerCapitalFee.total),
+        capitalFeeTotalUsd: usdEquivalentString(fees.relayerCapitalFee.total),
+        expectedFillTimeInMinutes: timeToRelay,
+        fromAmount: formatTokens(amount),
+        fromAmountUsd: usdEquivalentString(amount),
+        fromChainId: selectedRoute.fromChain.toString(),
+        fromChainName: fromChainInfo.name,
+        isAmountTooLow: fees.isAmountTooLow,
+        isSenderEqRecipient: toAddress === account,
+        lpFeePct: formatWeiEtherPct(fees.lpFee.pct),
+        lpFeeTotal: formatTokens(fees.lpFee.total),
+        lpFeeTotalUsd: usdEquivalentString(fees.lpFee.total),
+        quoteLatencyMilliseconds: (
+          Date.now() - Number(fees.quoteTimestamp?.toString() ?? Date.now())
+        ).toString(),
+        quoteTimestamp: String(fees.quoteTimestamp ?? Date.now()),
+        recipient: toAddress,
+        relayFeePct: formatWeiEtherPct(fees.relayerFee.pct),
+        relayFeeTotal: formatTokens(fees.relayerFee.total),
+        relayFeeTotalUsd: usdEquivalentString(fees.relayerFee.total),
+        relayGasFeePct: formatWeiEtherPct(fees.relayerGasFee.pct),
+        relayGasFeeTotal: formatTokens(fees.relayerGasFee.total),
+        relayGasFeeTotalUsd: usdEquivalentString(fees.relayerGasFee.total),
+        sender: account,
+        routeChainIdFromTo: toChainInfo.chainId.toString(),
+        routeChainNameFromTo: toChainInfo.name,
+        toAmount: formatTokens(amount),
+        toAmountUsd: usdEquivalentString(amount),
+        toChainId: selectedRoute.toChain.toString(),
+        toChainName: toChainInfo.name,
+        tokenSymbol: tokenInfo.symbol,
+        totalBridgeFee: formatTokens(
+          fees.relayerCapitalFee.total
+            .add(fees.lpFee.total)
+            .add(fees.relayerFee.total)
+            .add(fees.relayerGasFee.total)
+        ),
+        totalBridgeFeeUsd: usdEquivalentString(
+          fees.relayerCapitalFee.total
+            .add(fees.lpFee.total)
+            .add(fees.relayerFee.total)
+            .add(fees.relayerGasFee.total)
+        ),
+        totalBridgeFeePct: formatWeiEtherPct(
+          fees.relayerCapitalFee.pct
+            .add(fees.lpFee.pct)
+            .add(fees.relayerFee.pct)
+            .add(fees.relayerGasFee.pct)
+        ),
+        transferQuoteBlockNumber: fees.quoteBlock.toString(),
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fees, selectedRoute, tokenInfo]);
 
   const handleActionClick = async () => {
     if (status !== "ready" || !selectedRoute || bridgeDisabled) {
@@ -41,9 +163,14 @@ export default function useSendAction(
         }
         return tx;
       } else {
+        // Instrument amplitude before sending the transaction for the submit button.
+
         // We save the fees here, in case they change between here and when we save the deposit.
         const feesUsed = fees;
         const tx = await send();
+
+        // Instrument amplitude after signing the transaction for the submit button.
+
         // NOTE: This check is redundant, as if `status` is `ready`, all of those are defined.
         if (tx && toAddress && account && feesUsed) {
           setTxHash(tx.hash);
@@ -62,6 +189,8 @@ export default function useSendAction(
             })
             .catch(console.error)
             .finally(() => {
+              // Instrument amplitude after the transaction is confirmed for the submit button.
+
               setTxPending(false);
               setTxHash("");
             });
@@ -109,5 +238,11 @@ export default function useSendAction(
     txHash,
     limits,
     limitsError: isError,
+    showFees,
+    amountMinusFees,
+    toChainInfo,
+    fromChainInfo,
+    tokenInfo,
+    timeToRelay,
   };
 }
