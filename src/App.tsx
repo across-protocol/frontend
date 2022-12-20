49f5835ba24fb9fdffba229042fe15523764ccb3
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-12-20 17:42:24 +0100

    refactor: refine ampli config (#542)
    * feat: refine log level and tracking options
    
    * refactor: disable amplitude if api key not set

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { ampli } from "./ampli";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Types } from "@amplitude/analytics-browser";
+import { ampli } from "./ampli";
 import Routes from "./Routes";
-import { amplitudeAPIKey, amplitudeEnvironment } from "utils";
+import {
+  amplitudeEnvironment,
+  amplitudeAPIKey,
+  isProductionBuild,
+} from "utils";
 
 // Record an event when the application is loaded
 ampli.load({
   environment: amplitudeEnvironment,
+  disabled: !Boolean(amplitudeAPIKey),
   client: {
-    apiKey: amplitudeAPIKey || "",
+    apiKey: amplitudeAPIKey,
     configuration: {
       disableCookies: true,
+      logLevel: isProductionBuild ? Types.LogLevel.Error : Types.LogLevel.Debug,
+      trackingOptions: {
+        ipAddress: false,
+        carrier: false,
+        city: false,
+        region: false,
+        dma: false, // designated market area
+      },
     },
   },
 });
