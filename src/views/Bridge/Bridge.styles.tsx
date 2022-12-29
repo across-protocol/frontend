8a8eaa5ccf0fad79c1818af6bf6045e0760928d3
james-a-morris <jaamorris@cs.stonybrook.edu>
2022-12-28 20:50:45 -0500

    feat: include coin selector logic

--- src/views/Bridge/Bridge.styles.tsx
+++ src/views/Bridge/Bridge.styles.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { QUERIESV2 } from "utils";
+import ExternalCardWrapper from "components/CardWrapper";
 
 export const Wrapper = styled.div`
   background-color: transparent;
@@ -17,10 +18,30 @@ export const Wrapper = styled.div`
   }
 `;
 
+export const CardWrapper = styled(ExternalCardWrapper)`
+  width: 100%;
+`;
+
 export const RowWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   padding: 0px;
   gap: 12px;
+
+  width: 100%;
+`;
+
+export const ChainIconTextWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0px;
+  gap: 12px;
+`;
+
+export const ChainIcon = styled.img`
+  width: 24px;
+  height: 24px;
 `;
--- src/views/Bridge/Bridge.tsx
+++ src/views/Bridge/Bridge.tsx
@@ -1,41 +1,101 @@
-import styled from "@emotion/styled";
 import { LayoutV2 } from "components";
-import ExternalCardWrapper from "components/CardWrapper";
+import Selector from "components/Selector/Selector";
 import { Text } from "components/Text";
-import { RowWrapper, Wrapper } from "./Bridge.styles";
+import { capitalizeFirstLetter } from "utils";
+import {
+  CardWrapper,
+  ChainIcon,
+  ChainIconTextWrapper,
+  RowWrapper,
+  Wrapper,
+} from "./Bridge.styles";
 import Breadcrumb from "./components/Breadcrumb";
+import CoinSelector from "./components/CoinSelector";
 import SlippageAlert from "./components/SlippageAlert";
+import { useBridge } from "./hooks/useBridge";
 
-const Bridge = () => (
-  <LayoutV2 maxWidth={600}>
-    <Wrapper>
-      <Breadcrumb />
-      <CardWrapper>
-        <RowWrapper>
-          <Text size="md" color="grey-400">
-            Send
-          </Text>
-        </RowWrapper>
-        <RowWrapper>
-          <Text size="md" color="grey-400">
-            From
-          </Text>
-        </RowWrapper>
-        <RowWrapper>
-          <Text size="md" color="grey-400">
-            To
-          </Text>
-        </RowWrapper>
-      </CardWrapper>
-      <CardWrapper>
-        <SlippageAlert />
-      </CardWrapper>
-    </Wrapper>
-  </LayoutV2>
-);
+const Bridge = () => {
+  const {
+    availableTokens,
+    currentToken,
+    setCurrentToken,
+    setAmountToBridge,
+    currentBalance,
+    currentFromRoute,
+    setCurrentFromRoute,
+    availableFromRoutes,
+    availableToRoutes,
+    currentToRoute,
+    setCurrentToRoute,
+  } = useBridge();
+  return (
+    <LayoutV2 maxWidth={600}>
+      <Wrapper>
+        <Breadcrumb />
+        <CardWrapper>
+          <RowWrapper>
+            <Text size="md" color="grey-400">
+              Send
+            </Text>
+            <CoinSelector
+              fromChain={currentFromRoute ?? 1}
+              toChain={currentToRoute ?? 1}
+              tokenChoices={availableTokens}
+              tokenSelected={currentToken}
+              setTokenSelected={setCurrentToken}
+              setAmountToBridge={setAmountToBridge}
+              currentSelectedBalance={currentBalance}
+            />
+          </RowWrapper>
+          <RowWrapper>
+            <Text size="md" color="grey-400">
+              From
+            </Text>
+            <Selector<number>
+              elements={availableFromRoutes.map((r) => ({
+                value: r.chainId,
+                element: (
+                  <ChainIconTextWrapper>
+                    <ChainIcon src={r.logoURI} />
+                    <Text size="md" color="white-100">
+                      {capitalizeFirstLetter(r.fullName ?? r.name)}
+                    </Text>
+                  </ChainIconTextWrapper>
+                ),
+              }))}
+              selectedValue={currentFromRoute ?? 1}
+              setSelectedValue={(v) => setCurrentFromRoute(v)}
+              title="Chain"
+            />
+          </RowWrapper>
+          <RowWrapper>
+            <Text size="md" color="grey-400">
+              To
+            </Text>
+            <Selector<number>
+              elements={availableToRoutes.map((r) => ({
+                value: r.chainId,
+                element: (
+                  <ChainIconTextWrapper>
+                    <ChainIcon src={r.logoURI} />
+                    <Text size="md" color="white-100">
+                      {capitalizeFirstLetter(r.fullName ?? r.name)}
+                    </Text>
+                  </ChainIconTextWrapper>
+                ),
+              }))}
+              selectedValue={currentToRoute ?? 1}
+              setSelectedValue={(v) => setCurrentToRoute(v)}
+              title="Chain"
+            />
+          </RowWrapper>
+        </CardWrapper>
+        <CardWrapper>
+          <SlippageAlert />
+        </CardWrapper>
+      </Wrapper>
+    </LayoutV2>
+  );
+};
 
 export default Bridge;
-
-const CardWrapper = styled(ExternalCardWrapper)`
-  width: 100%;
-`;
