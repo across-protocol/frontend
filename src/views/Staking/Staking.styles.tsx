33dadaeae1f27e55dcc234f8d98b40030e2fb45c
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2022-12-29 09:34:58 -0500

    feat: ✨ add bridge UI update (#554)
    * feat: :sparkles: add central card component
    
    * feat: create slippage alert
    
    * feat: create beginning of bridge route
    
    * improve: add functionality to alert and text
    
    * feat: create selector
    
    * improve: update selector to use value instead of index
    
    * feat: include coin selector logic
    
    * fix: remove logs
    
    * fix: change naming convention

--- src/views/Staking/Staking.styles.tsx
+++ src/views/Staking/Staking.styles.tsx
@@ -17,30 +17,6 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const Card = styled.div`
-  width: 100%;
-
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  background: #34353b;
-
-  border: 1px solid #3e4047;
-  border-radius: 10px;
-
-  flex-wrap: nowrap;
-
-  padding: 24px;
-  gap: 24px;
-  @media ${QUERIESV2.sm.andDown} {
-    padding: 12px 16px 16px;
-    gap: 16px;
-    margin-top: -4px;
-  }
-`;
-
 export const Divider = styled.div`
   width: 100%;
   height: 1px;
