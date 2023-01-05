7bf8c6663c6af1172a24380b363a2a87be94ff40
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-01-05 17:51:26 +0100

    feat: liquidity pool mutations (#560)
    * feat: add `useApprove` hook
    
    * feat: add l1 token balance to user lp query
    
    * refactor: more concise l1 naming
    
    * feat: add max amount and validation hooks
    
    * feat: lp action mutations
    
    * fix: refetch info on pool select change
    
    * fix: suppress `React does not recognize ...` console error
    
    * fix: re-enable pool symbol deep link
    
    * fix: user lp query if disconnected
    
    * fix: lp fees conversion
    
    * fixup
    
    * fix: mobile adjustments
    
    * refactor: option for noop in `useApprove` hook
    
    * fix: lp max amounts query

---
+++ src/views/LiquidityPool/utils.ts
@@ -0,0 +1,30 @@
+import { BigNumber, utils } from "ethers";
+
+export function parseAndValidateAmountInput(
+  amountInput: string,
+  amountDecimals: number,
+  maxAmount: BigNumber
+) {
+  const cleanedInput = amountInput.replaceAll(",", "");
+
+  if (cleanedInput.split(".")[1]?.length > amountDecimals) {
+    throw new Error("Input amount decimals exceeds max decimals");
+  }
+
+  let amountBN: BigNumber;
+  try {
+    amountBN = utils.parseUnits(cleanedInput, amountDecimals);
+  } catch (error) {
+    throw new Error("Invalid amount");
+  }
+
+  if (maxAmount.lt(amountBN)) {
+    throw new Error("Input amount exceeds max amount");
+  }
+
+  if (amountBN.lte(0)) {
+    throw new Error("Input amount must be greater than 0");
+  }
+
+  return amountBN;
+}
