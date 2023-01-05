0734496f459a8ea868b7b7df73d4521190b0c0b5
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-01-05 17:51:26 +0100

    feat: liquidity pool mutations (#560)
    * feat: add `useApprove` hook
    
    * feat: add l1 token balance to user lp query
    
    * refactor: more concise l1 naming
    
    * feat: add max amount and validation hooks
    
    * feat: lp action mutations
    
    * fix: refetch info on pool select change
    
    * fix: suppress `React does not recognize ...` console error
    
    * fix: re-enable pool symbol deep link
    
    * fix: user lp query if disconnected
    
    * fix: lp fees conversion
    
    * fixup
    
    * fix: mobile adjustments
    
    * refactor: option for noop in `useApprove` hook
    
    * fix: lp max amounts query

---
+++ src/hooks/useApprove.ts
@@ -0,0 +1,47 @@
+import { useConnection } from "hooks";
+import { useMutation } from "react-query";
+import { BigNumberish } from "ethers";
+import { ERC20__factory } from "@across-protocol/contracts-v2";
+
+import { MAX_APPROVAL_AMOUNT, notificationEmitter } from "utils";
+import { useIsWrongNetwork } from "hooks";
+
+export function useApprove() {
+  const { account, signer, notify } = useConnection();
+  const { isWrongNetwork, isWrongNetworkHandler } = useIsWrongNetwork();
+
+  const handleApprove = async (args: {
+    erc20Address: string;
+    approvalAmount: BigNumberish;
+    allowedContractAddress: string;
+    enforceCorrectNetwork?: boolean;
+  }) => {
+    if (!signer || !account) {
+      return;
+    }
+
+    if (isWrongNetwork) {
+      if (args.enforceCorrectNetwork) {
+        await isWrongNetworkHandler();
+      } else {
+        return;
+      }
+    }
+
+    const erc20 = ERC20__factory.connect(args.erc20Address, signer);
+    const allowance = await erc20.allowance(
+      account,
+      args.allowedContractAddress
+    );
+
+    if (allowance.lt(args.approvalAmount)) {
+      const txResponse = await erc20.approve(
+        args.allowedContractAddress,
+        MAX_APPROVAL_AMOUNT
+      );
+      await notificationEmitter(txResponse.hash, notify);
+    }
+  };
+
+  return useMutation(handleApprove);
+}
