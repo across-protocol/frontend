9ae81b9bf788cf509601f281a1afef11e57ecb8e
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-01-11 07:17:26 -0500

    refactor(ui): :fire: remove unused hooks and views (#578)
    This change removes views and hooks that are no longer required in the new bridge page. The new bridge logic has condensed this logic and collocated it in the Bridge view.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,6 @@
 export { default as GlobalStyles } from "./GlobalStyles";
 export { default as Layout } from "./Layout";
 export { default as Header } from "./Header";
-export { default as ChainSelection } from "./ChainSelection";
-export { default as CoinSelection } from "./CoinSelection";
-export { default as AddressSelection } from "./AddressSelection";
-export { default as SendAction } from "./SendAction";
 export { default as Dialog } from "./Dialog";
 export { default as SuperHeader } from "./SuperHeader";
 export { default as Stepper } from "./Stepper";
