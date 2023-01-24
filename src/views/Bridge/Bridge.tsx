74f513be99d3cbf55310534ed217b9fcf3441333
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-01-24 16:44:56 +0100

    fix: display wrong network banner on pool page (#617)
    * fix: display wrong network banner on pool page
    
    * fixup

--- src/views/Bridge/Bridge.tsx
+++ src/views/Bridge/Bridge.tsx
@@ -1,5 +1,4 @@
-import { LayoutV2, SuperHeader } from "components";
-import { getChainInfo } from "utils";
+import { LayoutV2, WrongNetworkHeader } from "components";
 import { Wrapper } from "./Bridge.styles";
 import Breadcrumb from "./components/Breadcrumb";
 import BridgeForm from "./components/BridgeForm";
@@ -20,8 +19,6 @@ const Bridge = () => {
     availableToRoutes,
     currentToRoute,
     setCurrentToRoute,
-    isWrongChain,
-    handleChainSwitch,
     handleQuickSwap,
     isConnected,
     buttonActionHandler,
@@ -43,16 +40,8 @@ const Bridge = () => {
   } = useBridge();
   return (
     <>
-      {isWrongChain && currentFromRoute && (
-        <SuperHeader>
-          <div>
-            You are on an incorrect network. Please{" "}
-            <button onClick={handleChainSwitch}>
-              switch to {getChainInfo(currentFromRoute).name}
-            </button>
-            .
-          </div>
-        </SuperHeader>
+      {currentFromRoute && (
+        <WrongNetworkHeader requiredChainId={currentFromRoute} />
       )}
       {toAccount && (
         <ChangeAccountModal
