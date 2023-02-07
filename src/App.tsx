3c2199935fed0ea9f2d13d61a3aef0c7b21ca9fe
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-02-07 10:08:51 +0100

    feat: integrate @amplitude/marketing-analytics-browser (#648)
    * feat: integrate @amplitude/marketing-analytics-browser
    
    * fix: remove truncate decimals tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { Types } from "@amplitude/analytics-browser";
+import { createInstance } from "@amplitude/marketing-analytics-browser";
 import { ampli } from "./ampli";
 import Routes from "./Routes";
-import {
-  amplitudeEnvironment,
-  amplitudeAPIKey,
-  isProductionBuild,
-} from "utils";
+import { amplitudeAPIKey, isProductionBuild } from "utils";
 
-// Record an event when the application is loaded
-ampli.load({
-  environment: amplitudeEnvironment,
-  disabled: !Boolean(amplitudeAPIKey),
-  client: {
-    apiKey: amplitudeAPIKey,
-    configuration: {
-      disableCookies: true,
-      logLevel: isProductionBuild ? Types.LogLevel.Error : Types.LogLevel.Debug,
-      trackingOptions: {
-        ipAddress: false,
-        carrier: false,
-        city: false,
-        region: false,
-        dma: false, // designated market area
-      },
+if (amplitudeAPIKey) {
+  const instance = createInstance();
+  instance.init(amplitudeAPIKey, undefined, {
+    disableCookies: true,
+    logLevel: isProductionBuild ? Types.LogLevel.Error : Types.LogLevel.Debug,
+    trackingOptions: {
+      ipAddress: false,
+      carrier: false,
+      city: false,
+      region: false,
+      dma: false, // designated market area
     },
-  },
-});
+    attribution: {
+      disabled: !Boolean(amplitudeAPIKey),
+    },
+  });
+  ampli.load({
+    client: { instance },
+  });
+}
+// Record an event when the application is loaded
 ampli.applicationLoaded();
 
 function App() {
