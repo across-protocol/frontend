9ccba3ad5d30fba977883d9fb7736e28de93031d
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-02-10 15:38:57 -0500

    fix: :ambulance: resolve compiler issue (#653)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -398,6 +398,8 @@ export const onboardApiKey = process.env.REACT_APP_PUBLIC_ONBOARD_API_KEY;
 export const matomoUrl = process.env.REACT_APP_MATOMO_URL;
 export const debug = Boolean(process.env.REACT_APP_DEBUG);
 export const isProductionBuild = process.env.NODE_ENV === "production";
+export const isAmplitudeLoggingEnabled =
+  process.env.REACT_APP_AMPLITUDE_DEBUG_LOGGING === "true";
 
 export const rewardsBannerWarning =
   process.env.REACT_APP_REWARDS_BANNER_WARNING;
