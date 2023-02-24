4e947b6687489339e4ec9a5c79f180de33b09aab
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2023-02-24 09:10:05 -0500

    improve: properly set AllAssetsAllNetworksWalletCurrentBalanceUsd (#669)
    * improve: properly set amplitude environment
    
    * improve: initiate retry until balance is resolved
    
    * improve: convert to useQuery
    
    * chore: revert environment
    
    * fix: remove error correction

--- src/components/WalletBalanceTrace/useWalletBalanceTrace.ts
+++ src/components/WalletBalanceTrace/useWalletBalanceTrace.ts
@@ -1,6 +1,5 @@
 import { BigNumber, utils } from "ethers";
 import { useConnection } from "hooks";
-import { useEffect } from "react";
 import {
   ChainId,
   fixedPointAdjustment,
@@ -13,100 +12,107 @@ import {
 } from "utils";
 import { ConvertDecimals } from "utils/convertdecimals";
 import getApiEndpoint from "utils/serverless-api";
-import Sentry from "utils/sentry";
+import { useQuery } from "react-query";
 
 export function useWalletBalanceTrace() {
-  const { account, isConnected } = useConnection();
-  useEffect(() => {
-    if (account && isConnected) {
-      calculateUsdBalances(account)
-        .then((balance) => {
-          reportTotalWalletUsdBalance(balance);
-        })
-        .catch((e) => {
-          console.error("Failed to fetch balances:", e);
-          Sentry.captureException(e);
-        });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [account]);
-}
+  const { account } = useConnection();
 
-type TokenSymbolAddressType = {
-  fromTokenSymbol: string;
-  fromTokenAddress: string;
-};
-const availableTokens: Record<number, TokenSymbolAddressType[]> = getRoutes(
-  getConfig().getHubPoolChainId()
-).routes.reduce((acc, route) => {
-  const { fromChain, fromTokenSymbol, fromTokenAddress } = route;
-  const payload = { fromTokenSymbol, fromTokenAddress };
-
-  if (!acc[fromChain]?.some((t) => t.fromTokenAddress === fromTokenAddress)) {
-    acc[fromChain] = [...(acc[fromChain] ?? []), payload];
-  }
-  return acc;
-}, {} as Record<number, TokenSymbolAddressType[]>);
-
-const availableSymbols = [
-  ...new Set(getRoutes(ChainId.MAINNET).routes.map((r) => r.fromTokenSymbol)),
-];
-
-const calculateUsdBalances = async (account: string) => {
-  const coingeckoPrices = await Promise.all(
-    availableSymbols.map((symbol) =>
-      getApiEndpoint().coingecko(getToken(symbol).mainnetAddress!, "usd")
-    )
-  );
-  const usdPrices: Record<string, BigNumber> = coingeckoPrices.reduce(
-    (acc, { price }, i) => {
-      return { ...acc, [availableSymbols[i]]: price };
+  useQuery(
+    ["wallet-balance", account ?? "loading"],
+    async () => {
+      if (!account) return;
+      const balance = await calculateUsdBalances(account);
+      reportTotalWalletUsdBalance(balance);
+      return balance;
     },
-    {}
+    {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+      retry: 5,
+      retryDelay: 3000,
+    }
   );
 
-  const balances = (
-    await Promise.all(
-      Object.entries(availableTokens).flatMap(async ([chainId, tokens]) => {
-        const promises = tokens.map(
-          async ({ fromTokenSymbol, fromTokenAddress }) => ({
-            fromTokenSymbol,
-            fromTokenAddress,
-            balance: await getBalance(
-              Number(chainId),
-              account,
-              fromTokenAddress
-            ),
-          })
-        );
-        if (chainId !== "137") {
-          const fn = async () => {
-            return {
-              fromTokenSymbol: "ETH",
-              fromTokenAddress: "0x0000000000000000000000000000000000000000",
-              balance: await getNativeBalance(Number(chainId), account),
-            };
-          };
-          promises.push(fn());
-        }
-        return await Promise.all(promises);
-      })
-    )
-  ).flat(2);
+  type TokenSymbolAddressType = {
+    fromTokenSymbol: string;
+    fromTokenAddress: string;
+  };
+  const availableTokens: Record<number, TokenSymbolAddressType[]> = getRoutes(
+    getConfig().getHubPoolChainId()
+  ).routes.reduce((acc, route) => {
+    const { fromChain, fromTokenSymbol, fromTokenAddress } = route;
+    const payload = { fromTokenSymbol, fromTokenAddress };
 
-  const totalBalance = balances.reduce((acc, { fromTokenSymbol, balance }) => {
-    const token = getToken(fromTokenSymbol);
-    const usdPrice = usdPrices[fromTokenSymbol];
-    const usdBalance = Number(
-      utils.formatUnits(
-        usdPrice
-          .mul(ConvertDecimals(token.decimals, 18)(balance))
-          .div(fixedPointAdjustment),
-        18
+    if (!acc[fromChain]?.some((t) => t.fromTokenAddress === fromTokenAddress)) {
+      acc[fromChain] = [...(acc[fromChain] ?? []), payload];
+    }
+    return acc;
+  }, {} as Record<number, TokenSymbolAddressType[]>);
+
+  const availableSymbols = [
+    ...new Set(getRoutes(ChainId.MAINNET).routes.map((r) => r.fromTokenSymbol)),
+  ];
+
+  const calculateUsdBalances = async (account: string) => {
+    const coingeckoPrices = await Promise.all(
+      availableSymbols.map((symbol) =>
+        getApiEndpoint().coingecko(getToken(symbol).mainnetAddress!, "usd")
       )
     );
-    return acc + usdBalance;
-  }, 0);
+    const usdPrices: Record<string, BigNumber> = coingeckoPrices.reduce(
+      (acc, { price }, i) => {
+        return { ...acc, [availableSymbols[i]]: price };
+      },
+      {}
+    );
 
-  return totalBalance;
-};
+    const balances = (
+      await Promise.all(
+        Object.entries(availableTokens).flatMap(async ([chainId, tokens]) => {
+          const promises = tokens.map(
+            async ({ fromTokenSymbol, fromTokenAddress }) => ({
+              fromTokenSymbol,
+              fromTokenAddress,
+              balance: await getBalance(
+                Number(chainId),
+                account,
+                fromTokenAddress
+              ),
+            })
+          );
+          if (chainId !== "137") {
+            const fn = async () => {
+              return {
+                fromTokenSymbol: "ETH",
+                fromTokenAddress: "0x0000000000000000000000000000000000000000",
+                balance: await getNativeBalance(Number(chainId), account),
+              };
+            };
+            promises.push(fn());
+          }
+          return await Promise.all(promises);
+        })
+      )
+    ).flat(2);
+
+    const totalBalance = balances.reduce(
+      (acc, { fromTokenSymbol, balance }) => {
+        const token = getToken(fromTokenSymbol);
+        const usdPrice = usdPrices[fromTokenSymbol];
+        const usdBalance = Number(
+          utils.formatUnits(
+            usdPrice
+              .mul(ConvertDecimals(token.decimals, 18)(balance))
+              .div(fixedPointAdjustment),
+            18
+          )
+        );
+        return acc + usdBalance;
+      },
+      0
+    );
+
+    return totalBalance;
+  };
+}
