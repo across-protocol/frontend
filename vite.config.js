124ab5933dadea32de9e7170c0ab40101a8aad89
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-07-25 16:22:37 +0200

    remove obsolete vite plugins

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-// import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 import eslint from "vite-plugin-eslint";
 import EnvironmentPlugin from "vite-plugin-environment";
@@ -47,7 +46,6 @@ export default defineConfig({
           process: true,
           buffer: true,
         }),
-        // NodeModulesPolyfillPlugin(),
       ],
     },
   },
