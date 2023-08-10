9dedb25ca7a5595b7dd4d94d30249db7bf812e0b
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-08-10 18:03:31 +0200

    add arbitrum goerli testnet usdc

--- api/_utils.ts
+++ api/_utils.ts
@@ -397,7 +397,7 @@ export const queries: Record<number, () => QueryBase> = {
       getProvider(421613),
       undefined,
       "0xD29C85F15DF544bA632C9E25829fd29d767d7978",
-      undefined,
+      "0xfd064A18f3BF249cf1f87FC203E90D8f650f2d63",
       "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D",
       REACT_APP_COINGECKO_PRO_API_KEY,
       getLogger(),
