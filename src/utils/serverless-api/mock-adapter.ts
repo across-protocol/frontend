30435733bd1bacab6dac78018069c6e7ecc3de25
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-08-14 15:22:30 -0400

    fix: :bug: re-validate input on chain change

--- src/utils/serverless-api/mock-adapter.ts
+++ src/utils/serverless-api/mock-adapter.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 
 const isolatedAxiosInstance = axios.create();
+const mockAdapter = new MockAdapter(isolatedAxiosInstance);
 
 // This sets the mock adapter on the default instance
 export { isolatedAxiosInstance as axios };
-export default new MockAdapter(isolatedAxiosInstance);
+export default mockAdapter;
