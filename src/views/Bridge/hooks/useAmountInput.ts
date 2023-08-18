e8fc3ddec70362d574f1432cc81c491e10f90176
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-08-18 11:44:09 +0200

    refactor: extract bridge amount handling into separate hook

---
+++ src/views/Bridge/hooks/useAmountInput.ts
@@ -0,0 +1,89 @@
+import { useState, useEffect, useCallback } from "react";
+import { BigNumber, utils } from "ethers";
+
+import { useAmplitude, useBalanceBySymbol } from "hooks";
+import {
+  getConfig,
+  Route,
+  trackMaxButtonClicked,
+  isNumberEthersParseable,
+} from "utils";
+
+import { validateBridgeAmount } from "../utils";
+
+export function useAmountInput(selectedRoute: Route) {
+  const [userAmountInput, setUserAmountInput] = useState("");
+  const [parsedAmount, setParsedAmount] = useState<BigNumber | undefined>(
+    undefined
+  );
+
+  const { addToAmpliQueue } = useAmplitude();
+
+  const { balance } = useBalanceBySymbol(
+    selectedRoute.fromTokenSymbol,
+    selectedRoute.fromChain
+  );
+
+  const token = getConfig().getTokenInfoBySymbol(
+    selectedRoute.fromChain,
+    selectedRoute.fromTokenSymbol
+  );
+
+  const handleClickMaxBalance = useCallback(() => {
+    if (balance) {
+      setUserAmountInput(utils.formatUnits(balance, token.decimals));
+      addToAmpliQueue(() => {
+        trackMaxButtonClicked("bridgeForm");
+      });
+    }
+  }, [balance, token.decimals, addToAmpliQueue]);
+
+  const handleChangeAmountInput = useCallback((changedInput: string) => {
+    setUserAmountInput(changedInput);
+  }, []);
+
+  useEffect(() => {
+    setUserAmountInput("");
+    setParsedAmount(undefined);
+  }, [selectedRoute]);
+
+  useEffect(() => {
+    setParsedAmount(
+      isNumberEthersParseable(userAmountInput)
+        ? utils.parseUnits(userAmountInput, token.decimals)
+        : undefined
+    );
+  }, [userAmountInput, token.decimals]);
+
+  return {
+    handleChangeAmountInput,
+    handleClickMaxBalance,
+    userAmountInput,
+    parsedAmount,
+    balance,
+  };
+}
+
+export function useValidAmount(
+  parsedAmount?: BigNumber,
+  isAmountTooLow?: boolean,
+  currentBalance?: BigNumber,
+  maxDeposit?: BigNumber
+) {
+  const [validationError, setValidationError] = useState<string | undefined>();
+
+  useEffect(() => {
+    const { errorMessage } = validateBridgeAmount(
+      parsedAmount,
+      isAmountTooLow,
+      currentBalance,
+      maxDeposit
+    );
+    setValidationError(errorMessage);
+  }, [parsedAmount, isAmountTooLow, currentBalance, maxDeposit]);
+
+  return {
+    amountValidationError: validationError,
+    isAmountValid: !Boolean(validationError),
+  };
+}
