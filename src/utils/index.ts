e137e55a2530c2139a48579bf8d545661ee0155e
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-08-18 11:43:18 +0200

    refactor: remove obsolete chains utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,6 @@ export * from "./constants";
 export * from "./address";
 export * from "./format";
 export * from "./bridge";
-export * from "./chains";
 export * from "./errors";
 export * from "./onboard";
 export * from "./math";
