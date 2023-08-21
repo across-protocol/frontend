55c2fc557dfe4412e1bc3daf1ea7172997126adb
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-08-21 11:51:17 +0200

    fix: only reset amount input on token change

--- src/views/Bridge/hooks/useAmountInput.ts
+++ src/views/Bridge/hooks/useAmountInput.ts
@@ -40,7 +40,7 @@ export function useAmountInput(selectedRoute: Route) {
   useEffect(() => {
     setUserAmountInput("");
     setParsedAmount(undefined);
-  }, [selectedRoute]);
+  }, [selectedRoute.fromTokenSymbol]);
 
   useEffect(() => {
     try {
