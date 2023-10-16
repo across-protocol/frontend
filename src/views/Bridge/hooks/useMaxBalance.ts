de11ae35751d8dc4a5e396b328d025f16da658b1
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-10-16 13:57:18 +0200

    fix: use pre-computed gas usage for deposit max amounts

--- src/views/Bridge/hooks/useMaxBalance.ts
+++ src/views/Bridge/hooks/useMaxBalance.ts
@@ -1,17 +1,8 @@
 import { useQuery } from "react-query";
-import { BigNumber, providers, constants, utils } from "ethers";
+import { BigNumber, constants } from "ethers";
 
 import { useBalanceBySymbol, useConnection } from "hooks";
-import {
-  getConfig,
-  Route,
-  max,
-  getProvider,
-  fallbackEstimatedGasCosts,
-} from "utils";
-import { getPaddedGasEstimation } from "utils/transactions";
-
-const config = getConfig();
+import { Route, max, getProvider, gasExpenditureDeposit } from "utils";
 
 export function useMaxBalance(selectedRoute: Route) {
   const { balance } = useBalanceBySymbol(
@@ -35,14 +26,9 @@ export function useMaxBalance(selectedRoute: Route) {
           selectedRoute.fromTokenSymbol !== "ETH"
             ? balance
             : // For ETH, we need to take the gas costs into account before setting the max. bridgable amount
-              await estimateGasCostsForDeposit(selectedRoute, signer)
-                .then((estimatedGasCosts) =>
-                  max(balance.sub(estimatedGasCosts), 0)
-                )
-                .catch((err) => {
-                  console.error(err);
-                  return max(balance.sub(fallbackEstimatedGasCosts), 0);
-                });
+              await estimateGasCostsForDeposit(selectedRoute).then(
+                (estimatedGasCosts) => max(balance.sub(estimatedGasCosts), 0)
+              );
       } else {
         maxBridgeAmount = constants.Zero;
       }
@@ -51,38 +37,13 @@ export function useMaxBalance(selectedRoute: Route) {
     },
     {
       enabled: Boolean(account && balance && signer),
+      retry: true,
     }
   );
 }
 
-async function estimateGasCostsForDeposit(
-  selectedRoute: Route,
-  signer: providers.JsonRpcSigner
-) {
+async function estimateGasCostsForDeposit(selectedRoute: Route) {
   const provider = getProvider(selectedRoute.fromChain);
-  const spokePool = config.getSpokePool(selectedRoute.fromChain, signer);
-  const tokenInfo = config.getTokenInfoByAddress(
-    selectedRoute.fromChain,
-    selectedRoute.fromTokenAddress
-  );
-  const amount = utils.parseUnits("0.000001", tokenInfo.decimals);
-  const argsForEstimation = {
-    recipient: await signer.getAddress(),
-    originToken: tokenInfo.address,
-    amount,
-    destinationChain: selectedRoute.toChain,
-    relayerFeePct: 0,
-    quoteTimestamp: BigNumber.from(Math.floor(Date.now() / 1000)).sub(60 * 60),
-    message: "0x",
-    maxCount: constants.MaxUint256,
-  };
-  const paddedGasEstimation = await getPaddedGasEstimation(
-    selectedRoute.fromChain,
-    spokePool,
-    "deposit",
-    ...Object.values(argsForEstimation),
-    { value: selectedRoute.isNative ? amount : 0 }
-  );
   const gasPrice = await provider.getGasPrice();
-  return gasPrice.mul(paddedGasEstimation);
+  return gasPrice.mul(gasExpenditureDeposit);
 }
