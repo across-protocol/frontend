bc6c8f73b975ca0f6a1025a16ca36003ac8730c9
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-10-24 16:07:35 -0400

    chore: remove blockTag

--- api/account-balance.ts
+++ api/account-balance.ts
@@ -12,7 +12,6 @@ const AccountBalanceQueryParamsSchema = type({
   token: optional(validAddress()),
   account: validAddress(),
   chainId: min(integer(), 1),
-  blockTag: optional(min(integer(), 0)),
 });
 
 type AccountBalanceQueryParams = Infer<typeof AccountBalanceQueryParamsSchema>;
@@ -31,34 +30,28 @@ const handler = async (
     // Validate the query parameters
     assert(query, AccountBalanceQueryParamsSchema);
     // Deconstruct the query parameters
-    let { token, account, chainId, blockTag } = query;
+    let { token, account, chainId } = query;
     // Rely on the utils to query the balance of either the native
     // token or an ERC20 token.
-    const balance = await getBalance(chainId, account, token, blockTag);
+    const balance = await getBalance(chainId, account, token);
     // Package the response
     const result = {
       balance: balance.toString(),
       account: account,
       token: token,
       isNative: token === undefined,
-      tag: blockTag,
+      tag: "latest",
     };
-    // Determine the age of the caching of the response
-    // 1. If the blockTag is specified, then we can cache for 5 minutes (300 seconds)
-    // 2. If the blockTag is not specified, the "latest" block is used, and we should
-    //    only cache for 10 seconds.
-    const cachingTime = blockTag ? 300 : 10;
     // Log the response
     logger.debug({
       at: "AccountBalance",
       message: "Response data",
       responseJson: result,
-      secondsCached: cachingTime,
     });
     // Set the caching headers that will be used by the CDN.
     response.setHeader(
       "Cache-Control",
-      `s-maxage=${cachingTime}, stale-while-revalidate=${cachingTime}}`
+      "s-maxage=150, stale-while-revalidate=150"
     );
     // Return the response
     response.status(200).json(result);
