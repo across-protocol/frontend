04e68e049490dd8e77aaffe52fd8283c181e83e9
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-10-27 12:05:25 -0400

    improve: pipe recipient address into suggested fees

--- src/views/Transactions/hooks/useSpeedUp.tsx
+++ src/views/Transactions/hooks/useSpeedUp.tsx
@@ -19,7 +19,8 @@ export function useSpeedUp(transfer: Deposit, token: Token) {
     BigNumber.from(transfer.amount),
     transfer.sourceChainId,
     transfer.destinationChainId,
-    token.symbol
+    token.symbol,
+    transfer.depositorAddr
   );
 
   const [speedUpTxLink, setSpeedUpTxLink] = useState<string>("");
