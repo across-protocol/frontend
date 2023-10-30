701999168ad0abbbe33ded1345ab9b8b24f16776
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-10-30 11:38:17 +0100

    fixup

--- src/views/Referrals/comp/index.ts
+++ src/views/Referrals/comp/index.ts
@@ -1,5 +1,3 @@
-export { default as RewardBreakdown } from "./RewardBreakdown";
-export { default as RewardHero } from "./RewardHero";
 export { default as RewardTable } from "../../../components/RewardTable";
 export { default as RewardReferral } from "./RewardReferral";
 export { default as ConnectTableOverlay } from "./ConnectTableOverlay";
