bb0253434c6027b6fd7c34db3e8ada1230452353
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-11-01 16:27:26 -0400

    feat: add table logic

--- src/views/Bridge/components/EstimatedTable.tsx
+++ src/views/Bridge/components/EstimatedTable.tsx
@@ -4,15 +4,20 @@ import { BigNumber } from "ethers";
 import { Text } from "components/Text";
 import { Tooltip } from "components/Tooltip";
 import { ReactComponent as InfoIcon } from "assets/icons/info-16.svg";
+import { ReactComponent as UnstyledArrowIcon } from "assets/icons/arrow-16.svg";
 
 import {
   bridgedUSDCSymbolsMap,
   capitalizeFirstLetter,
+  formatUnits,
+  formatUnitsFnBuilder,
   getChainInfo,
+  getToken,
   TokenInfo,
 } from "utils";
 
 import TokenFee from "./TokenFee";
+import { useState } from "react";
 import { ConvertTokensToBaseCurrencyType } from "../hooks/useBridge";
 
 type EstimatedTableProps = {
@@ -29,6 +34,36 @@ type EstimatedTableProps = {
   depositReferralReward?: BigNumber;
 };
 
+const PriceFee = ({
+  tokenFee,
+  baseCurrencyFee,
+  token,
+  highlightTokenFee = false,
+}: {
+  tokenFee?: BigNumber;
+  baseCurrencyFee?: BigNumber;
+  token: TokenInfo;
+  highlightTokenFee?: boolean;
+}) => (
+  <BaseCurrencyWrapper>
+    {baseCurrencyFee && tokenFee && (
+      <Text size="md" color="grey-400">
+        {`$${formatUnits(baseCurrencyFee, 18)}`}
+      </Text>
+    )}
+    {tokenFee ? (
+      <Text size="md" color={highlightTokenFee ? "primary" : "white"}>
+        {`${formatUnits(tokenFee, token.decimals)} ${token.symbol}`}
+      </Text>
+    ) : (
+      <Text size="md" color="grey-400">
+        -
+      </Text>
+    )}
+    {tokenFee && <TokenSymbol src={token.logoURI} />}
+  </BaseCurrencyWrapper>
+);
+
 const EstimatedTable = ({
   fromChainId,
   toChainId,
@@ -38,55 +73,166 @@ const EstimatedTable = ({
   token,
   totalReceived,
   receiveToken,
-}: EstimatedTableProps) => (
-  <Wrapper>
-    <Row>
-      <Text size="md" color="grey-400">
-        Time to{" "}
-        <WhiteText>
-          {capitalizeFirstLetter(getChainInfo(toChainId).name)}
-        </WhiteText>
-      </Text>
-      <Text size="md" color="grey-400">
-        {estimatedTime ? estimatedTime : "-"}
-      </Text>
-    </Row>
-    <Row>
-      <Text size="md" color="grey-400">
-        Destination gas fee
-      </Text>
-      <Text size="md" color="grey-400">
-        {gasFee ? <TokenFee amount={gasFee} token={token} /> : "-"}
-      </Text>
-    </Row>
-    <Row>
-      <Text size="md" color="grey-400">
-        Bridge fee
-      </Text>
-      <Text size="md" color="grey-400">
-        {bridgeFee ? <TokenFee amount={bridgeFee} token={token} /> : "-"}
-      </Text>
-    </Row>
-    <Row>
-      <Text size="md" color="grey-400">
-        You will receive
-      </Text>
-      <Text size="md" color="grey-400">
-        {totalReceived ? (
-          <TotalReceive
-            totalReceived={totalReceived}
-            token={token}
-            receiveToken={receiveToken}
-            srcChainId={fromChainId}
-            destinationChainId={toChainId}
-          />
-        ) : (
-          "-"
-        )}
-      </Text>
-    </Row>
-  </Wrapper>
-);
+  convertTokensToBaseCurrency: { l1: convertL1Token, acx: convertACXToken },
+  depositReferralReward,
+}: EstimatedTableProps) => {
+  const [isDetailedFeesAvailable, setIsDetailedFeesAvailable] = useState(false);
+  const ArrowIcon = isDetailedFeesAvailable ? ArrowIconUp : ArrowIconDown;
+  const hasDepositReferralReward =
+    depositReferralReward && depositReferralReward.gt(0);
+
+  const referralRewardAsBaseCurrency = convertACXToken(depositReferralReward);
+  const gasFeeAsBaseCurrency = convertL1Token(gasFee);
+  const bridgeFeeAsBaseCurrency = convertL1Token(bridgeFee);
+  const netFeeAsBaseCurrency =
+    gasFeeAsBaseCurrency && bridgeFeeAsBaseCurrency
+      ? gasFeeAsBaseCurrency.add(bridgeFeeAsBaseCurrency)
+      : undefined;
+  const formatUsd = formatUnitsFnBuilder(18);
+
+  return (
+    <Wrapper>
+      {hasDepositReferralReward && (
+        <Row>
+          <Text size="md" color="grey-400">
+            Across Referral Rewards
+          </Text>
+          <ReferralRewardWrapper>
+            <PriceFee
+              token={getToken("ACX")}
+              tokenFee={depositReferralReward}
+              baseCurrencyFee={referralRewardAsBaseCurrency}
+            />
+          </ReferralRewardWrapper>
+        </Row>
+      )}
+      <Row>
+        <Text size="md" color="grey-400">
+          Time to{" "}
+          <WhiteText>
+            {capitalizeFirstLetter(getChainInfo(toChainId).name)}
+          </WhiteText>
+        </Text>
+        <Text size="md" color="grey-400">
+          {estimatedTime ? estimatedTime : "-"}
+        </Text>
+      </Row>
+      <Divider />
+      <ClickableRow onClick={() => setIsDetailedFeesAvailable((v) => !v)}>
+        <ToolTipWrapper>
+          <ArrowIcon />
+          <Text size="md" color="grey-400">
+            Net fee
+          </Text>
+          <Tooltip
+            body="Total fees less any rewards, in USD"
+            title="Net fee"
+            placement="bottom-start"
+          >
+            <InfoIconWrapper>
+              <InfoIcon />
+            </InfoIconWrapper>
+          </Tooltip>
+        </ToolTipWrapper>
+        <Text size="md" color={netFeeAsBaseCurrency ? "white" : "grey-400"}>
+          {netFeeAsBaseCurrency ? `$ ${formatUsd(netFeeAsBaseCurrency)}` : "-"}
+        </Text>
+      </ClickableRow>
+      {isDetailedFeesAvailable && (
+        <>
+          <ShiftedRow>
+            <Divider />
+          </ShiftedRow>
+          <ShiftedRow>
+            <ToolTipWrapper>
+              <Text size="md" color="grey-400">
+                Bridge fee
+              </Text>
+              <Tooltip
+                title="Bridge fee"
+                body="Fee paid to Across Liquidity Providers and Relayers."
+                placement="bottom-start"
+              >
+                <InfoIconWrapper>
+                  <InfoIcon />
+                </InfoIconWrapper>
+              </Tooltip>
+            </ToolTipWrapper>
+            <PriceFee
+              token={token}
+              tokenFee={bridgeFee}
+              baseCurrencyFee={bridgeFeeAsBaseCurrency}
+            />
+          </ShiftedRow>
+          <ShiftedRow>
+            <ToolTipWrapper>
+              <Text size="md" color="grey-400">
+                Destination gas fee
+              </Text>
+              <Tooltip
+                title="Destination gas fee"
+                body="Fee to cover gas for destination chain fill transaction."
+                placement="bottom-start"
+              >
+                <InfoIconWrapper>
+                  <InfoIcon />
+                </InfoIconWrapper>
+              </Tooltip>
+            </ToolTipWrapper>
+            <PriceFee
+              token={token}
+              tokenFee={gasFee}
+              baseCurrencyFee={gasFeeAsBaseCurrency}
+            />
+          </ShiftedRow>
+          {hasDepositReferralReward && (
+            <ShiftedRow>
+              <ToolTipWrapper>
+                <Text size="md" color="grey-400">
+                  ACX Referral Reward
+                </Text>
+                <Tooltip
+                  title="ACX Referral Reward"
+                  body="Estimate of ACX earned on this transfer from Referral Rewards program."
+                  placement="bottom-start"
+                >
+                  <InfoIconWrapper>
+                    <InfoIcon />
+                  </InfoIconWrapper>
+                </Tooltip>
+              </ToolTipWrapper>
+              <PriceFee
+                token={getToken("ACX")}
+                tokenFee={depositReferralReward}
+                baseCurrencyFee={referralRewardAsBaseCurrency}
+                highlightTokenFee
+              />
+            </ShiftedRow>
+          )}
+        </>
+      )}
+      <Divider />
+      <Row>
+        <Text size="md" color="grey-400">
+          You will receive
+        </Text>
+        <Text size="md" color="grey-400">
+          {totalReceived ? (
+            <TotalReceive
+              totalReceived={totalReceived}
+              token={token}
+              receiveToken={receiveToken}
+              srcChainId={fromChainId}
+              destinationChainId={toChainId}
+            />
+          ) : (
+            "-"
+          )}
+        </Text>
+      </Row>
+    </Wrapper>
+  );
+};
 
 function TotalReceive({
   totalReceived,
@@ -164,6 +310,14 @@ const Row = styled.div`
   width: 100%;
 `;
 
+const ClickableRow = styled(Row)`
+  cursor: pointer;
+`;
+
+const ShiftedRow = styled(Row)`
+  padding-left: 24px;
+`;
+
 const WhiteText = styled.span`
   color: #e0f3ff;
 `;
@@ -180,4 +334,67 @@ const WarningInfoIcon = styled(InfoIcon)`
   path {
     stroke: #f9d26c;
   }
+`;
+
+const ToolTipWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+`;
+
+const InfoIconWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 16px;
+  width: 16px;
+`;
+
+const ReferralRewardWrapper = styled.div`
+  //Layout
+  display: flex;
+  padding: 6px 12px;
+  align-items: center;
+  gap: 4px;
+
+  // Style
+  border-radius: 22px;
+  border: 1px solid var(--color-interface-aqua-15, rgba(108, 249, 216, 0.15));
+  background: var(--color-interface-aqua-5, rgba(108, 249, 216, 0.05));
+`;
+
+const BaseCurrencyWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Divider = styled.div`
+  height: 1px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+  background: var(--color-neutrals-grey-600, #3e4047);
+  width: 100%;
+`;
+
+const ArrowIconDown = styled(UnstyledArrowIcon)`
+  path {
+    stroke: #9daab2;
+  }
+`;
+
+const ArrowIconUp = styled(ArrowIconDown)`
+  rotate: 180deg;
+`;
+
+const TokenSymbol = styled.img`
+  width: 16px;
+  height: 16px;
 `;
