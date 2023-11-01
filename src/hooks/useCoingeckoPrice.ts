03a5a9bead054b43336451e4a36440e37fc1965c
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-11-01 16:23:36 -0400

    feat: export convert function with coingecko

--- src/hooks/useCoingeckoPrice.ts
+++ src/hooks/useCoingeckoPrice.ts
@@ -1,4 +1,9 @@
+import { utils } from "@across-protocol/sdk-v2";
+import { BigNumber } from "ethers";
+import { useCallback } from "react";
 import { useQuery } from "react-query";
+import { fixedPointAdjustment, getTokenByAddress } from "utils";
+import { ConvertDecimals } from "utils/convertdecimals";
 import getApiEndpoint from "utils/serverless-api";
 
 export function useCoingeckoPrice(
@@ -6,11 +11,34 @@ export function useCoingeckoPrice(
   baseCurrency: string,
   enabled: boolean = true
 ) {
-  return useQuery(
+  const query = useQuery(
     ["price", l1Token, baseCurrency],
     async () => getApiEndpoint().coingecko(l1Token, baseCurrency),
     {
       enabled,
     }
   );
+  const convertTokenToBaseCurrency = useCallback(
+    (amount?: BigNumber) => {
+      // Resolve the price as an 18 decimal big number which takes
+      // the form {PRICE} / 1 token
+      const price = query.data?.price;
+      // If the price is not defined yet, return undefined
+      if (!utils.isDefined(price) || !utils.isDefined(amount)) {
+        return undefined;
+      }
+      // Resolve token decimals
+      const { decimals } = getTokenByAddress(l1Token);
+      // Convert the amount to the base currency
+      const convertedAmount = ConvertDecimals(decimals, 18)(amount);
+      // Return the product of the price and the amount
+      return price.mul(convertedAmount).div(fixedPointAdjustment);
+    },
+    [l1Token, query.data]
+  );
+
+  return {
+    ...query,
+    convertTokenToBaseCurrency,
+  };
 }
