9ff792fc44ce40a6c8445ce5afda456309cb8bba
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-11-13 17:03:22 -0500

    improve: centralize reward page

--- src/views/Rewards/components/RewardProgramSection.tsx
+++ src/views/Rewards/components/RewardProgramSection.tsx
@@ -2,20 +2,25 @@ import styled from "@emotion/styled";
 import SectionWrapper from "components/SectionTitleWrapperV2/SectionWrapperV2";
 
 import RewardProgramCard from "./RewardProgramCard";
-import { QUERIESV2, getToken, rebateTokensAvailable } from "utils";
+import {
+  QUERIESV2,
+  getToken,
+  rewardProgramsAvailable,
+  rewardPrograms,
+} from "utils";
 
-const DesktopRewardProgramSection = () => {
-  const tokens = ["ACX", ...rebateTokensAvailable];
-  return (
-    <SectionWrapper title="Reward programs">
-      <InnerWrapper>
-        {tokens.map((token) => (
-          <RewardProgramCard token={getToken(token)} key={token} />
-        ))}
-      </InnerWrapper>
-    </SectionWrapper>
-  );
-};
+const DesktopRewardProgramSection = () => (
+  <SectionWrapper title="Reward programs">
+    <InnerWrapper>
+      {rewardProgramsAvailable.map((program) => (
+        <RewardProgramCard
+          token={getToken(rewardPrograms[program].rewardTokenSymbol)}
+          key={rewardPrograms[program].rewardTokenSymbol}
+        />
+      ))}
+    </InnerWrapper>
+  </SectionWrapper>
+);
 
 export default DesktopRewardProgramSection;
 
