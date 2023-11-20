ed661873c6c649f60eced9306481ad748e36ae64
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-11-20 11:19:39 -0500

    chore: refactor imports

--- src/utils/ethers.ts
+++ src/utils/ethers.ts
@@ -1,13 +1,23 @@
-import { Contract, ethers } from "ethers";
+import { Contract, ethers, providers } from "ethers";
 import type { Event } from "ethers";
 import { Provider } from "@ethersproject/providers";
 import { Signer } from "@ethersproject/abstract-signer";
 
 import type { TypedEvent, TypedEventFilter } from "utils/typechain";
 
-export { Provider, Signer, Contract, TypedEventFilter, TypedEvent, Event };
+export {
+  Provider,
+  Signer,
+  Contract,
+  TypedEventFilter,
+  TypedEvent,
+  Event,
+  providers,
+};
 
 export type Result = ethers.utils.Result;
+
+export type ContractTransaction = ethers.ContractTransaction;
 
 export interface Callable {
   (...args: any[]): any;
