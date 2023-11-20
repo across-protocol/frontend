f7d8895f44f4279f78b12dd6448b484709a240f6
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-11-20 15:00:58 -0500

    improve: networking

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,10 +1,11 @@
 /**
  * Resolves the current vercel endpoint dynamically
  * @returns A valid URL of the current endpoint in vercel
+ * @todo These are always going to be the same since we don't export REACT_APP currently.
  */
 export const resolveWebsiteUrl = () => {
-  const url = process.env.VERCEL_URL ?? "across.to";
-  const env = process.env.VERCEL_ENV ?? "development";
+  const url = process.env.REACT_APP_VERCEL_URL ?? "across.to";
+  const env = process.env.REACT_APP_VERCEL_ENV ?? "development";
   switch (env) {
     case "preview":
     case "production":
