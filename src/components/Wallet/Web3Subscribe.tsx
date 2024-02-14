420403c1a1eb64e3f529989d264d258561ba1519
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2024-02-14 11:57:28 -0500

    feat(coinbase): add web3s (#955)
    * feat(coinbase): add web3s

---
+++ src/components/Wallet/Web3Subscribe.tsx
@@ -0,0 +1,48 @@
+import { useCallback, useEffect, useState } from "react";
+import { SubscribeButton } from "./Wallet.styles";
+import { useConnection } from "hooks";
+
+const Web3Subscribe = () => {
+  const { wallet } = useConnection();
+  const [isCBWallet, setIsCBWallet] = useState(false);
+  const [isSubscribed, setISubscribed] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const handleSubscribe = useCallback(() => {
+    if (isCBWallet) {
+      (window as any).CBWSubscribe.toggleSubscription();
+    }
+  }, [isCBWallet]);
+
+  // Check if CBWallet is available
+  useEffect(() => {
+    setIsCBWallet(wallet?.label === "Coinbase Wallet");
+  }, [wallet]);
+
+  useEffect(() => {
+    (window as any).CBWSubscribe.createSubscriptionUI({
+      partnerAddress: "0x7765007Ef1b9B75378F481613D842Fd7613e26f2",
+      partnerName: "Across Protocol",
+      modalTitle: "Subscribe to Across",
+      modalBody: "Subscribe to Across to receive updates and notifications.",
+      onSubscriptionChange: setISubscribed,
+      onLoading: setIsLoading,
+    });
+  }, []);
+
+  const subscribeButtonText = isLoading
+    ? "Loading..."
+    : isSubscribed
+    ? "Unsubscribe"
+    : "Subscribe";
+
+  return isCBWallet ? (
+    <SubscribeButton onClick={handleSubscribe}>
+      {subscribeButtonText}
+    </SubscribeButton>
+  ) : (
+    <></>
+  );
+};
+
+export default Web3Subscribe;
