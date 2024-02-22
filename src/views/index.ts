efcdfefebebd1743ee9b99d7352149885261d08f
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-02-22 09:42:14 +0100

    chore: remove airdrop page (#962)
    * chore: remove airdrop page
    
    * chore: remove airdrop stories

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,6 +2,5 @@ export { default as LiquidityPool } from "./LiquidityPool";
 export { default as Transactions } from "./Transactions";
 export { default as ACXReferralsProgram } from "./RewardsProgram/ACXReferralsProgram";
 export { default as Rewards } from "./Rewards";
-export { default as Airdrop } from "./Airdrop";
 export { default as NotFound } from "./NotFound";
 export { default as Staking } from "./Staking";
