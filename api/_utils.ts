2b676df2988843f737337feb065d86e4fff2739f
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-03-26 22:13:26 +0700

    fix: bump balancer sdk to fix pools  (#994)
    * chore: bump to balancer sdk beta
    
    * fix: use custom cg api key
    
    * remove demo key
    
    * add patch

--- api/_utils.ts
+++ api/_utils.ts
@@ -995,6 +995,9 @@ async function getBalancerPoolState(poolTokenAddress: string) {
       },
     },
     rpcUrl: getProvider(HUB_POOL_CHAIN_ID).connection.url,
+    coingecko: {
+      coingeckoApiKey: REACT_APP_COINGECKO_PRO_API_KEY!,
+    },
   };
   const balancer = new BalancerSDK(config);
 
