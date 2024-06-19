53222867b9e21fe934365191c52914b1b2dc9b98
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-06-19 14:43:28 +0200

    fixup

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,13 +23,5 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
   ],
 });
