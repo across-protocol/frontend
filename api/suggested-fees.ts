e8a6a50d376ae44124c8ee8896e236d899b4c472
Melisa Guevara <mguevara@umaproject.org>
2024-06-21 12:53:17 +0200

    fix handle case where lite chain key does not exist in config contract for suggested fees

--- api/suggested-fees.ts
+++ api/suggested-fees.ts
@@ -239,7 +239,8 @@ const handler = async (
       sdk.contracts.acrossConfigStore.Client.parseL1TokenConfig(
         String(rawL1TokenConfig)
       );
-    const liteChainIds = JSON.parse(liteChainIdsEncoded);
+    const liteChainIds =
+      liteChainIdsEncoded === "" ? [] : JSON.parse(liteChainIdsEncoded);
     const originChainIsLiteChain = liteChainIds.includes(computedOriginChainId);
     // We enforce repayment on the origin chain for lite chain deposits
     // so we overwrite the key to get the right rate model
