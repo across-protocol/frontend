c059a878bdb4a32adbe9f9b53284d7ccbbc21811
nicholaspai <npai.nyc@gmail.com>
2024-06-21 11:40:39 -0400

    fix
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- api/_utils.ts
+++ api/_utils.ts
@@ -376,12 +376,14 @@ export const getRouteDetails = (
 export const getTokenByAddress = (
   tokenAddress: string,
   chainId?: number
-): {
-  decimals: number;
-  symbol: string;
-  name: string;
-  addresses: Record<number, string>;
-} => {
+):
+  | {
+      decimals: number;
+      symbol: string;
+      name: string;
+      addresses: Record<number, string>;
+    }
+  | undefined => {
   const matches =
     Object.entries(TOKEN_SYMBOLS_MAP).filter(([_symbol, { addresses }]) =>
       chainId
