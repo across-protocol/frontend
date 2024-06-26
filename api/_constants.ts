479df5483fb78a7a9137a0e44190e1b55073a86a
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2024-06-26 09:03:48 -0400

    hotfix(wsteth-acx): resolve underlying subgraph error (#1085)
    * hotfix(wsteth-acx): resolve underlying subgraph error
    
    ---------
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- api/_constants.ts
+++ api/_constants.ts
@@ -115,6 +115,8 @@ export const defaultRelayerAddressOverridePerToken: Record<
 export const defaultRelayerAddressOverride =
   process.env.RELAYER_ADDRESS_OVERRIDE;
 
+export const graphAPIKey = process.env.GRAPH_API_KEY;
+
 const relayerFeeCapitalCostOverrides: Record<
   string,
   Record<string, Record<string, relayFeeCalculator.CapitalCostConfig>>
