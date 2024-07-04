b6d99a2f63f50b09e9b93292cf8e3bf88e32d865
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-07-04 14:32:10 +0200

    feat: decrease /limits cache time (#1102)
    * feat: decrease /limits cache time
    
    * Update api/limits.ts
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>

--- api/limits.ts
+++ api/limits.ts
@@ -267,9 +267,9 @@ const handler = async (
       message: "Response data",
       responseJson,
     });
-    // Respond with a 200 status code and 4 minutes of cache cache with
-    // a minute of stale-while-revalidate.
-    sendResponse(response, responseJson, 200, 240, 60);
+    // Respond with a 200 status code and 15 seconds of cache with
+    // 45 seconds of stale-while-revalidate.
+    sendResponse(response, responseJson, 200, 15, 45);
   } catch (error: unknown) {
     return handleErrorCondition("limits", response, logger, error);
   }
