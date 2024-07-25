2531a587669f55dfafa20deaffb34211a1f58f41
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-07-25 09:29:07 +0200

    fixup

--- scripts/chain-configs/scroll/index.ts
+++ scripts/chain-configs/scroll/index.ts
@@ -1,8 +1,8 @@
 import { CHAIN_IDs, PUBLIC_NETWORKS } from "@across-protocol/constants";
-// import { utils as sdkUtils } from "@across-protocol/sdk";
+import { utils as sdkUtils } from "@across-protocol/sdk";
 import { ChainConfig } from "../types";
 
-// const { getDeployedAddress } = sdkUtils;
+const { getDeployedAddress } = sdkUtils;
 
 const chainId = CHAIN_IDs.SCROLL;
 const chainInfoBase = PUBLIC_NETWORKS[chainId];
@@ -11,8 +11,7 @@ export default {
   ...chainInfoBase,
   logoPath: "./assets/logo.svg",
   grayscaleLogoPath: "./assets/grayscale-logo.svg",
-  // spokePool: getDeployedAddress("SpokePool", chainId),
-  spokePool: "0x3baD7AD0728f9917d1Bf08af5782dCbD516cDd96",
+  spokePool: getDeployedAddress("SpokePool", chainId),
   chainId,
   publicRpcUrl: "https://rpc.scroll.io",
   blockTimeSeconds: 5,
