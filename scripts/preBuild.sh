06df4e6c239202dd3ce265e48a52573504df2cbe
Evaldo Felipe <contato@evaldofelipe.com>
2024-07-30 13:42:06 -0300

    Update package.json prebuild command
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- scripts/preBuild.sh
+++ scripts/preBuild.sh
@@ -3,12 +3,16 @@
  set -o errexit
  set -o nounset
 
- if [ -z "$GH_TOKEN" ]; then
-     FILE="fill-times.json"
-     FILE_PATH="src/data/fill-times.json"
-     curl -s -o ${FILE_PATH} "https://${GH_TOKEN}@${GH_REPO_URL}/${FILE}"
-     echo "Updated data at ${FILE_PATH}"
- else
-      cat src/data/fill-times-preset.json > ${FILE_PATH}
-      echo "Using default data at ${FILE_PATH}"
- fi
+FILE="fill-times.json"
+FILE_PATH="src/data/fill-times.json"
+
+if [ -n "${GH_TOKEN}" ]; then
+    echo "Getting files from config repo..."
+    curl -o ${FILE_PATH} "https://${GH_TOKEN}@${GH_REPO_URL}/${FILE}"
+    echo "Updated data at ${FILE_PATH}"
+    echo "Done!"
+else
+    cat src/data/fill-times-preset.json > ${FILE_PATH}
+    echo "Using default data at ${FILE_PATH}"
+    echo "Done!"
+fi
