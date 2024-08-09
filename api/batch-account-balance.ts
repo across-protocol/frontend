932ac363751f00fcadf8192185ead49e5ee0e31b
Gerhard Steenkamp <gerhard@umaproject.org>
2024-08-09 15:18:28 +0200

    batch fetch native and erc20 balances

---
+++ api/batch-account-balance.ts
@@ -0,0 +1,103 @@
+import { VercelResponse } from "@vercel/node";
+import { assert, Infer, type, string, array } from "superstruct";
+import { TypedVercelRequest } from "./_types";
+import {
+  callViaMulticall3,
+  getLogger,
+  getProvider,
+  handleErrorCondition,
+  validAddress,
+} from "./_utils";
+import { getConfig } from "utils";
+import { ERC20__factory } from "@across-protocol/contracts";
+
+const BatchAccountBalanceQueryParamsSchema = type({
+  tokenAddress: validAddress(), // can be 0x00 address, ie native
+  addresses: array(validAddress()),
+  chainId: string(),
+});
+
+type BatchAccountBalanceQueryParams = Infer<
+  typeof BatchAccountBalanceQueryParamsSchema
+>;
+
+export type BatchAccountBalanceResponse = Record<string, string>;
+
+const handler = async (
+  { query }: TypedVercelRequest<BatchAccountBalanceQueryParams>,
+  response: VercelResponse
+) => {
+  const logger = getLogger();
+  logger.debug({
+    at: "BatchAccountBalance",
+    message: "Query data",
+    query,
+  });
+  try {
+    // Validate the query parameters
+    assert(query, BatchAccountBalanceQueryParamsSchema);
+    // Deconstruct the query parameters
+    let { tokenAddress, addresses, chainId } = query;
+    const chainIdAsInt = parseInt(chainId);
+    const config = getConfig();
+
+    let balances: BatchAccountBalanceResponse = {};
+
+    const tokenInfo = config.getTokenInfoByAddress(chainIdAsInt, tokenAddress);
+    const provider = getProvider(chainIdAsInt);
+
+    if (tokenInfo.isNative) {
+      // 1. if token address is native, map native balance calls
+      const res = await Promise.all(
+        addresses.map((a) => provider.getBalance(a, "latest"))
+      );
+      addresses.forEach(
+        (address, i) => (balances[address] = res[i].toString())
+      );
+    } else {
+      // 2. if token address is erc20, map token balance calls
+      const multicalls = addresses.map((address) => {
+        const erc20Contract = ERC20__factory.connect(
+          tokenInfo.address,
+          provider
+        );
+        return {
+          contract: erc20Contract,
+          functionName: "balanceOf",
+          args: [address],
+        };
+      });
+
+      // 3. batch through multicall
+      const res = await callViaMulticall3(provider, multicalls, {
+        blockTag: "latest",
+      });
+      addresses.forEach(
+        (address, i) => (balances[address] = res[i].toString())
+      );
+    }
+
+    // Log the response
+    logger.debug({
+      at: "BatchAccountBalance",
+      message: "Response data",
+      responseJson: balances,
+    });
+    // Set the caching headers that will be used by the CDN.
+    response.setHeader(
+      "Cache-Control",
+      "s-maxage=150, stale-while-revalidate=150"
+    );
+    // Return the response
+    response.status(200).json(balances);
+  } catch (error: unknown) {
+    return handleErrorCondition(
+      "batch-account-balance",
+      response,
+      logger,
+      error
+    );
+  }
+};
+
+export default handler;
