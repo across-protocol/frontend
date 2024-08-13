14fd6a1b293d0808fca0bc272750934496a85e47
Gerhard Steenkamp <gerhard@umaproject.org>
2024-08-13 14:42:18 +0200

    use zero address instead

--- api/batch-account-balance.ts
+++ api/batch-account-balance.ts
@@ -1,13 +1,5 @@
 import { VercelResponse } from "@vercel/node";
-import {
-  assert,
-  Infer,
-  type,
-  string,
-  array,
-  literal,
-  union,
-} from "superstruct";
+import { assert, Infer, type, string, array } from "superstruct";
 import { TypedVercelRequest } from "./_types";
 import {
   getBatchBalanceViaMulticall3,
@@ -16,9 +8,10 @@ import {
   handleErrorCondition,
   validAddress,
 } from "./_utils";
+import * as sdk from "@across-protocol/sdk";
 
 const BatchAccountBalanceQueryParamsSchema = type({
-  tokenAddress: union([literal("native"), validAddress()]),
+  tokenAddress: validAddress(),
   addresses: array(validAddress()),
   chainId: string(),
 });
@@ -32,6 +25,18 @@ export type BatchAccountBalanceResponse = Record<
   string
 >;
 
+/**
+ * ## Description
+ * Returns the ERC20 (or native) balances, as BigNumber strings, for a given list of relayer addresses, on a given chain
+ *
+ * ##  Example request:
+ * ```bash
+ * curl {{BASE_URL}}/api/batch-account-balance?chainId=1&tokenAddress=0x0000000000000000000000000000000000000000&addresses=0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D&addresses=0xD25f7e77386F9f797b64E878A3D060956de99163&addresses=0xC54070dA79E7E3e2c95D3a91fe98A42000e65a48
+ * ```
+ * @param tokenAddress - ERC20 address or "zero" address, ie. 0x0000000000000000000000000000000000000000
+ * @param addresses - list of relayer addresses
+ * @param chainId - chainId on which we want to check balances
+ */
 const handler = async (
   { query }: TypedVercelRequest<BatchAccountBalanceQueryParams>,
   response: VercelResponse
@@ -54,7 +59,7 @@ const handler = async (
 
     const provider = getProvider(chainIdAsInt);
 
-    if (tokenAddress === "native") {
+    if (tokenAddress === sdk.constants.ZERO_ADDRESS) {
       // fetch native balances
       const res = await Promise.all(
         addresses.map((a) => provider.getBalance(a, "latest"))
