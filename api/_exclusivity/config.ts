bfadec75f72807cfcd6d9ea7ffdf38ecccbdb9cc
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-08-26 18:09:26 +0700

    feat: add weighted strategy selection

--- api/_exclusivity/config.ts
+++ api/_exclusivity/config.ts
@@ -43,10 +43,29 @@ const EXCLUSIVITY_STRATEGIES = {
  * and selection from on env-based configuration.
  * @returns A handler function that will narrow an array of relayers to a single address.
  */
-export function getStrategy(): RelayerSelector {
-  // @todo: Determine strategy based on configuration.
-  const defaultStrategy = "randomWeighted";
-  return EXCLUSIVITY_STRATEGIES[defaultStrategy];
+export function getStrategy(): { name: string; selectorFn: RelayerSelector } {
+  // @todo: use weights from global config
+  const weights = {
+    none: 0.8,
+    randomWeighted: 0.2,
+  } as const;
+  const keys = Object.keys(weights);
+  const cumulativeWeights = Object.values(weights).map((_, index) =>
+    Object.values(weights)
+      .slice(0, index + 1)
+      .reduce((sum, w) => sum + w, 0)
+  );
+  const randomNumber = Math.random();
+  const strategyIndex = cumulativeWeights.findIndex((w) => randomNumber < w);
+  const strategyKey = keys[strategyIndex];
+
+  return {
+    name: strategyKey,
+    selectorFn:
+      EXCLUSIVITY_STRATEGIES[
+        strategyKey as keyof typeof EXCLUSIVITY_STRATEGIES
+      ],
+  };
 }
 
 /**
