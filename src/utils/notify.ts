4e1fd97cfcb23e7ff61afbd8a6774948cbcac589
dima-safin-2025 <dima.safin.2025@mail.ru>
2024-08-29 17:10:43 +0800

    docs: Improve JSDoc comments in notify.ts

--- src/utils/notify.ts
+++ src/utils/notify.ts
@@ -48,11 +48,15 @@ export const notificationEmitter = async (
 
 /**
  * Calls and waits on the Notify API to resolve the status of a TX if the chain is supported by Onboard
+ *
+ * @param requiredChainId The ID of the chain that the transaction was executed on.
  * @param tx The transaction to wait for
  * @param notify The BNC Notify API that is used to handle the UI visualization
  * @param timingBuffer An optional waiting time in milliseconds to wait to resolve this promise on a successful tx confirmation in Notify (Default: 5000ms)
  * @param ignoreErrors An optional parameter to ignore tx failure and return successful
- **/
+ *
+ * @throws {Error} If the transaction fails and `ignoreErrors` is set to `false`.
+ */
 export const waitOnTransaction = async (
   requiredChainId: number,
   tx: ContractTransaction,
