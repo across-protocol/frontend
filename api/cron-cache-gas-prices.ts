7db36a9f6fb006b6cbb5e9b0b641fcfb83d91078
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2024-10-08 13:50:48 -0400

    improve: add exponential smoothing to cached gas fees (#1238)
    * improve: add exponential smoothing to cached gas fees
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * nit: wrap in promise.all
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    * nit: docs
    
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>
    
    * chore: hoist cache abstraction out of loop
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    
    ---------
    
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>

--- api/cron-cache-gas-prices.ts
+++ api/cron-cache-gas-prices.ts
@@ -3,17 +3,22 @@ import { TypedVercelRequest } from "./_types";
 import {
   HUB_POOL_CHAIN_ID,
   getLogger,
+  getMaxFeePerGas,
   handleErrorCondition,
   latestGasPriceCache,
 } from "./_utils";
 import { UnauthorizedError } from "./_errors";
 
 import mainnetChains from "../src/data/chains_1.json";
+import { fixedPointAdjustment } from "utils";
+import { utils } from "@across-protocol/sdk";
 
 const updateIntervalsSecPerChain = {
   default: 10,
   1: 12,
 };
+
+const SECONDS_OF_GAS_AVERAGE = 2 * 60 * 60; // 2 hours
 
 const maxDurationSec = 60;
 
@@ -50,27 +55,44 @@ const handler = async (
     // The minimum interval for Vercel Serverless Functions cron jobs is 1 minute.
     // But we want to update gas prices more frequently than that.
     // To circumvent this, we run the function in a loop and update gas prices every
-    // `updateIntervalSec` seconds and stop after `maxDurationSec` seconds (1 minute).
+    // `secondsPerUpdateForChain` seconds and stop after `maxDurationSec` seconds (1 minute).
     const gasPricePromises = mainnetChains.map(async (chain) => {
+      // Resolve how often we will be polling this chain for gas prices
+      const secondsPerUpdateForChain =
+        updateIntervalsSecPerChain[
+          chain.chainId as keyof typeof updateIntervalsSecPerChain
+        ] || updateIntervalsSecPerChain.default;
+      // Resolve the caching abstraction for the current chain
+      const cache = latestGasPriceCache(chain.chainId);
       while (true) {
         const diff = Date.now() - functionStart;
-
         // Stop after `maxDurationSec` seconds
         if (diff >= maxDurationSec * 1000) {
           break;
         }
-
-        // Update gas price every `updateIntervalSec` seconds
-        await latestGasPriceCache(chain.chainId).set();
-
+        // Grab into the cache to get the previous value of the gas price
+        // and directly compute the current value of the gas price.
+        // Note: If the cache is empty, this fn will return a direct
+        //       value from the chain.
+        const [previousGasPrice, currentGasPrice] = await Promise.all([
+          cache.get(),
+          getMaxFeePerGas(chain.chainId),
+        ]);
+        // Resolve and scale our alpha value
+        const alpha = fixedPointAdjustment
+          .mul(secondsPerUpdateForChain)
+          .div(SECONDS_OF_GAS_AVERAGE);
+        // We are computing an exponential weighted moving average of the
+        // gas price. To do this, we will follow the formula:
+        // newValue = (1 - alpha) * previousValue + alpha * currentValue
+        const newGasAverage = fixedPointAdjustment
+          .sub(alpha)
+          .mul(previousGasPrice)
+          .add(alpha.mul(currentGasPrice));
+        // Scale the value back and set it in the cache
+        await cache.set(newGasAverage.div(fixedPointAdjustment));
         // Sleep for `updateIntervalSec` seconds
-        const updateIntervalSec =
-          updateIntervalsSecPerChain[
-            chain.chainId as keyof typeof updateIntervalsSecPerChain
-          ] || updateIntervalsSecPerChain.default;
-        await new Promise((resolve) =>
-          setTimeout(resolve, updateIntervalSec * 1000)
-        );
+        await utils.delay(secondsPerUpdateForChain);
       }
     });
     await Promise.all(gasPricePromises);
