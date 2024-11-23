91c4a5638b19c1aadef643d2bdd967a29f54a392
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-11-23 11:20:29 +0700

    feat: refund on origin flag (#1284)
    * feat: handle refund on origin flag
    
    * fixup
    
    * allow custom refund address via origin
    
    * fix: refund address for leftovers
    
    * add refundToken to response
    
    * fixup

--- api/swap/allowance.ts
+++ api/swap/allowance.ts
@@ -86,6 +86,9 @@ const handler = async (
         gas: originTxGas?.toString(),
         gasPrice: originTxGasPrice?.toString(),
       },
+      refundToken: crossSwapQuotes.crossSwap.refundOnOrigin
+        ? crossSwapQuotes.bridgeQuote.inputToken
+        : crossSwapQuotes.bridgeQuote.outputToken,
       inputAmount:
         crossSwapQuotes.originSwapQuote?.expectedAmountIn.toString() ??
         crossSwapQuotes.bridgeQuote.inputAmount.toString(),
