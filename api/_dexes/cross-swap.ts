3e537e6e4711f3c431f501f42e82b7bc80aedcff
Gerhard Steenkamp <gerhard@umaproject.org>
2024-12-02 12:52:44 +0200

    measure by cross swap type

--- api/_dexes/cross-swap.ts
+++ api/_dexes/cross-swap.ts
@@ -8,6 +8,8 @@ import {
   isOutputTokenBridgeable,
   getBridgeQuoteForMinOutput,
   getSpokePool,
+  Profiler,
+  getLogger,
 } from "../_utils";
 import {
   getBestUniswapCrossSwapQuotesForOutputA2A,
@@ -78,6 +80,10 @@ export async function getCrossSwapQuotes(
 }
 
 export async function getCrossSwapQuotesForOutput(crossSwap: CrossSwap) {
+  const profiler = new Profiler({
+    at: "api/cross-swap#getCrossSwapQuotesForOutput",
+    logger: getLogger(),
+  });
   const crossSwapType = getCrossSwapType({
     inputToken: crossSwap.inputToken.address,
     originChainId: crossSwap.inputToken.chainId,
@@ -86,19 +92,35 @@ export async function getCrossSwapQuotesForOutput(crossSwap: CrossSwap) {
   });
 
   if (crossSwapType === CROSS_SWAP_TYPE.BRIDGEABLE_TO_BRIDGEABLE) {
-    return getCrossSwapQuotesForOutputB2B(crossSwap);
+    return await profiler.measureAsync(
+      getCrossSwapQuotesForOutputB2B(crossSwap),
+      "getCrossSwapQuotesForOutputB2B",
+      crossSwap
+    );
   }
 
   if (crossSwapType === CROSS_SWAP_TYPE.BRIDGEABLE_TO_ANY) {
-    return getCrossSwapQuotesForOutputB2A(crossSwap);
+    return await profiler.measureAsync(
+      getCrossSwapQuotesForOutputB2A(crossSwap),
+      "getCrossSwapQuotesForOutputB2A",
+      crossSwap
+    );
   }
 
   if (crossSwapType === CROSS_SWAP_TYPE.ANY_TO_BRIDGEABLE) {
-    return getCrossSwapQuotesForOutputA2B(crossSwap);
+    return await profiler.measureAsync(
+      getCrossSwapQuotesForOutputA2B(crossSwap),
+      "getCrossSwapQuotesForOutputA2B",
+      crossSwap
+    );
   }
 
   if (crossSwapType === CROSS_SWAP_TYPE.ANY_TO_ANY) {
-    return getCrossSwapQuotesForOutputA2A(crossSwap);
+    return await profiler.measureAsync(
+      getCrossSwapQuotesForOutputA2A(crossSwap),
+      "getCrossSwapQuotesForOutputA2A",
+      crossSwap
+    );
   }
 
   throw new Error("Invalid cross swap type");
