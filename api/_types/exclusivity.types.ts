9b42060694a2ad95a550ef0d93ef2998064f0368
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-12-11 11:34:42 -0600

    chore: types change
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- api/_types/exclusivity.types.ts
+++ api/_types/exclusivity.types.ts
@@ -1,56 +1,17 @@
-export type RelayerFillLimit = {
-  originChainId: number;
-  inputToken: string;
-  destinationChainId: number;
-  outputToken: string;
-  minOutputAmount?: number;
-  maxOutputAmount?: number;
-  minExclusivityPeriod?: number;
-  minProfitThreshold?: number;
-  balanceMultiplier?: number;
-  msgFill?: boolean;
-};
+import { boolean, Infer, object, optional } from "superstruct";
+import { positiveIntStr, validAddress } from "../_utils";
 
-// Example config.
-export const RelayerConfigUpdate: RelayerFillLimit[] = [
-  {
-    originChainId: 1,
-    inputToken: "",
-    destinationChainId: 42161,
-    outputToken: "",
-    minExclusivityPeriod: 20,
-    minProfitThreshold: 0.0003,
-    balanceMultiplier: 0.6,
-    maxOutputAmount: 2500,
-  },
-  {
-    originChainId: 10,
-    inputToken: "",
-    destinationChainId: 42161,
-    outputToken: "",
-    minExclusivityPeriod: 5,
-    minProfitThreshold: 0.0003,
-    balanceMultiplier: 0.6,
-    maxOutputAmount: 2500,
-  },
-  {
-    originChainId: 137,
-    inputToken: "",
-    destinationChainId: 42161,
-    outputToken: "",
-    minExclusivityPeriod: 5,
-    minProfitThreshold: 0.0003,
-    balanceMultiplier: 0.6,
-    maxOutputAmount: 2500,
-  },
-  {
-    originChainId: 324,
-    inputToken: "",
-    destinationChainId: 42161,
-    outputToken: "",
-    minExclusivityPeriod: 5,
-    minProfitThreshold: 0.0003,
-    balanceMultiplier: 0.6,
-    maxOutputAmount: 2500,
-  },
-];
+export const RelayerFillLimitSchema = object({
+  originChainId: positiveIntStr(),
+  destinationChainId: positiveIntStr(),
+  inputToken: validAddress(),
+  outputToken: validAddress(),
+  minOutputAmount: optional(positiveIntStr()),
+  maxOutputAmount: optional(positiveIntStr()),
+  minExclusivityPeriod: optional(positiveIntStr()),
+  minProfitThreshold: optional(positiveIntStr()),
+  balanceMultiplier: optional(positiveIntStr()),
+  msgFill: boolean(),
+});
+
+export type RelayerFillLimit = Infer<typeof RelayerFillLimitSchema>;
