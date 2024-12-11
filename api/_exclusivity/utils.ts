36d324f439018084234267d05ae54f245c7be433
Paul <108695806+pxrl@users.noreply.github.com>
2024-12-11 22:34:09 +0000

    Add basic sorting/parsing

--- api/_exclusivity/utils.ts
+++ api/_exclusivity/utils.ts
@@ -1,15 +1,18 @@
 import { ethers } from "ethers";
+import { bnZero } from "../../src/utils/sdk";
+import { RelayerFillLimit } from "../_types";
 
 export const MAX_MESSAGE_AGE_SECONDS = 300;
 
 // TODO: get this from gh
-export const getWhiteListedRelayers = () => {
-  return [
-    "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D", // dev wallet
-  ];
-};
+export const whiteListedRelayers = [
+  "0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D", // dev wallet
+];
 
-export const getRelayerFromSignature = (signature: string, message: string) => {
+export const getRelayerFromSignature = (
+  signature: string,
+  message: string
+) => {
   return ethers.utils.verifyMessage(message, signature);
 };
 
@@ -20,3 +23,35 @@ export const isTimestampValid = (
   const currentTime = Math.floor(Date.now() / 1000);
   return currentTime - timestamp <= maxAgeSeconds;
 };
+
+export async function updateLimits(
+  relayer: string,
+  limits: RelayerFillLimit[]
+): Promise<void> {
+  const sortedLimits = limits
+    .map(({ minOutputAmount, maxOutputAmount, ...rest }) => ({
+      minOutputAmount: ethers.BigNumber.from(minOutputAmount),
+      maxOutputAmount: ethers.BigNumber.from(maxOutputAmount),
+      ...rest,
+    }))
+    .sort(({ minOutputAmount: minA }, { minOutputAmount: minB }) =>
+      minA.sub(minB).gte(bnZero) ? 1 : -1
+    );
+
+  const sorted = sortedLimits
+    .slice(1)
+    .every(({ minOutputAmount, maxOutputAmount }, idx) => {
+      const { maxOutputAmount: prevMax } = sortedLimits[idx];
+      return maxOutputAmount.gt(minOutputAmount) && minOutputAmount.gt(prevMax);
+    });
+
+  if (!sorted) {
+    throw new Error("Relayer limits are overlapping");
+  }
+
+  // todo: Push each limit entry to the backend cache/db.
+  // The config types need to be reverted to strings as numbers.
+  relayer; 
+
+  return;
+}
