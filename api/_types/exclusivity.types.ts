4b02044446b1474786660a6f5eba4ddbefd0d0a5
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-12-11 11:38:46 -0600

    nit: reintroduce comment
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- api/_types/exclusivity.types.ts
+++ api/_types/exclusivity.types.ts
@@ -15,3 +15,47 @@ export const RelayerFillLimitSchema = object({
 });
 
 export type RelayerFillLimit = Infer<typeof RelayerFillLimitSchema>;
+
+// // Example config.
+// export const RelayerConfigUpdate: RelayerFillLimit[] = [
+//   {
+//     originChainId: 1,
+//     inputToken: "",
+//     destinationChainId: 42161,
+//     outputToken: "",
+//     minExclusivityPeriod: 20,
+//     minProfitThreshold: 0.0003,
+//     balanceMultiplier: 0.6,
+//     maxOutputAmount: 2500,
+//   },
+//   {
+//     originChainId: 10,
+//     inputToken: "",
+//     destinationChainId: 42161,
+//     outputToken: "",
+//     minExclusivityPeriod: 5,
+//     minProfitThreshold: 0.0003,
+//     balanceMultiplier: 0.6,
+//     maxOutputAmount: 2500,
+//   },
+//   {
+//     originChainId: 137,
+//     inputToken: "",
+//     destinationChainId: 42161,
+//     outputToken: "",
+//     minExclusivityPeriod: 5,
+//     minProfitThreshold: 0.0003,
+//     balanceMultiplier: 0.6,
+//     maxOutputAmount: 2500,
+//   },
+//   {
+//     originChainId: 324,
+//     inputToken: "",
+//     destinationChainId: 42161,
+//     outputToken: "",
+//     minExclusivityPeriod: 5,
+//     minProfitThreshold: 0.0003,
+//     balanceMultiplier: 0.6,
+//     maxOutputAmount: 2500,
+//   },
+// ];
