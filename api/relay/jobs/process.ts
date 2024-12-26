0a70028eaada35c5be7d265626b209e04a87f3a7
Dong-Ha Kim <dongha.kim210@gmail.com>
2024-12-26 13:21:30 +0100

    fixup

--- api/relay/jobs/process.ts
+++ api/relay/jobs/process.ts
@@ -16,7 +16,7 @@ import { strategiesByName } from "../_strategies";
 import { BaseRelayRequestBodySchema } from "../index";
 
 const messageReceiver = new Receiver({
-  currentSigningKey: process.env.QSTASH_SIGNING_KEY!,
+  currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY!,
   nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY!,
 });
 
