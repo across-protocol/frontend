cb7f46d810d09fa7f5ceaf009530686b77224556
James Morris, MS <96435344+james-a-morris@users.noreply.github.com>
2025-01-02 08:50:24 -0500

    feat(ink): deploy ink (#1342)

--- scripts/chain-configs/index.ts
+++ scripts/chain-configs/index.ts
@@ -5,6 +5,7 @@ export { default as BASE } from "./base";
 export { default as BASE_SEPOLIA } from "./base-sepolia";
 export { default as BLAST } from "./blast";
 export { default as BLAST_SEPOLIA } from "./blast-sepolia";
+export { default as INK } from "./ink";
 export { default as LINEA } from "./linea";
 export { default as LISK } from "./lisk";
 export { default as LISK_SEPOLIA } from "./lisk-sepolia";
