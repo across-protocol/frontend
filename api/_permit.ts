73b19f8b787fa4751ff9a9efa7ddcdb2f12c041a
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-01-06 23:50:00 +0100

    feat: unified swap endpoint (#1353)
    * feat: unified swap endpoint
    
    * refactor: change DEFAULT_PERMIT_DEADLINE to a constant in PermitSwapQueryParamsSchema
    
    * fixup

--- api/_permit.ts
+++ api/_permit.ts
@@ -3,6 +3,20 @@ import { BigNumberish, ethers } from "ethers";
 import { getProvider } from "./_utils";
 import { ERC20_PERMIT_ABI } from "./_abis";
 
+export class PermitNotSupportedError extends Error {
+  constructor(tokenAddress: string, cause?: Error) {
+    super(`ERC-20 contract ${tokenAddress} does not support permit`, {
+      cause,
+    });
+  }
+}
+
+export class PermitDomainSeparatorMismatchError extends Error {
+  constructor(tokenAddress: string) {
+    super(`Permit EIP712 domain separator mismatch for ${tokenAddress}`);
+  }
+}
+
 export async function getPermitTypedData(params: {
   tokenAddress: string;
   chainId: number;
@@ -12,76 +26,8 @@ export async function getPermitTypedData(params: {
   deadline: number;
   eip712DomainVersion?: number;
 }) {
-  const provider = getProvider(params.chainId);
-  const erc20Permit = new ethers.Contract(
-    params.tokenAddress,
-    ERC20_PERMIT_ABI,
-    provider
-  );
-
-  const [
-    nameResult,
-    versionFromContractResult,
-    nonceResult,
-    domainSeparatorResult,
-  ] = await Promise.allSettled([
-    erc20Permit.name(),
-    erc20Permit.version(),
-    erc20Permit.nonces(params.ownerAddress),
-    erc20Permit.DOMAIN_SEPARATOR(),
-  ]);
-
-  if (
-    nameResult.status === "rejected" ||
-    nonceResult.status === "rejected" ||
-    domainSeparatorResult.status === "rejected"
-  ) {
-    const error =
-      nameResult.status === "rejected"
-        ? nameResult.reason
-        : nonceResult.status === "rejected"
-          ? nonceResult.reason
-          : domainSeparatorResult.status === "rejected"
-            ? domainSeparatorResult.reason
-            : new Error("Unknown error");
-    throw new Error(
-      `ERC-20 contract ${params.tokenAddress} does not support permit`,
-      {
-        cause: error,
-      }
-    );
-  }
-
-  const name = nameResult.value;
-  const versionFromContract =
-    versionFromContractResult.status === "fulfilled"
-      ? versionFromContractResult.value
-      : undefined;
-  const nonce = nonceResult.value;
-  const domainSeparator = domainSeparatorResult.value;
-
-  const eip712DomainVersion = [1, 2, "1", "2"].includes(versionFromContract)
-    ? Number(versionFromContract)
-    : params.eip712DomainVersion || 1;
-
-  const domainSeparatorHash = ethers.utils.keccak256(
-    ethers.utils.defaultAbiCoder.encode(
-      ["bytes32", "bytes32", "bytes32", "uint256", "address"],
-      [
-        ethers.utils.id(
-          "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-        ),
-        ethers.utils.id(name),
-        ethers.utils.id(eip712DomainVersion.toString()),
-        params.chainId,
-        params.tokenAddress,
-      ]
-    )
-  );
-
-  if (domainSeparator !== domainSeparatorHash) {
-    throw new Error("EIP712 domain separator mismatch");
-  }
+  const { name, domainSeparator, eip712DomainVersion, nonce } =
+    await getPermitArgsFromContract(params);
 
   return {
     domainSeparator,
@@ -125,5 +71,87 @@ export async function getPermitTypedData(params: {
         deadline: String(params.deadline),
       },
     },
+  };
+}
+
+export async function getPermitArgsFromContract(params: {
+  tokenAddress: string;
+  chainId: number;
+  ownerAddress: string;
+  spenderAddress: string;
+  value: BigNumberish;
+  eip712DomainVersion?: number;
+}) {
+  const provider = getProvider(params.chainId);
+  const erc20Permit = new ethers.Contract(
+    params.tokenAddress,
+    ERC20_PERMIT_ABI,
+    provider
+  );
+
+  const [
+    nameResult,
+    versionFromContractResult,
+    nonceResult,
+    domainSeparatorResult,
+  ] = await Promise.allSettled([
+    erc20Permit.name(),
+    erc20Permit.version(),
+    erc20Permit.nonces(params.ownerAddress),
+    erc20Permit.DOMAIN_SEPARATOR(),
+  ]);
+
+  if (
+    nameResult.status === "rejected" ||
+    nonceResult.status === "rejected" ||
+    domainSeparatorResult.status === "rejected"
+  ) {
+    const error =
+      nameResult.status === "rejected"
+        ? nameResult.reason
+        : nonceResult.status === "rejected"
+          ? nonceResult.reason
+          : domainSeparatorResult.status === "rejected"
+            ? domainSeparatorResult.reason
+            : new Error("Unknown error");
+    throw new PermitNotSupportedError(params.tokenAddress, error);
+  }
+
+  const name: string = nameResult.value;
+  const versionFromContract =
+    versionFromContractResult.status === "fulfilled"
+      ? versionFromContractResult.value
+      : undefined;
+  const nonce: BigNumberish = nonceResult.value;
+  const domainSeparator: string = domainSeparatorResult.value;
+
+  const eip712DomainVersion = [1, 2, "1", "2"].includes(versionFromContract)
+    ? Number(versionFromContract)
+    : params.eip712DomainVersion || 1;
+
+  const domainSeparatorHash = ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(
+      ["bytes32", "bytes32", "bytes32", "uint256", "address"],
+      [
+        ethers.utils.id(
+          "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+        ),
+        ethers.utils.id(name),
+        ethers.utils.id(eip712DomainVersion.toString()),
+        params.chainId,
+        params.tokenAddress,
+      ]
+    )
+  );
+
+  if (domainSeparator !== domainSeparatorHash) {
+    throw new PermitDomainSeparatorMismatchError(params.tokenAddress);
+  }
+
+  return {
+    name,
+    domainSeparator,
+    eip712DomainVersion,
+    nonce,
   };
 }
--- api/_transfer-with-auth.ts
+++ api/_transfer-with-auth.ts
@@ -3,6 +3,25 @@ import { getProvider } from "./_utils";
 import { ERC_TRANSFER_WITH_AUTH_ABI } from "./_abis";
 import { utils } from "ethers";
 
+export class TransferWithAuthNotSupportedError extends Error {
+  constructor(tokenAddress: string, cause?: Error) {
+    super(
+      `ERC-20 contract ${tokenAddress} does not support transfer with authorization`,
+      {
+        cause,
+      }
+    );
+  }
+}
+
+export class TransferWithAuthDomainSeparatorMismatchError extends Error {
+  constructor(tokenAddress: string) {
+    super(
+      `Transfer with authorization EIP712 domain separator mismatch for ${tokenAddress}`
+    );
+  }
+}
+
 export function hashDomainSeparator(params: {
   name: string;
   version: string | number;
@@ -36,60 +55,8 @@ export async function getReceiveWithAuthTypedData(params: {
   validAfter?: number;
   eip712DomainVersion?: number;
 }) {
-  const provider = getProvider(params.chainId);
-
-  const erc20Permit = new ethers.Contract(
-    params.tokenAddress,
-    ERC_TRANSFER_WITH_AUTH_ABI,
-    provider
-  );
-
-  const [nameResult, versionFromContractResult, domainSeparatorResult] =
-    await Promise.allSettled([
-      erc20Permit.name(),
-      erc20Permit.version(),
-      erc20Permit.DOMAIN_SEPARATOR(),
-    ]);
-
-  if (
-    nameResult.status === "rejected" ||
-    domainSeparatorResult.status === "rejected"
-  ) {
-    const error =
-      nameResult.status === "rejected"
-        ? nameResult.reason
-        : domainSeparatorResult.status === "rejected"
-          ? domainSeparatorResult.reason
-          : new Error("Unknown error");
-    throw new Error(
-      `Contract ${params.tokenAddress} does not support transfer with authorization`,
-      {
-        cause: error,
-      }
-    );
-  }
-
-  const name = nameResult.value;
-  const versionFromContract =
-    versionFromContractResult.status === "fulfilled"
-      ? versionFromContractResult.value
-      : undefined;
-  const domainSeparator = domainSeparatorResult.value;
-
-  const eip712DomainVersion = [1, 2, "1", "2"].includes(versionFromContract)
-    ? Number(versionFromContract)
-    : params.eip712DomainVersion || 1;
-
-  const domainSeparatorHash = hashDomainSeparator({
-    name,
-    version: eip712DomainVersion,
-    chainId: params.chainId,
-    verifyingContract: params.tokenAddress,
-  });
-
-  if (domainSeparator !== domainSeparatorHash) {
-    throw new Error("EIP712 domain separator mismatch");
-  }
+  const { name, domainSeparator, eip712DomainVersion } =
+    await getReceiveWithAuthArgsFromContract(params);
 
   return {
     domainSeparator,
@@ -120,5 +87,70 @@ export async function getReceiveWithAuthTypedData(params: {
         nonce: params.nonce, // non-sequential nonce, random 32 byte hex string
       },
     },
+  };
+}
+
+export async function getReceiveWithAuthArgsFromContract(params: {
+  tokenAddress: string;
+  chainId: number;
+  ownerAddress: string;
+  spenderAddress: string;
+  value: BigNumberish;
+  eip712DomainVersion?: number;
+}) {
+  const provider = getProvider(params.chainId);
+
+  const erc20Permit = new ethers.Contract(
+    params.tokenAddress,
+    ERC_TRANSFER_WITH_AUTH_ABI,
+    provider
+  );
+
+  const [nameResult, versionFromContractResult, domainSeparatorResult] =
+    await Promise.allSettled([
+      erc20Permit.name(),
+      erc20Permit.version(),
+      erc20Permit.DOMAIN_SEPARATOR(),
+    ]);
+
+  if (
+    nameResult.status === "rejected" ||
+    domainSeparatorResult.status === "rejected"
+  ) {
+    const error =
+      nameResult.status === "rejected"
+        ? nameResult.reason
+        : domainSeparatorResult.status === "rejected"
+          ? domainSeparatorResult.reason
+          : new Error("Unknown error");
+    throw new TransferWithAuthNotSupportedError(params.tokenAddress, error);
+  }
+
+  const name: string = nameResult.value;
+  const versionFromContract =
+    versionFromContractResult.status === "fulfilled"
+      ? versionFromContractResult.value
+      : undefined;
+  const domainSeparator: string = domainSeparatorResult.value;
+
+  const eip712DomainVersion = [1, 2, "1", "2"].includes(versionFromContract)
+    ? Number(versionFromContract)
+    : params.eip712DomainVersion || 1;
+
+  const domainSeparatorHash = hashDomainSeparator({
+    name,
+    version: eip712DomainVersion,
+    chainId: params.chainId,
+    verifyingContract: params.tokenAddress,
+  });
+
+  if (domainSeparator !== domainSeparatorHash) {
+    throw new TransferWithAuthDomainSeparatorMismatchError(params.tokenAddress);
+  }
+
+  return {
+    name,
+    domainSeparator,
+    eip712DomainVersion,
   };
 }
