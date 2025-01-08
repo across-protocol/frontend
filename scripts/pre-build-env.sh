04ea26acdef1638d04bceb2326d64a7715912f8e
Evaldo Felipe <contato@evaldofelipe.com>
2025-01-08 17:45:14 -0300

    update env
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- scripts/pre-build-env.sh
+++ scripts/pre-build-env.sh
@@ -1,18 +1,17 @@
-# #!/bin/bash
+#!/bin/bash
 
-#   set -o errexit
-#   set -o nounset
+  set -o errexit
+  set -o nounset
 
-#  FILE_REMOTE="projects/across/frontend/outputs/output.env"
-#  FILE_PATH="output.env"
-#  GH_REPO="umaprotocol/git-env"
+ FILE_REMOTE="projects/across/frontend/outputs/output.env"
+ FILE_PATH="output.env"
 
-#  if [ -n "${GH_TOKEN}" ]; then
-#      echo "Getting env files from config repo..."
-#      curl -o ${FILE_PATH} "https://${GH_TOKEN}@${GH_REPO}/${FILE_REMOTE}"
-#      echo "Updated data at ${FILE_PATH}"
-#      echo $GIT_ENV_EXPORTED
-#      echo "Done!"
-#  else
-#      echo "No env exported"
-#  fi
+ if [ -n "${GH_TOKEN}" ]; then
+     echo "Getting env files from config repo..."
+     curl -o ${FILE_PATH} "https://${GH_TOKEN}@${GIT_ENV_REPO}/${FILE_REMOTE}"
+     echo "Updated data at ${FILE_PATH}"
+     echo $GIT_ENV_EXPORTED
+     echo "Done!"
+ else
+     echo "No env exported"
+ fi
