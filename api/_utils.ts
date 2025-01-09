5cdec8800539f04985f31d009e6477b6333f926e
nicholaspai <npai.nyc@gmail.com>
2025-01-09 14:23:23 -0500

    improve(API): Pass gasPrice into relayerFeeDetails call
    We should pass the pre-computed gas price into this call so that we don't query the gas price twice. Additionally, we can use the scaled up gas price value to derive the gas fee totals, which currently are not using the scaled up values

--- api/_utils.ts
+++ api/_utils.ts
@@ -1948,6 +1948,7 @@ export function isContractCache(chainId: number, address: string) {
 
 export function getCachedFillGasUsage(
   deposit: Parameters<typeof buildDepositForSimulation>[0],
+  gasPrice: BigNumber,
   overrides?: Partial<{
     spokePoolAddress: string;
     relayerAddress: string;
@@ -1975,6 +1976,7 @@ export function getCachedFillGasUsage(
       buildDepositForSimulation(deposit),
       overrides?.relayerAddress,
       {
+        gasPrice,
         // Scale the op stack L1 gas cost component by the base fee multiplier.
         // Consider adding a new environment variable OP_STACK_L1_GAS_COST_MARKUP if we want finer-grained control.
         opStackL1GasCostMultiplier: getGasMarkup(deposit.destinationChainId)
