4a26327eb283d561b22011aff726b1367daf65b2
Evaldo Felipe <contato@evaldofelipe.com>
2025-01-09 13:15:29 -0300

    test
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- scripts/pre-build.ts
+++ scripts/pre-build.ts
@@ -105,6 +105,7 @@ const remoteConfigs = {
 
 (async () => {
   console.log("Running pre-build script to fetch remote config files...");
+  console.log(process.env.GIT_ENV_EXPORTED);
 
   try {
     for (const [configType, { fetchFn, localFilePath }] of Object.entries(
