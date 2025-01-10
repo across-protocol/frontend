194a0702b222333175d8e92fc85c083743d2a6c6
Gerhard Steenkamp <gerhard@umaproject.org>
2025-01-10 13:06:23 +0200

    add chain config

---
+++ scripts/chain-configs/lens-sepolia/index.ts
@@ -0,0 +1,23 @@
+import { CHAIN_IDs, PUBLIC_NETWORKS } from "@across-protocol/constants";
+import { utils as sdkUtils } from "@across-protocol/sdk";
+import { ChainConfig } from "../types";
+
+const { getDeployedAddress, getDeployedBlockNumber } = sdkUtils;
+
+const chainId = CHAIN_IDs.LENS_SEPOLIA;
+const chainInfoBase = PUBLIC_NETWORKS[chainId];
+
+export default {
+  ...chainInfoBase,
+  logoPath: "./assets/logo.svg",
+  grayscaleLogoPath: "./assets/grayscale-logo.svg",
+  spokePool: {
+    address: getDeployedAddress("SpokePool", chainId),
+    blockNumber: getDeployedBlockNumber("SpokePool", chainId),
+  },
+  chainId,
+  publicRpcUrl: "https://rpc.testnet.lens.dev",
+  blockTimeSeconds: 1,
+  tokens: [],
+  enableCCTP: false,
+} as ChainConfig;
