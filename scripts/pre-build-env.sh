74fa74d0784ae32bea2e66df5cf99fb50f14c3b4
Evaldo Felipe <contato@evaldofelipe.com>
2025-01-10 15:09:15 -0300

    test
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- scripts/pre-build-env.sh
+++ scripts/pre-build-env.sh
@@ -10,7 +10,9 @@
      echo "Getting env files from config repo..."
      curl -o ${FILE_PATH} "https://${GH_TOKEN}@${GIT_ENV_REPO}/${FILE_REMOTE}"
      echo "Updated data at ${FILE_PATH}"
+     echo "exporting.."
      source /tmp/output.env
+     echo $GIT_ENV_EXPORTED
  else
      echo "No env exported"
  fi
