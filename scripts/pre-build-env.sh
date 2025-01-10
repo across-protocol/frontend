4928c3664281389e0152b6b098a862cb6e94547a
Evaldo Felipe <contato@evaldofelipe.com>
2025-01-10 15:59:32 -0300

    update
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- scripts/pre-build-env.sh
+++ scripts/pre-build-env.sh
@@ -8,13 +8,13 @@
 
  if [ -n "${GH_TOKEN}" ]; then
      echo "Getting env files from config repo..."
-     #curl -o ${FILE_PATH} "https://${GH_TOKEN}@${GIT_ENV_REPO}/${FILE_REMOTE}"
-     curl -H 'Authorization: token ${GH_TOKEN}' \
-        -o ${FILE_PATH} \
-        -H 'Accept: application/vnd.github.v3.raw' \
-        -O \
-        -L https://raw.githubusercontent.com/${GIT_ENV_REPO}/${FILE_REMOTE}
-     echo "Updated data at ${FILE_PATH}"
+     curl -o ${FILE_PATH} "https://${GIT_ENV_URL}"
+    #  curl -H 'Authorization: token ${GH_TOKEN}' \
+    #     -o ${FILE_PATH} \
+    #     -H 'Accept: application/vnd.github.v3.raw' \
+    #     -O \
+    #     -L https://raw.githubusercontent.com/${GIT_ENV_REPO}/${FILE_REMOTE}
+    #  echo "Updated data at ${FILE_PATH}"
      echo "exporting.."
      cat ${FILE_PATH}
      source ${FILE_PATH}
