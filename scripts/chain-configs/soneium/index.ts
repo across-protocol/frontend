f63330ad897ba84b3f734df84a934e5da3ea9ba2
james-a-morris <jaamorris@cs.stonybrook.edu>
2025-01-14 18:23:50 -0500

    improve: add ETH routes
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- scripts/chain-configs/soneium/index.ts
+++ scripts/chain-configs/soneium/index.ts
@@ -18,6 +18,6 @@ export default {
   chainId,
   publicRpcUrl: "https://rpc.soneium.org",
   blockTimeSeconds: 2,
-  tokens: ["WETH", "USDC.e"],
+  tokens: ["WETH", "ETH", "USDC.e"],
   enableCCTP: false,
 } as ChainConfig;
