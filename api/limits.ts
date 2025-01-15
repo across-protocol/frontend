f0cd8d1051b8a930fcdb3b15f27c5587d540796f
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-01-15 13:54:18 +0100

    chore: improve error log  (#1374)
    * chore: improve error log
    
    * fixup
    
    * fix: pin block tag
    
    * fixup
    
    * fixup

--- api/limits.ts
+++ api/limits.ts
@@ -3,7 +3,7 @@ import { VercelResponse } from "@vercel/node";
 import { BigNumber, ethers } from "ethers";
 import { CHAIN_IDs, DEFAULT_SIMULATED_RECIPIENT_ADDRESS } from "./_constants";
 import { TokenInfo, TypedVercelRequest } from "./_types";
-import { object, assert, Infer, optional, string } from "superstruct";
+import { assert, Infer, optional, string, type } from "superstruct";
 
 import {
   ENABLED_ROUTES,
@@ -38,7 +38,7 @@ import {
 } from "./_utils";
 import { MissingParamError } from "./_errors";
 
-const LimitsQueryParamsSchema = object({
+const LimitsQueryParamsSchema = type({
   token: optional(validAddress()),
   inputToken: optional(validAddress()),
   outputToken: optional(validAddress()),
