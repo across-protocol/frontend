322ba3aedba83f36d015aeb5e41d7fb37e9830e4
Matt Rice <matthewcrice32@gmail.com>
2025-01-15 14:56:29 -0500

    test

--- api/available-routes.ts
+++ api/available-routes.ts
@@ -3,6 +3,8 @@ import { object, Infer, optional, string } from "superstruct";
 import dotenv from "dotenv";
 import { validAddress, positiveIntStr } from "./_utils";
 import { TypedVercelRequest } from "./_types";
+import fs from "fs";
+import path from "path";
 
 const AvailableRoutesQueryParamsSchema = object({
   originToken: optional(validAddress()),
@@ -17,11 +19,18 @@ dotenv.config({
   path: "./output_api.env",
 });
 
+
+let envPath = path.join(process.cwd(), 'output_api.env');
+let envFile = fs.readFileSync(envPath);
+
 type AvailableRoutesQueryParams = Infer<
   typeof AvailableRoutesQueryParamsSchema
 >;
 
 const handler = async (_: any, response: VercelResponse) => {
+  if (envFile === "") {
+    response.status(500).json({ error: "ERROR NO ENV FOUND" });
+  }
   response.status(200).json({
     test: process.env.GIT_ENV_EXPORTED,
   });
