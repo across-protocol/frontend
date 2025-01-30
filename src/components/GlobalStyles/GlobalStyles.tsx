87d6b99753c7db9bb756cb9bb3e37eda158f82b5
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-01-30 23:28:57 +0700

    fix: linter warnings

--- src/components/GlobalStyles/GlobalStyles.tsx
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -9,9 +9,9 @@ export const typography = css`
     font-weight: 400;
     font-display: fallback;
     src: url("/fonts/Barlow-Regular.woff2") format("woff2");
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
   }
   @font-face {
     font-family: "Barlow";
@@ -19,9 +19,9 @@ export const typography = css`
     font-weight: 500;
     font-display: fallback;
     src: url("/fonts/Barlow-Medium.woff2") format("woff2");
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
   }
   @font-face {
     font-family: "Barlow";
@@ -29,9 +29,9 @@ export const typography = css`
     font-weight: 700;
     font-display: fallback;
     src: url("/fonts/Barlow-Bold.woff2") format("woff2");
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
   }
 `;
 const variables = css`
