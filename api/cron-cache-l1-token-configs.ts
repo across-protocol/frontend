cd26d7c03362ddde4ab1d86b0cea30133fe9c2af
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-02-17 18:19:51 +0700

    feat: safe l1 token config parsing

---
+++ api/cron-cache-l1-token-configs.ts
@@ -0,0 +1,73 @@
+import { VercelResponse } from "@vercel/node";
+import { TypedVercelRequest } from "./_types";
+
+import {
+  HUB_POOL_CHAIN_ID,
+  getLogger,
+  handleErrorCondition,
+  getL1TokenConfigCache,
+} from "./_utils";
+import { UnauthorizedError } from "./_errors";
+
+import mainnetChains from "../src/data/chains_1.json";
+
+const handler = async (
+  request: TypedVercelRequest<Record<string, never>>,
+  response: VercelResponse
+) => {
+  const logger = getLogger();
+  logger.debug({
+    at: "CronCacheL1TokenConfigs",
+    message: "Starting cron job...",
+  });
+  try {
+    const authHeader = request.headers?.["authorization"];
+    if (
+      !process.env.CRON_SECRET ||
+      authHeader !== `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      throw new UnauthorizedError();
+    }
+
+    // Skip cron job on testnet
+    if (HUB_POOL_CHAIN_ID !== 1) {
+      return;
+    }
+
+    const mainnetChain = mainnetChains.find((chain) => chain.chainId === 1);
+
+    if (!mainnetChain) {
+      throw new Error("Mainnet chain not found");
+    }
+
+    const l1TokenAddresses = mainnetChain.inputTokens.map(
+      (token) => token.address
+    );
+
+    for (const l1TokenAddress of l1TokenAddresses) {
+      const l1TokenConfigCache = getL1TokenConfigCache(l1TokenAddress);
+
+      logger.info({
+        at: "CronCacheL1TokenConfigs",
+        message: `Caching L1 token config for ${l1TokenAddress}`,
+      });
+      await l1TokenConfigCache.set();
+    }
+
+    logger.debug({
+      at: "CronCacheL1TokenConfigs",
+      message: "Finished",
+    });
+    response.status(200);
+    response.send("OK");
+  } catch (error: unknown) {
+    return handleErrorCondition(
+      "cron-cache-l1-token-configs",
+      response,
+      logger,
+      error
+    );
+  }
+};
+
+export default handler;
