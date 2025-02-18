1aa70c8b448afb438613706117a9449ba8f9f92a
Evaldo Felipe <contato@evaldofelipe.com>
2025-02-18 11:03:48 -0300

    fix lint
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- scripts/pre-build-env.sh
+++ scripts/pre-build-env.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-   set -o errexit
+set -o errexit
 
-  BASE_URL="${GIT_ENV_REPO}/${GIT_ENV_PROJECT}/outputs/"
+BASE_URL="${GIT_ENV_REPO}/${GIT_ENV_PROJECT}/outputs/"
 
-  if [ -n "${GH_TOKEN}" ]; then
-
-     files=(output_vercel_build.env output_vercel_api.env)
-
-     for file in "${files[@]}"; do
-         curl -H "Authorization: token ${GH_TOKEN}" -L "${BASE_URL}${file}" -o "./${file}"
-     done
-     echo "All files downloaded."
-  else
-
-     echo "No env exported"
-  fi
+if [ -n "${GH_TOKEN}" ]; then
+  files=(output_vercel_build.env output_vercel_api.env)
+  
+  for file in "${files[@]}"; do
+    curl -H "Authorization: token ${GH_TOKEN}" -L "${BASE_URL}${file}" -o "./${file}"
+  done
+  echo "All files downloaded."
+else
+  echo "No env exported"
+fi
