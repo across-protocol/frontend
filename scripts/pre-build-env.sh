1a8fcdd9f1453566f012a711dab0059e4982d20b
Evaldo Felipe <contato@evaldofelipe.com>
2025-03-21 16:47:02 -0300

    fix: update env files (#1505)
    * Test ci
    
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>
    
    * test env
    
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>
    
    * update test var
    
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>
    
    * update ci files
    
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>
    
    * update command
    
    ---------
    
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- scripts/pre-build-env.sh
+++ scripts/pre-build-env.sh
@@ -5,7 +5,7 @@ set -o errexit
 BASE_URL="${GIT_ENV_REPO}/${GIT_ENV_PROJECT}/outputs/"
 
 if [ -n "${GH_TOKEN}" ]; then
-  files=(output_vercel_build.env output_vercel_api.env)
+  files=(output_vercel.env)
   
   for file in "${files[@]}"; do
     curl -H "Authorization: token ${GH_TOKEN}" -L "${BASE_URL}${file}" -o "./${file}"
