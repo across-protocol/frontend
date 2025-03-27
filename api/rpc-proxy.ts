74aeee3af65ef9ce4693d498e06682d35c047c45
Gerhard Steenkamp <gerhard@umaproject.org>
2025-03-27 16:54:59 +0200

    enforce auth token
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- api/rpc-proxy.ts
+++ api/rpc-proxy.ts
@@ -4,12 +4,7 @@ import {
   InvalidParamError,
   UnauthorizedError,
 } from "./_errors";
-import {
-  getLogger,
-  getProviderHeaders,
-  positiveIntStr,
-  resolveVercelEndpoint,
-} from "./_utils";
+import { getLogger, getProviderHeaders, positiveIntStr } from "./_utils";
 import { CHAIN_IDs } from "@across-protocol/constants";
 import { assert, Infer, optional, string, type } from "superstruct";
 import { TypedVercelRequest } from "./_types";
@@ -19,7 +14,6 @@ const PROTECTED_RPC_MAP: Record<number, string> = {
   [CHAIN_IDs.LENS]: "https://api.lens.matterhosted.dev",
 };
 
-const ALLOWED_ORIGINS = [resolveVercelEndpoint()]; // add more here
 const { RPC_PROXY_AUTH_TOKEN } = getEnvs();
 
 const RpcProxyQueryParamsSchema = type({
@@ -81,24 +75,10 @@ const handler = async (
 
 export default handler;
 
-function authenticate({
-  query,
-  headers,
-}: TypedVercelRequest<RpcProxyQueryParams>) {
-  const origin = headers?.origin;
-
-  // First check if auth token is provided
-  if (query.authToken) {
-    const authenticated = query.authToken === RPC_PROXY_AUTH_TOKEN;
-    if (!authenticated) {
-      throw new UnauthorizedError({
-        message: `Not Allowed: Invalid auth token ${query.authToken}`,
-      });
-    }
-    // if not then we may allow access for same origin requests (from our frontend)
-  } else if (!origin || !ALLOWED_ORIGINS.includes(origin)) {
+function authenticate({ query }: TypedVercelRequest<RpcProxyQueryParams>) {
+  if (!query.authToken || query.authToken !== RPC_PROXY_AUTH_TOKEN) {
     throw new UnauthorizedError({
-      message: "Origin not allowed",
+      message: `Not Allowed: Invalid auth token ${query.authToken}`,
     });
   }
 }
