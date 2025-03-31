ef20a5a4dda91026b8f56ba149a988fc82f699c5
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-03-31 21:48:07 +0700

    fixup

--- scripts/generate-swap-routes.ts
+++ scripts/generate-swap-routes.ts
@@ -8,42 +8,48 @@ import {
   enabledSepoliaChainConfigs,
 } from "./generate-routes";
 
-const enabledSwapRoutes = {
+const enabledSwapRoutes: {
+  [hubPoolChainId: number]: {
+    [tokenInSymbol: string]: {
+      all?: {
+        enabledDestinationChains: number[];
+        enabledOutputTokens: string[];
+      };
+      [originChainId: number]: {
+        enabledDestinationChains: number[];
+        enabledOutputTokens: string[];
+      };
+    };
+  };
+} = {
   [CHAIN_IDs.MAINNET]: {
     [TOKEN_SYMBOLS_MAP.GHO.symbol]: {
-      enabledOriginChains: [CHAIN_IDs.MAINNET, CHAIN_IDs.LENS],
-      enabledDestinationChains: [CHAIN_IDs.MAINNET, CHAIN_IDs.LENS],
-      enabledOutputTokens: ["WGHO", "USDC", "USDT", "DAI"],
+      [CHAIN_IDs.MAINNET]: {
+        enabledDestinationChains: [CHAIN_IDs.LENS],
+        enabledOutputTokens: ["WGHO", "GHO"],
+      },
+      [CHAIN_IDs.LENS]: {
+        enabledDestinationChains: [CHAIN_IDs.MAINNET],
+        enabledOutputTokens: ["WGHO", "GHO"],
+      },
     },
     [TOKEN_SYMBOLS_MAP.USDC.symbol]: {
-      enabledOriginChains: "all",
-      enabledDestinationChains: [CHAIN_IDs.MAINNET],
-      enabledOutputTokens: ["GHO"],
+      all: {
+        enabledDestinationChains: [CHAIN_IDs.LENS],
+        enabledOutputTokens: ["GHO"],
+      },
     },
     [TOKEN_SYMBOLS_MAP.USDT.symbol]: {
-      enabledOriginChains: "all",
-      enabledDestinationChains: [CHAIN_IDs.MAINNET],
-      enabledOutputTokens: ["GHO"],
+      all: {
+        enabledDestinationChains: [CHAIN_IDs.LENS],
+        enabledOutputTokens: ["GHO"],
+      },
     },
     [TOKEN_SYMBOLS_MAP.DAI.symbol]: {
-      enabledOriginChains: "all",
-      enabledDestinationChains: [CHAIN_IDs.MAINNET],
-      enabledOutputTokens: ["GHO"],
-    },
-    [TOKEN_SYMBOLS_MAP["USDC.e"].symbol]: {
-      enabledOriginChains: [CHAIN_IDs.ARBITRUM],
-      enabledDestinationChains: [CHAIN_IDs.OPTIMISM, CHAIN_IDs.MAINNET],
-      enabledOutputTokens: ["USDC", "ETH", "WETH", "GHO"],
-    },
-    [TOKEN_SYMBOLS_MAP.ETH.symbol]: {
-      enabledOriginChains: [CHAIN_IDs.ARBITRUM],
-      enabledDestinationChains: [CHAIN_IDs.OPTIMISM],
-      enabledOutputTokens: ["USDC"],
-    },
-    [TOKEN_SYMBOLS_MAP.WETH.symbol]: {
-      enabledOriginChains: [CHAIN_IDs.ARBITRUM],
-      enabledDestinationChains: [CHAIN_IDs.OPTIMISM],
-      enabledOutputTokens: ["USDC"],
+      all: {
+        enabledDestinationChains: [CHAIN_IDs.LENS],
+        enabledOutputTokens: ["GHO"],
+      },
     },
   },
   [CHAIN_IDs.SEPOLIA]: {},
@@ -57,102 +63,104 @@ async function generateSwapRoutes(hubPoolChainId = 1) {
 
   const swapRoutes: any[] = [];
 
-  for (const [inputTokenSymbol, config] of Object.entries(
+  for (const [inputTokenSymbol, configPerOriginChain] of Object.entries(
     enabledSwapRoutes[hubPoolChainId]
   )) {
-    const {
-      enabledOriginChains,
-      enabledDestinationChains,
-      enabledOutputTokens,
-    } = config;
+    for (const [originChainId, config] of Object.entries(
+      configPerOriginChain
+    )) {
+      const { enabledDestinationChains, enabledOutputTokens } = config;
 
-    const originChains =
-      enabledOriginChains === "all"
-        ? enabledChains
-        : enabledChains.filter((chain) =>
-            enabledOriginChains.includes(chain.chainId)
-          );
-    const destinationChains = enabledChains.filter((chain) =>
-      enabledDestinationChains.includes(chain.chainId)
-    );
-
-    for (const originChain of originChains) {
-      const originChainHasInputToken =
-        TOKEN_SYMBOLS_MAP[inputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP]
-          .addresses[originChain.chainId] !== undefined;
-
-      if (!originChainHasInputToken) {
-        console.warn(
-          `Origin chain ${originChain.chainId} does not have input token ${inputTokenSymbol}. Skipping...`
-        );
-        continue;
-      }
-
-      const relevantDestinationChains = destinationChains.filter(
-        (chain) => chain.chainId !== originChain.chainId
+      const originChains =
+        originChainId === "all"
+          ? enabledChains
+          : enabledChains.filter(
+              (chain) => Number(originChainId) === chain.chainId
+            );
+      const destinationChains = enabledChains.filter((chain) =>
+        enabledDestinationChains.includes(chain.chainId)
       );
 
-      // Check for each enabled destination chain if enabled output token exists
-      for (const destinationChain of relevantDestinationChains) {
-        for (const outputTokenSymbol of enabledOutputTokens) {
-          const destinationChainHasOutputToken =
-            TOKEN_SYMBOLS_MAP[
-              outputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP
-            ].addresses[destinationChain.chainId] !== undefined;
+      for (const originChain of originChains) {
+        const originChainHasInputToken =
+          TOKEN_SYMBOLS_MAP[inputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP]
+            .addresses[originChain.chainId] !== undefined;
 
-          if (!destinationChainHasOutputToken) {
-            console.warn(
-              `Destination chain ${destinationChain.chainId} does not have output token ${outputTokenSymbol}. Skipping...`
-            );
-            continue;
-          }
-
-          const inputToken =
-            TOKEN_SYMBOLS_MAP[
-              inputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP
-            ];
-          const outputToken =
-            TOKEN_SYMBOLS_MAP[
-              outputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP
-            ];
-          if (!inputToken || !outputToken) {
-            throw new Error(`No token info for 'inputToken' or 'outputToken'`);
-          }
-
-          const swapRoute = {
-            fromChain: originChain.chainId,
-            toChain: destinationChain.chainId,
-            fromTokenAddress:
-              originChain.nativeToken === inputToken.symbol
-                ? ethers.constants.AddressZero
-                : inputToken.addresses[originChain.chainId],
-            toTokenAddress:
-              destinationChain.nativeToken === outputToken.symbol
-                ? ethers.constants.AddressZero
-                : outputToken.addresses[destinationChain.chainId],
-            fromTokenSymbol: inputToken.symbol,
-            toTokenSymbol: outputToken.symbol,
-            fromSpokeAddress: originChain.spokePool.address,
-            l1TokenAddress: inputToken.addresses[hubPoolChainId],
-            type: "universal-swap",
-            isNative:
-              originChain.nativeToken === inputToken.symbol &&
-              destinationChain.nativeToken === outputToken.symbol,
-          };
-
-          const doesExist = swapRoutes.some(
-            (route) =>
-              route.fromChain === swapRoute.fromChain &&
-              route.toChain === swapRoute.toChain &&
-              route.fromTokenSymbol === swapRoute.fromTokenSymbol &&
-              route.toTokenSymbol === swapRoute.toTokenSymbol
+        if (!originChainHasInputToken) {
+          console.warn(
+            `Origin chain ${originChain.chainId} does not have input token ${inputTokenSymbol}. Skipping...`
           );
+          continue;
+        }
 
-          if (doesExist) {
-            continue;
+        const relevantDestinationChains = destinationChains.filter(
+          (chain) => chain.chainId !== originChain.chainId
+        );
+
+        // Check for each enabled destination chain if enabled output token exists
+        for (const destinationChain of relevantDestinationChains) {
+          for (const outputTokenSymbol of enabledOutputTokens) {
+            const destinationChainHasOutputToken =
+              TOKEN_SYMBOLS_MAP[
+                outputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP
+              ].addresses[destinationChain.chainId] !== undefined;
+
+            if (!destinationChainHasOutputToken) {
+              console.warn(
+                `Destination chain ${destinationChain.chainId} does not have output token ${outputTokenSymbol}. Skipping...`
+              );
+              continue;
+            }
+
+            const inputToken =
+              TOKEN_SYMBOLS_MAP[
+                inputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP
+              ];
+            const outputToken =
+              TOKEN_SYMBOLS_MAP[
+                outputTokenSymbol as keyof typeof TOKEN_SYMBOLS_MAP
+              ];
+            if (!inputToken || !outputToken) {
+              throw new Error(
+                `No token info for 'inputToken' or 'outputToken'`
+              );
+            }
+
+            const swapRoute = {
+              fromChain: originChain.chainId,
+              toChain: destinationChain.chainId,
+              fromTokenAddress:
+                originChain.nativeToken === inputToken.symbol
+                  ? ethers.constants.AddressZero
+                  : inputToken.addresses[originChain.chainId],
+              toTokenAddress:
+                destinationChain.nativeToken === outputToken.symbol
+                  ? ethers.constants.AddressZero
+                  : outputToken.addresses[destinationChain.chainId],
+              fromTokenSymbol: inputToken.symbol,
+              toTokenSymbol: outputToken.symbol,
+              fromSpokeAddress: originChain.spokePool.address,
+              l1TokenAddress: inputToken.addresses[hubPoolChainId],
+              type: "universal-swap",
+              isNative:
+                originChain.nativeToken === inputToken.symbol &&
+                destinationChain.nativeToken === outputToken.symbol,
+            };
+
+            const doesExist = swapRoutes.some(
+              (route) =>
+                route.fromChain === swapRoute.fromChain &&
+                route.toChain === swapRoute.toChain &&
+                route.fromTokenSymbol === swapRoute.fromTokenSymbol &&
+                route.toTokenSymbol === swapRoute.toTokenSymbol
+            );
+
+            if (doesExist) {
+              continue;
+            }
+
+            swapRoutes.push(swapRoute);
           }
-
-          swapRoutes.push(swapRoute);
         }
       }
     }
