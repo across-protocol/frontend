67d478a9ba91e35d0cbb492b6a1c72284ad0747c
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-04-12 13:48:35 +0700

    chore: temp mock limits and fees for solana

--- src/utils/serverless-api/prod/retrieveLimits.ts
+++ src/utils/serverless-api/prod/retrieveLimits.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ChainId, vercelApiBaseUrl } from "utils";
 import { BridgeLimitInterface } from "../types";
+import { BigNumber, utils } from "ethers";
 
 export async function retrieveLimits(
   inputToken: string,
@@ -8,6 +9,15 @@ export async function retrieveLimits(
   fromChainId: string | ChainId,
   toChainId: string | ChainId
 ): Promise<BridgeLimitInterface> {
+  if ([ChainId.SOLANA, ChainId.SOLANA_DEVNET].includes(Number(fromChainId))) {
+    return retrieveLimitsForSVM(
+      inputToken,
+      outputToken,
+      fromChainId,
+      toChainId
+    );
+  }
+
   const { data } = await axios.get<BridgeLimitInterface>(
     `${vercelApiBaseUrl}/api/limits`,
     {
@@ -20,4 +30,20 @@ export async function retrieveLimits(
     }
   );
   return data;
+}
+
+export async function retrieveLimitsForSVM(
+  inputToken: string,
+  outputToken: string,
+  fromChainId: string | ChainId,
+  toChainId: string | ChainId
+): Promise<BridgeLimitInterface> {
+  const maxDeposit = utils.parseUnits("100", 6);
+
+  return {
+    maxDeposit,
+    maxDepositInstant: maxDeposit,
+    maxDepositShortDelay: maxDeposit,
+    minDeposit: BigNumber.from(0),
+  };
 }
