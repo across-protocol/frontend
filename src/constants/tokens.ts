5e74a9497db62e3f08388e4e4cfbc6f41a2ec837
Paul <108695806+pxrl@users.noreply.github.com>
2025-05-08 16:05:59 +0000

    Reorder

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -74,6 +74,7 @@ export const similarTokensMap: Record<string, string[]> = similarTokens.reduce(
 
 // Order of this map determines the order of the tokens in the token selector
 export const orderedTokenLogos = {
+  VLR: unknownLogo, // xxx relocate before merging to prod
   ETH: ethLogo,
   WETH: wethLogo,
   MATIC: maticLogo,
@@ -107,5 +108,4 @@ export const orderedTokenLogos = {
   "TATARA-USDS": unknownLogo,
   "TATARA-USDT": usdtLogo,
   "TATARA-WBTC": wbtcLogo,
-  VLR: unknownLogo,
 } as const satisfies Partial<Record<keyof typeof TOKEN_SYMBOLS_MAP, string>>;
