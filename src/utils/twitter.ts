dad022de03a3ff3be1f39078ccb4fa50b809e710
Gerhard Steenkamp <gerhard@umaproject.org>
2025-06-26 16:03:28 +0200

    fixup
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- src/utils/twitter.ts
+++ src/utils/twitter.ts
@@ -1,4 +1,4 @@
-import { buildSearchParams, getChainInfo, vercelApiBaseUrl } from "utils";
+import { buildSearchParams, getChainInfo, resolveWebsiteUrl } from "utils";
 
 // https://developer.x.com/en/docs/x-for-websites/tweet-button/guides/web-intent
 export function buildTwitterShareUrl(params: {
@@ -11,11 +11,13 @@ export function buildTwitterShareUrl(params: {
   const hashTags = ["PoweredByIntents"]; // tags without "#"
   const relatedAccounts = ["@AcrossProtocol"];
   const tweetText = `I just used @AcrossProtocol to bridge from ${originChainName} to ${destinationChainName} in ${params.time} seconds!\n\nTry it yourself app.across.to`; // TODO: get copy
-  const imageUrl = `${vercelApiBaseUrl}/api/twitter-share?${buildSearchParams({
-    seconds: params.time,
-    originChainId: params.originChainId,
-    destinationChainId: params.destinationChainId,
-  })}`;
+  const imageUrl = `${resolveWebsiteUrl()}/api/twitter-share?${buildSearchParams(
+    {
+      seconds: params.time,
+      originChainId: params.originChainId,
+      destinationChainId: params.destinationChainId,
+    }
+  )}`;
   const twitterUrl = `https://twitter.com/intent/tweet?${buildSearchParams({
     text: tweetText,
     hashtags: hashTags.join(","),
