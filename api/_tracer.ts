3e7a268a6e6805fbe78dfab19692422ca9409f9e
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-06-30 16:05:09 +0200

    fixup

--- api/_tracer.ts
+++ api/_tracer.ts
@@ -1,6 +1,6 @@
 import { trace } from "@opentelemetry/api";
 
-import { serviceName } from "../instrumentation";
+import { serviceName } from "../instrumentation.cjs";
 
 export function getTracer() {
   return trace.getTracer(serviceName);
