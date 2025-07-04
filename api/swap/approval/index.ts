211a64f30594629e79c8cec7d9b635d4658b5eb1
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-07-04 10:53:35 +0200

    feat: add tracing to /swap/approval

--- api/swap/approval/index.ts
+++ api/swap/approval/index.ts
@@ -1,9 +1,11 @@
 import { VercelResponse } from "@vercel/node";
+import { SpanStatusCode } from "@opentelemetry/api";
 
 import { TypedVercelRequest } from "../../_types";
-import { getLogger, handleErrorCondition, Profiler } from "../../_utils";
+import { getLogger, handleErrorCondition } from "../../_utils";
 import { BaseSwapQueryParams } from "../_utils";
 import { handleApprovalSwap } from "./_service";
+import { tracer } from "../../../instrumentation";
 
 const handler = async (
   request: TypedVercelRequest<BaseSwapQueryParams>,
@@ -15,25 +17,27 @@ const handler = async (
     message: "Query data",
     query: request.query,
   });
-  try {
-    const profiler = new Profiler({
-      at: "swap/approval",
-      logger: console,
-    });
-    const mark = profiler.start("e2e endpoint runtime");
+  return tracer.startActiveSpan("swap/approval", async (span) => {
+    try {
+      const responseJson = await handleApprovalSwap(request);
 
-    const responseJson = await handleApprovalSwap(request);
-
-    mark.stop();
-    logger.debug({
-      at: "Swap/approval",
-      message: "Response data",
-      responseJson,
-    });
-    response.status(200).json(responseJson);
-  } catch (error: unknown) {
-    return handleErrorCondition("swap/approval", response, logger, error);
-  }
+      logger.debug({
+        at: "Swap/approval",
+        message: "Response data",
+        responseJson,
+      });
+      response.status(200).json(responseJson);
+    } catch (error: unknown) {
+      span.recordException(error as Error);
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: (error as Error).message,
+      });
+      return handleErrorCondition("swap/approval", response, logger, error);
+    } finally {
+      span.end();
+    }
+  });
 };
 
 export default handler;
