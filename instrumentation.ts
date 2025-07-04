71a01eea793bbda3435136d6915606076985cea6
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-07-04 11:37:15 +0200

    fixup

--- instrumentation.ts
+++ instrumentation.ts
@@ -27,7 +27,11 @@ const processor = new BatchSpanProcessor(new OTLPTraceExporter());
 const sdk = new NodeSDK({
   resource,
   spanProcessors: [processor],
-  instrumentations: [new HttpInstrumentation()],
+  instrumentations: [
+    new HttpInstrumentation({
+      disableIncomingRequestInstrumentation: true,
+    }),
+  ],
 });
 
 sdk.start();
