bb32fb5a29471bcd8f3bcefd89e04fe6aa184082
ashwinrava <213675439+ashwinrava@users.noreply.github.com>
2025-07-10 10:14:34 -0700

    Add relayer config and orderbook types

---
+++ api/relayer/_types.ts
@@ -0,0 +1,60 @@
+import {
+  Infer,
+  number,
+  object,
+  type,
+  record,
+  string,
+  boolean,
+  optional,
+  unknown,
+} from "superstruct";
+import { validAddress } from "../_utils";
+
+export const OrderbookQueryParamsSchema = type({
+  originChainId: number(),
+  destinationChainId: number(),
+  originToken: validAddress(),
+  destinationToken: validAddress(),
+});
+
+export const OrderbookResponseSchema = type({
+  relayerAddresses: record(validAddress(), record(string(), number())),
+});
+
+export const RelayerConfigSchema = type({
+  prices: record(
+    string(),
+    object({
+      origin: record(
+        string(),
+        record(string(), record(string(), record(string(), number())))
+      ),
+      destination: record(
+        string(),
+        record(string(), record(string(), record(string(), number())))
+      ),
+      messageExecution: boolean(),
+    })
+  ),
+  minExclusivityPeriods: object({
+    default: record(string(), record(string(), number())),
+    routes: optional(record(string(), record(string(), number()))),
+    origin: optional(record(string(), record(string(), number()))),
+    destination: optional(record(string(), record(string(), number()))),
+    sizes: optional(record(string(), number())),
+  }),
+  authentication: optional(
+    object({
+      address: validAddress(),
+      method: string(),
+      payload: record(string(), unknown()),
+    })
+  ),
+});
+
+export type OrderbookQueryParams = Infer<typeof OrderbookQueryParamsSchema>;
+
+export type OrderbookResponse = Infer<typeof OrderbookResponseSchema>;
+
+export type RelayerConfig = Infer<typeof RelayerConfigSchema>;
