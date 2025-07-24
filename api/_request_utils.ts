ae7218b80389819cab37509f90d6ae87108410e8
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-07-24 14:33:41 +0200

    fixup

--- api/_request_utils.ts
+++ api/_request_utils.ts
@@ -1,7 +1,7 @@
 import { TypedVercelRequest } from "./_types";
 
 export function getRequestId(req: TypedVercelRequest<unknown, unknown>) {
-  const requestIdFromHeader = req.headers["x-request-id"];
+  const requestIdFromHeader = req.headers["x-vercel-id"];
   const fallbackRequestId = crypto.randomUUID();
 
   if (!requestIdFromHeader || typeof requestIdFromHeader !== "string") {
