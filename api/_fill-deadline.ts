3c567fc8d21ddac5e6382bcc58af98fc7b8eae5b
Matt Rice <matthewcrice32@gmail.com>
2025-07-30 22:32:51 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- api/_fill-deadline.ts
+++ api/_fill-deadline.ts
@@ -17,7 +17,7 @@ export function getFillDeadline(
 ): number {
   const fillDeadlineBuffer = getFillDeadlineBuffer(chainId);
   // Quote timestamp cannot be in the future or more than an hour old, so this is safe (i.e. cannot
-  // cause the contract to revert) as long as the fill deadline buffer is at least 1 hour smaller
-  // than the maximum buffer length allowed by the contract.
+  // cause the contract to revert or an unfillable deposit) as long as the fill deadline buffer is
+  // greater than 1 hour (+ some cushion for fill time).
   return Number(quoteTimestamp) + fillDeadlineBuffer;
 }
