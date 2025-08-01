5ae1b9700ed934f71ef4a4e8668e0d3f12d00bf9
Melisa Guevara <melisaguevarav@gmail.com>
2025-08-01 10:16:45 -0300

    fix: body parsing for swap requests (#1699)

--- api/swap/approval/_service.ts
+++ api/swap/approval/_service.ts
@@ -57,10 +57,9 @@ export async function handleApprovalSwap(
     includeSources,
   } = await handleBaseSwapQueryParams(request.query);
 
-  const { actions } =
-    Object.keys(request.body).length > 0
-      ? handleSwapBody(request.body, Number(request.query.destinationChainId))
-      : { actions: [] };
+  const { actions } = request.body
+    ? handleSwapBody(request.body, Number(request.query.destinationChainId))
+    : { actions: [] };
 
   const crossSwapQuotes = await getCrossSwapQuotes(
     {
