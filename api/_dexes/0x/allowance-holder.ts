fd7187f1a9353890ab7b8bc732535cfc7f286900
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-08-04 17:03:51 +0200

    feat: improve upstream swap provider errors

--- api/_dexes/0x/allowance-holder.ts
+++ api/_dexes/0x/allowance-holder.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from "ethers";
 import { TradeType } from "@uniswap/sdk-core";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 import { getLogger } from "../../_utils";
 import {
@@ -18,7 +18,11 @@ import {
   makeGetSources,
 } from "../utils";
 import { SOURCES } from "./utils/sources";
-import { compactAxiosError, SwapQuoteUnavailableError } from "../../_errors";
+import {
+  compactAxiosError,
+  UpstreamSwapProviderError,
+  UPSTREAM_SWAP_PROVIDER_ERRORS,
+} from "../../_errors";
 
 const { API_KEY_0X } = getEnvs();
 
@@ -29,6 +33,8 @@ const API_HEADERS = {
   "0x-api-key": `${API_KEY_0X}`,
   "0x-version": "v2",
 };
+
+const SWAP_PROVIDER_NAME = "0x";
 
 export function get0xStrategy(
   originSwapEntryPointContractName: OriginEntryPointContractName
@@ -105,10 +111,12 @@ export function get0xStrategy(
       const quote = response.data;
 
       if (!quote.liquidityAvailable) {
-        throw new SwapQuoteUnavailableError({
+        throw new UpstreamSwapProviderError({
           message: `0x: No liquidity available for ${
             swap.tokenIn.symbol
           } -> ${swap.tokenOut.symbol} on chain ${swap.chainId}`,
+          code: UPSTREAM_SWAP_PROVIDER_ERRORS.INSUFFICIENT_LIQUIDITY,
+          swapProvider: "0x",
         });
       }
 
@@ -144,7 +152,7 @@ export function get0xStrategy(
         slippageTolerance: swap.slippageTolerance,
         swapTxns: [swapTx],
         swapProvider: {
-          name: "0x",
+          name: SWAP_PROVIDER_NAME,
           sources: usedSources,
         },
       };
@@ -170,15 +178,79 @@ export function get0xStrategy(
         message: "Error fetching 0x quote",
         error: compactAxiosError(error as Error),
       });
-      throw error;
+      throw parse0xError(error);
     }
   };
 
   return {
-    strategyName: "0x",
+    strategyName: SWAP_PROVIDER_NAME,
     getRouter,
     getOriginEntryPoints,
     fetchFn,
     getSources,
   };
+}
+
+// https://0x.org/docs/introduction/api-issues#swap-api
+export function parse0xError(error: unknown) {
+  if (error instanceof UpstreamSwapProviderError) {
+    return error;
+  }
+
+  if (error instanceof AxiosError) {
+    const compactedError = compactAxiosError(error);
+
+    if (!error.response?.data) {
+      return new UpstreamSwapProviderError(
+        {
+          message: "Unknown error",
+          code: UPSTREAM_SWAP_PROVIDER_ERRORS.UNKNOWN_ERROR,
+          swapProvider: SWAP_PROVIDER_NAME,
+        },
+        { cause: compactedError }
+      );
+    }
+
+    const { data, status } = error.response;
+
+    if (
+      [
+        "INPUT_INVALID",
+        "SWAP_VALIDATION_FAILED",
+        "TOKEN_NOT_SUPPORTED",
+        "TAKER_NOT_AUTHORIZED_FOR_TRADE",
+        "BUY_TOKEN_NOT_AUTHORIZED_FOR_TRADE",
+        "SELL_TOKEN_NOT_AUTHORIZED_FOR_TRADE",
+      ].includes(data.name)
+    ) {
+      return new UpstreamSwapProviderError(
+        {
+          message: data.message,
+          code: UPSTREAM_SWAP_PROVIDER_ERRORS.NO_POSSIBLE_ROUTE,
+          swapProvider: SWAP_PROVIDER_NAME,
+        },
+        { cause: compactedError }
+      );
+    }
+
+    if (status >= 500) {
+      return new UpstreamSwapProviderError(
+        {
+          message: "Service unavailable",
+          code: UPSTREAM_SWAP_PROVIDER_ERRORS.SERVICE_UNAVAILABLE,
+          swapProvider: SWAP_PROVIDER_NAME,
+        },
+        { cause: compactedError }
+      );
+    }
+
+    return new UpstreamSwapProviderError(
+      {
+        message: "Unknown error",
+        code: UPSTREAM_SWAP_PROVIDER_ERRORS.UNKNOWN_ERROR,
+        swapProvider: SWAP_PROVIDER_NAME,
+      },
+      { cause: compactedError }
+    );
+  }
 }
