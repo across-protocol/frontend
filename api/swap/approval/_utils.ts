fe516565cd10bd2e584776f785a2df16c5944ad2
amateima <amatei@umaproject.org>
2025-08-06 15:57:09 +0300

    feat: add swap api calldata marker

--- api/swap/approval/_utils.ts
+++ api/swap/approval/_utils.ts
@@ -2,7 +2,7 @@ import { PopulatedTransaction } from "ethers";
 import * as sdk from "@across-protocol/sdk";
 
 import { CrossSwapQuotes } from "../../_dexes/types";
-import { tagIntegratorId } from "../../_integrator-id";
+import { tagIntegratorId, tagSwapApiMarker } from "../../_integrator-id";
 import { getSpokePool } from "../../_utils";
 import {
   getSpokePoolPeriphery,
@@ -233,11 +233,15 @@ export async function buildCrossSwapTxForAllowanceHolder(
       );
     }
   }
+  const txDataWithIntegratorId = integratorId
+    ? tagIntegratorId(integratorId, tx.data!)
+    : tx.data!;
+  const txDataWithSwapApiMarker = tagSwapApiMarker(txDataWithIntegratorId);
 
   return {
     from: crossSwapQuotes.crossSwap.depositor,
     to: toAddress,
-    data: integratorId ? tagIntegratorId(integratorId, tx.data!) : tx.data!,
+    data: txDataWithSwapApiMarker,
     value: tx.value,
   };
 }
