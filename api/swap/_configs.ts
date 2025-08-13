8cf9ecc2f538d6c09998dc17baf1cbdc4b82b080
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-08-13 14:15:34 +0200

    feat: support uniswap `UniversalRouter02` + trading API tweaks

--- api/swap/_configs.ts
+++ api/swap/_configs.ts
@@ -1,6 +1,7 @@
 import { CHAIN_IDs } from "../_constants";
 import { getWrappedGhoStrategy } from "../_dexes/gho/wrapped-gho";
 import { getWghoMulticallStrategy } from "../_dexes/gho/multicall";
+import { getUniversalRouter02Strategy } from "../_dexes/uniswap/universal-router-02";
 import { getSwapRouter02Strategy } from "../_dexes/uniswap/swap-router-02";
 import { get0xStrategy } from "../_dexes/0x/allowance-holder";
 import { getLifiStrategy } from "../_dexes/lifi/lifi-router";
@@ -15,7 +16,7 @@ export const quoteFetchStrategies: QuoteFetchStrategies = {
   default: [
     get0xStrategy("SpokePoolPeriphery"),
     getLifiStrategy("SpokePoolPeriphery"),
-    getSwapRouter02Strategy("SpokePoolPeriphery", "trading-api"),
+    getUniversalRouter02Strategy(),
   ],
   chains: {
     [CHAIN_IDs.LENS]: [
