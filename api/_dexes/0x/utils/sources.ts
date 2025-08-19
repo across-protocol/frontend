347ad69c519d1f24f361b7d2e6b549228c3738f0
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-08-19 12:23:22 +0200

    fix: make cache getter robust to upstream errors
    If the upstream cache provider has an outage or similar, we should still be able to retrieve a non-cached value.

--- api/_dexes/0x/utils/sources.ts
+++ api/_dexes/0x/utils/sources.ts
@@ -1,5 +1,5 @@
 // Auto-generated file. Do not edit manually.
-// Generated on 2025-08-14T14:52:02.130Z
+// Generated on 2025-08-19T10:23:42.508Z
 // This file contains available liquidity sources for 0x DEX integration
 
 export const SOURCES = {
