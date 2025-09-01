226193a87432a95207b6d71b90ef772b13d04efa
Gerhard Steenkamp <gerhard@umaproject.org>
2025-09-01 10:04:36 +0200

    temporarily comment out lens test case
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- test/api/_dexes/utils.test.ts
+++ test/api/_dexes/utils.test.ts
@@ -3,31 +3,33 @@ import { CHAIN_IDs, TOKEN_SYMBOLS_MAP } from "../../../api/_constants";
 
 describe("_dexes/utils", () => {
   describe("#getCrossSwapType()", () => {
-    test("L1 stable -> Lens GHO - should return any-to-bridgeable", () => {
-      const params = {
-        inputToken: TOKEN_SYMBOLS_MAP.DAI.addresses[CHAIN_IDs.MAINNET],
-        originChainId: CHAIN_IDs.MAINNET,
-        outputToken: TOKEN_SYMBOLS_MAP.WGHO.addresses[CHAIN_IDs.LENS],
-        destinationChainId: CHAIN_IDs.LENS,
-        isOutputNative: true,
-        isInputNative: false,
-      };
-      const crossSwapTypes = getCrossSwapTypes(params);
-      expect(crossSwapTypes).toEqual([CROSS_SWAP_TYPE.ANY_TO_BRIDGEABLE]);
-    });
+    // TODO: Temporarily disabled - Lens has been temporarily disabled
+    // test("L1 stable -> Lens GHO - should return any-to-bridgeable", () => {
+    //   const params = {
+    //     inputToken: TOKEN_SYMBOLS_MAP.DAI.addresses[CHAIN_IDs.MAINNET],
+    //     originChainId: CHAIN_IDs.MAINNET,
+    //     outputToken: TOKEN_SYMBOLS_MAP.WGHO.addresses[CHAIN_IDs.LENS],
+    //     destinationChainId: CHAIN_IDs.LENS,
+    //     isOutputNative: true,
+    //     isInputNative: false,
+    //   };
+    //   const crossSwapTypes = getCrossSwapTypes(params);
+    //   expect(crossSwapTypes).toEqual([CROSS_SWAP_TYPE.ANY_TO_BRIDGEABLE]);
+    // });
 
-    test("Lens GHO -> L1 GHO - should return bridgeable-to-any", () => {
-      const params = {
-        inputToken: TOKEN_SYMBOLS_MAP.WGHO.addresses[CHAIN_IDs.LENS],
-        originChainId: CHAIN_IDs.LENS,
-        outputToken: TOKEN_SYMBOLS_MAP.GHO.addresses[CHAIN_IDs.MAINNET],
-        destinationChainId: CHAIN_IDs.MAINNET,
-        isOutputNative: false,
-        isInputNative: true,
-      };
-      const crossSwapTypes = getCrossSwapTypes(params);
-      expect(crossSwapTypes).toEqual([CROSS_SWAP_TYPE.BRIDGEABLE_TO_ANY]);
-    });
+    // TODO: Temporarily disabled - Lens has been temporarily disabled
+    // test("Lens GHO -> L1 GHO - should return bridgeable-to-any", () => {
+    //   const params = {
+    //     inputToken: TOKEN_SYMBOLS_MAP.WGHO.addresses[CHAIN_IDs.LENS],
+    //     originChainId: CHAIN_IDs.LENS,
+    //     outputToken: TOKEN_SYMBOLS_MAP.GHO.addresses[CHAIN_IDs.MAINNET],
+    //     destinationChainId: CHAIN_IDs.MAINNET,
+    //     isOutputNative: false,
+    //     isInputNative: true,
+    //   };
+    //   const crossSwapTypes = getCrossSwapTypes(params);
+    //   expect(crossSwapTypes).toEqual([CROSS_SWAP_TYPE.BRIDGEABLE_TO_ANY]);
+    // });
 
     test("Optimism USDC -> Arbitrum USDC - should return bridgeable-to-bridgeable", () => {
       const params = {
