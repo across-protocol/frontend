38e7f2290be5dd5150af0e49c4f5bba21c621368
Melisa Guevara <melisaguevarav@gmail.com>
2025-09-02 13:18:57 -0300

    Revert "chore: temporary disable Lens" (#1791)
    * Revert "chore: temporary disable Lens (#1790)"
    
    This reverts commit 7879eae323117f0fb571f684b6448136a31f2010.
    
    * chore: Bump SDK
    
    * fix: use previous contracts version for SwapAndBridge definitions
    
    * regen routes
    
    * Bump SDK
    
    * Yarn lock SDK bump
    
    ---------
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    Co-authored-by: Aleksa Colovic <aleksa.colovic98@gmail.com>

--- src/utils/typechain.ts
+++ src/utils/typechain.ts
@@ -10,8 +10,15 @@ export { SpokePoolVerifier__factory } from "@across-protocol/contracts/dist/type
 export { ERC20__factory } from "@across-protocol/contracts/dist/typechain/factories/@openzeppelin/contracts/token/ERC20/ERC20__factory";
 export { AcceleratingDistributor__factory } from "@across-protocol/across-token/dist/typechain/factories/AcceleratingDistributor__factory";
 export { ClaimAndStake__factory } from "@across-protocol/across-token/dist/typechain/factories/ClaimAndStake__factory";
-export { UniversalSwapAndBridge__factory } from "@across-protocol/contracts/dist/typechain/factories/contracts/SwapAndBridge.sol/UniversalSwapAndBridge__factory";
-export { SwapAndBridge__factory } from "@across-protocol/contracts/dist/typechain/factories/contracts/SwapAndBridge.sol/SwapAndBridge__factory";
+
+// NOTE: SwapAndBridge and UniverslSwapAndBridge were removed in version 4.1.3 of the contracts repo
+// We need version 4.1.1 until we migrate to new SpokePoolPeriphery contracts.
+export { UniversalSwapAndBridge__factory } from "@across-protocol/contracts-v4.1.1/dist/typechain/factories/contracts/SwapAndBridge.sol/UniversalSwapAndBridge__factory";
+export { SwapAndBridge__factory } from "@across-protocol/contracts-v4.1.1/dist/typechain/factories/contracts/SwapAndBridge.sol/SwapAndBridge__factory";
+export type {
+  UniversalSwapAndBridge,
+  SwapAndBridge,
+} from "@across-protocol/contracts-v4.1.1/dist/typechain/contracts/SwapAndBridge.sol";
 
 export type { AcrossMerkleDistributor } from "@across-protocol/contracts/dist/typechain/contracts/merkle-distributor/AcrossMerkleDistributor";
 export type { HubPool } from "@across-protocol/contracts/dist/typechain/contracts/HubPool";
@@ -20,10 +27,6 @@ export type {
   FilledV3RelayEvent,
 } from "@across-protocol/contracts/dist/typechain/contracts/SpokePool";
 export type { SpokePoolVerifier } from "@across-protocol/contracts/dist/typechain/contracts/SpokePoolVerifier";
-export type {
-  UniversalSwapAndBridge,
-  SwapAndBridge,
-} from "@across-protocol/contracts/dist/typechain/contracts/SwapAndBridge.sol";
 export type { AcceleratingDistributor } from "@across-protocol/across-token/dist/typechain/AcceleratingDistributor";
 export type { ClaimAndStake } from "@across-protocol/across-token/dist/typechain/ClaimAndStake";
 export type {
