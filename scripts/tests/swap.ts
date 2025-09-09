3213451f3b38b9aa47a66784665f8bc8f363c478
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-09-09 15:55:27 +0200

    feat: support svm in swap test script (#1814)

--- scripts/tests/swap.ts
+++ scripts/tests/swap.ts
@@ -1,4 +1,6 @@
 import { Wallet } from "ethers";
+import { utils } from "@across-protocol/sdk";
+import { createKeyPairSignerFromBytes } from "@solana/kit";
 
 import { getProvider } from "../../api/_utils";
 import {
@@ -6,6 +8,8 @@ import {
   signAndWaitAllowanceFlow,
   signAndWaitPermitFlow,
   argsFromCli,
+  getSvmSignerSeed,
+  signAndWaitAllowanceFlowSvm,
 } from "./_swap-utils";
 
 async function swap() {
@@ -19,10 +23,29 @@ async function swap() {
 
     console.log("\nSwap quote:", JSON.stringify(swapQuote, null, 2));
 
-    if (process.env.DEV_WALLET_PK && !argsFromCli.skipTxExecution) {
+    if (argsFromCli.skipTxExecution) {
+      console.log("Skipping tx execution");
+      continue;
+    }
+
+    if (
+      utils.chainIsSvm(swapQuote.swapTx.chainId) &&
+      process.env.DEV_WALLET_KEY_PAIR_SVM
+    ) {
+      const seed = getSvmSignerSeed();
+      const wallet = await createKeyPairSignerFromBytes(seed);
+      console.log("Executing tx on SVM using wallet:", wallet.address);
+
+      if (swapQuote.eip712) {
+        throw new Error("EIP712 not supported on SVM");
+      } else {
+        await signAndWaitAllowanceFlowSvm({ wallet, swapResponse: swapQuote });
+      }
+    } else if (process.env.DEV_WALLET_PK) {
       const wallet = new Wallet(process.env.DEV_WALLET_PK!).connect(
         getProvider(swapQuote.swapTx.chainId)
       );
+      console.log("Executing tx on EVM using wallet:", wallet.address);
 
       // if a permit-based flow is available, the unified endpoint will prefer that over an
       // allowance-based flow and return the relevant EIP712 data.
@@ -35,6 +58,10 @@ async function swap() {
         // sign and send approval txns + swap txn
         await signAndWaitAllowanceFlow({ wallet, swapResponse: swapQuote });
       }
+    } else {
+      console.log(
+        "Tx execution skipped due to missing env vars: 'DEV_WALLET_PK' or 'DEV_WALLET_KEY_PAIR_SVM'"
+      );
     }
   }
 }
