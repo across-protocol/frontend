8482001745bffa6011a64baedf11f39045ffadd1
Melisa Guevara <melisaguevarav@gmail.com>
2025-09-16 15:51:57 -0300

    feat: reduce RPC usage in API (#1831)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    Co-authored-by: Dong-Ha Kim <dongha.kim210@gmail.com>

--- api/_fill-deadline.ts
+++ api/_fill-deadline.ts
@@ -1,8 +1,4 @@
-import * as sdk from "@across-protocol/sdk";
-
 import { DEFAULT_FILL_DEADLINE_BUFFER_SECONDS } from "./_constants";
-import { getSpokePool } from "./_spoke-pool";
-import { getSVMRpc } from "./_providers";
 
 export function getFillDeadlineBuffer(chainId: number) {
   const bufferFromEnv = (
@@ -14,26 +10,13 @@ export function getFillDeadlineBuffer(chainId: number) {
   return Number(bufferFromEnv ?? DEFAULT_FILL_DEADLINE_BUFFER_SECONDS);
 }
 
-async function getCurrentTimeSvm(chainId: number): Promise<number> {
-  const rpc = getSVMRpc(chainId);
-  const timestamp = await rpc
-    .getSlot({
-      commitment: "confirmed",
-    })
-    .send()
-    .then((slot) => rpc.getBlockTime(slot).send());
-  return Number(timestamp);
-}
-
-export async function getFillDeadline(chainId: number): Promise<number> {
+export function getFillDeadline(
+  chainId: number,
+  quoteTimestamp: number
+): number {
   const fillDeadlineBuffer = getFillDeadlineBuffer(chainId);
-  let currentTime: number;
-
-  if (sdk.utils.chainIsSvm(chainId)) {
-    currentTime = await getCurrentTimeSvm(chainId);
-  } else {
-    const spokePool = getSpokePool(chainId);
-    currentTime = (await spokePool.callStatic.getCurrentTime()).toNumber();
-  }
-  return currentTime + fillDeadlineBuffer;
+  // Quote timestamp cannot be in the future or more than an hour old, so this is safe (i.e. cannot
+  // cause the contract to revert or an unfillable deposit) as long as the fill deadline buffer is
+  // greater than 1 hour (+ some cushion for fill time).
+  return Number(quoteTimestamp) + fillDeadlineBuffer;
 }
