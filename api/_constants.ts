700188f2b602ea95d6664bfb5437edecfee87ed9
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-09-16 14:57:44 +0200

    feat: hype price lookup via `l1Token` (#1838)

--- api/_constants.ts
+++ api/_constants.ts
@@ -203,6 +203,7 @@ export function populateDefaultRelayerFeeCapitalCostConfig(
 
 export const coinGeckoAssetPlatformLookup: Record<string, number> = {
   "0x4200000000000000000000000000000000000042": CHAIN_IDs.OPTIMISM,
+  "0x5555555555555555555555555555555555555555": CHAIN_IDs.HYPEREVM,
 };
 
 export const graphAPIKey = GRAPH_API_KEY;
@@ -286,6 +287,7 @@ export const SUPPORTED_CG_DERIVED_CURRENCIES = new Set([
   "gho",
   "bnb",
   "sol",
+  "hype",
 ]);
 export const CG_CONTRACTS_DEFERRED_TO_ID = new Set([
   TOKEN_SYMBOLS_MAP.AZERO.addresses[CHAIN_IDs.MAINNET],
@@ -298,6 +300,8 @@ export const CG_CONTRACTS_DEFERRED_TO_ID = new Set([
   TOKEN_SYMBOLS_MAP.VLR.addresses[CHAIN_IDs.MAINNET],
   TOKEN_SYMBOLS_MAP.SOL.addresses[CHAIN_IDs.SOLANA],
   TOKEN_SYMBOLS_MAP.SOL.addresses[CHAIN_IDs.SOLANA_DEVNET],
+  TOKEN_SYMBOLS_MAP.HYPE.addresses[CHAIN_IDs.HYPEREVM],
+  TOKEN_SYMBOLS_MAP.HYPE.addresses[CHAIN_IDs.HYPEREVM_TESTNET],
 ]);
 
 // 1:1 because we don't need to handle underlying tokens on FE
@@ -346,4 +350,5 @@ export const CUSTOM_GAS_TOKENS = {
   ...sdkConstants.CUSTOM_GAS_TOKENS,
   [CHAIN_IDs.LENS]: "GHO",
   [CHAIN_IDs.BSC]: "BNB",
+  [CHAIN_IDs.HYPEREVM]: "HYPE",
 };
