32401c39e4a073ee0bb436e2f6447d9546e9d9f4
Gerhard Steenkamp <gerhard@umaproject.org>
2025-09-27 09:58:49 +0200

    centralize navigation links
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -8,20 +8,24 @@ import {
 } from "react-router-dom";
 import { Header, Sidebar } from "components";
 import { useConnection, useError } from "hooks";
-import {
-  enableMigration,
-  stringValueInArray,
-  getConfig,
-  chainEndpointToId,
-} from "utils";
+import { stringValueInArray, getConfig, chainEndpointToId } from "utils";
 import lazyWithRetry from "utils/lazy-with-retry";
-
+import { enableMigration } from "utils";
 import Toast from "components/Toast";
 import BouncingDotsLoader from "components/BouncingDotsLoader";
 import NotFound from "./views/NotFound";
 import ScrollToTop from "components/ScrollToTop";
 import { AmpliTrace } from "components/AmpliTrace";
 import Banners from "components/Banners";
+
+export const NAVIGATION_LINKS = !enableMigration
+  ? [
+      { href: "/bridge-and-swap", name: "Bridge & Swap" },
+      { href: "/pool", name: "Pool" },
+      { href: "/rewards", name: "Rewards" },
+      { href: "/transactions", name: "Transactions" },
+    ]
+  : [];
 
 const LiquidityPool = lazyWithRetry(
   () => import(/* webpackChunkName: "LiquidityPools" */ "./views/LiquidityPool")
@@ -140,13 +144,13 @@ const Routes: React.FC = () => {
               }
             }}
           />
-          <Route exact path="/bridge" component={SwapAndBridge} />
+          <Route exact path="/bridge-and-swap" component={SwapAndBridge} />
           <Route path="/bridge/:depositTxHash" component={DepositStatus} />
           <Redirect
             exact
             path="/"
             to={{
-              pathname: "/bridge",
+              pathname: "/bridge-and-swap",
               search: location.search,
             }}
           />
