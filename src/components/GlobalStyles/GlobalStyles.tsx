7a9e97061a7e6a8e46fc29fede0509eadd04ce53
Gerhard Steenkamp <gerhard@umaproject.org>
2025-10-14 15:33:28 +0200

    add all font sizes
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- src/components/GlobalStyles/GlobalStyles.tsx
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -6,6 +6,36 @@ export const typography = css`
   @font-face {
     font-family: "Barlow";
     font-style: normal;
+    font-weight: 100;
+    font-display: fallback;
+    src: url("/fonts/Barlow-Thin.woff2") format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+  @font-face {
+    font-family: "Barlow";
+    font-style: normal;
+    font-weight: 200;
+    font-display: fallback;
+    src: url("/fonts/Barlow-ExtraLight.woff2") format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+  @font-face {
+    font-family: "Barlow";
+    font-style: normal;
+    font-weight: 300;
+    font-display: fallback;
+    src: url("/fonts/Barlow-Light.woff2") format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+  @font-face {
+    font-family: "Barlow";
+    font-style: normal;
     font-weight: 400;
     font-display: fallback;
     src: url("/fonts/Barlow-Regular.woff2") format("woff2");
@@ -26,9 +56,29 @@ export const typography = css`
   @font-face {
     font-family: "Barlow";
     font-style: normal;
+    font-weight: 600;
+    font-display: fallback;
+    src: url("/fonts/Barlow-SemiBold.woff2") format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+  @font-face {
+    font-family: "Barlow";
+    font-style: normal;
     font-weight: 700;
     font-display: fallback;
     src: url("/fonts/Barlow-Bold.woff2") format("woff2");
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
+      U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
+      U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+  @font-face {
+    font-family: "Barlow";
+    font-style: normal;
+    font-weight: 800;
+    font-display: fallback;
+    src: url("/fonts/Barlow-ExtraBold.woff2") format("woff2");
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6,
       U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193,
       U+2212, U+2215, U+FEFF, U+FFFD;
