316ec3e4bddcdbf4b892d2bd886c249faa1ad227
Melisa Guevara <melisaguevarav@gmail.com>
2025-12-02 15:55:39 -0300

    chore(cctp): update finalizer and remove Ink from supported chains

--- api/_bridges/cctp/strategy.ts
+++ api/_bridges/cctp/strategy.ts
@@ -33,6 +33,7 @@ import {
   CCTP_SUPPORTED_CHAINS,
   CCTP_SUPPORTED_TOKENS,
   CCTP_FINALITY_THRESHOLDS,
+  DEFAULT_CCTP_ACROSS_FINALIZER_ADDRESS,
   getCctpTokenMessengerAddress,
   getCctpMessageTransmitterAddress,
   getCctpDomainId,
@@ -304,7 +305,7 @@ export function getCctpBridgeStrategy(
         amount: bridgeQuote.inputAmount,
         destinationDomain,
         mintRecipient: crossSwap.recipient,
-        destinationCaller: ethers.constants.AddressZero, // Anyone can finalize the message on domain when this is set to bytes32(0)
+        destinationCaller: DEFAULT_CCTP_ACROSS_FINALIZER_ADDRESS,
         maxFee,
         minFinalityThreshold,
       };
