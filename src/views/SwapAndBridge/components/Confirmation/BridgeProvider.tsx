ca0e00fdb7ac509aa0e1a829eabe9ccac10d02c2
Jørgen <jorgen.hovind@gmail.com>
2025-12-02 10:32:38 +0100

    feat: display bridge route (#2011)
    We support multiple bridge routes but currently we always show `across` as the bridge provider in the ui.
    
    Add logos and titles for all possible routes
    
    <img width="2078" height="2136" alt="Screenshot 2025-12-02 at 08 14 42 UTC@2x" src="https://github.com/user-attachments/assets/de364f57-fbe4-4606-9141-314e47b2305d" />
    
    <img width="1194" height="2060" alt="Screenshot 2025-12-02 at 08 14 58 UTC@2x" src="https://github.com/user-attachments/assets/d7ea20e4-a4c6-43cd-9aaf-6aec457dfab7" />

---
+++ src/views/SwapAndBridge/components/Confirmation/BridgeProvider.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { BridgeProvider, getProviderDisplay } from "./provider";
+import { Tooltip } from "../../../../components/Tooltip";
+
+export const ProviderBadge = ({
+  provider,
+  expanded,
+}: {
+  provider: BridgeProvider;
+  expanded: boolean;
+}) => {
+  const { label, logo } = getProviderDisplay(provider);
+  return expanded ? (
+    <ProviderBadgeWrapper provider={provider}>
+      {label}
+      {logo}
+    </ProviderBadgeWrapper>
+  ) : (
+    <Tooltip
+      tooltipAnchorHeight="inherit"
+      paddingPx={8}
+      tooltipId={`tooltip-${provider}`}
+      body={
+        <ToolipContent>
+          {logo}
+          {label}
+        </ToolipContent>
+      }
+    >
+      <LogoWrapper>{logo}</LogoWrapper>
+    </Tooltip>
+  );
+};
+
+const LogoWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+`;
+const ToolipContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 6px;
+  align-items: center;
+  justify-content: space-between;
+`;
+const ProviderBadgeWrapper = styled.span<{ provider: BridgeProvider }>`
+  display: inline-flex;
+  align-items: center;
+  font-size: 12px;
+  gap: 6px;
+`;
