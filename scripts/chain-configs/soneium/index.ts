c411fe70a893e95a7702818b661811c8ecc9fe85
bmzig <57361391+bmzig@users.noreply.github.com>
2025-12-04 17:17:27 -0600

    Revert "chore: Disable Soneium routes temporarily (#2050)" (#2051)

--- scripts/chain-configs/soneium/index.ts
+++ scripts/chain-configs/soneium/index.ts
@@ -18,6 +18,6 @@ export default {
   chainId,
   publicRpcUrl: "https://rpc.soneium.org",
   blockTimeSeconds: 2,
-  tokens: [],
+  tokens: ["WETH", "ETH", "USDC.e"],
   enableCCTP: false,
 } as ChainConfig;
