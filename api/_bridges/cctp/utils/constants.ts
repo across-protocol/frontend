ae720d3b9bdd6fa3a7f9d5e1c62945932df43363
ashwinrava <213675439+ashwinrava@users.noreply.github.com>
2025-12-04 12:30:56 -0500

    Add SVM destination support for CCTP strategy

--- api/_bridges/cctp/utils/constants.ts
+++ api/_bridges/cctp/utils/constants.ts
@@ -39,6 +39,17 @@ export const CCTP_FINALITY_THRESHOLDS = {
 export const DEFAULT_CCTP_ACROSS_FINALIZER_ADDRESS =
   "0x72adB07A487f38321b6665c02D289C413610B081";
 
+const CCTP_FINALIZER_ADDRESS_OVERRIDES: Record<number, string> = {
+  [CHAIN_IDs.SOLANA]: "5v4SXbcAKKo3YbPBXU9K7zNBMgJ2RQFsvQmg2RAFZT6t",
+};
+
+export const getCctpFinalizerAddress = (chainId: number): string => {
+  return (
+    CCTP_FINALIZER_ADDRESS_OVERRIDES[chainId] ||
+    DEFAULT_CCTP_ACROSS_FINALIZER_ADDRESS
+  );
+};
+
 // CCTP TokenMessenger contract addresses
 // Source: https://developers.circle.com/cctp/evm-smart-contracts
 const DEFAULT_CCTP_TOKEN_MESSENGER_ADDRESS =
