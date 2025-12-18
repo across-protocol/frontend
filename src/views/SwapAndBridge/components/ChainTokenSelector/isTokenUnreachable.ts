44266d265d5b933de810d5b9269a8b02bdc100f9
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-12-18 21:35:14 +0700

    chore: limit usdc usdh hyperevm to swap api (#2131)
    Co-authored-by: ashwinrava <213675439+ashwinrava@users.noreply.github.com>

--- src/views/SwapAndBridge/components/ChainTokenSelector/isTokenUnreachable.ts
+++ src/views/SwapAndBridge/components/ChainTokenSelector/isTokenUnreachable.ts
@@ -38,7 +38,13 @@ const RESTRICTED_ROUTES: RestrictedRoute[] = [
     toChainId: [CHAIN_IDs.HYPERCORE],
     toSymbol: ["USDC-SPOT"],
   },
-
+  // Only USDC can be bridged to USDH on HyperEVM
+  {
+    fromChainId: "*",
+    fromSymbol: ["!USDC"],
+    toChainId: [CHAIN_IDs.HYPEREVM],
+    toSymbol: ["USDH"],
+  },
   // only allow bridegable output to SOlana
   {
     fromChainId: "*",
