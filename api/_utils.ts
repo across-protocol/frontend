26f07f8b53b4b6d5560b9d77bef44d942e9c5ae8
Paul <108695806+pxrl@users.noreply.github.com>
2026-01-06 16:40:49 +0000

    chore: Migrate PoolClient from SDK
    The PoolClient lives in the SDK but is only used by the frontend
    repository. Migrate it here to unburden the SDK.

--- api/_utils.ts
+++ api/_utils.ts
@@ -95,6 +95,7 @@ import { isMessageTooLong } from "./_message";
 import { getSvmTokenInfo } from "./_svm-tokens";
 import { Span } from "@opentelemetry/api";
 import { getNormalizedSpotTokenSymbol } from "./_hypercore";
+import * as pool from "./_pool";
 
 export const { Profiler, toAddressType } = sdk.utils;
 export {
@@ -592,7 +593,7 @@ export const makeHubPoolClientConfig = (chainId = 1) => {
  */
 export const getHubPoolClient = () => {
   const hubPoolConfig = makeHubPoolClientConfig(HUB_POOL_CHAIN_ID);
-  return new sdk.pool.Client(
+  return new pool.Client(
     hubPoolConfig,
     {
       provider: getProvider(HUB_POOL_CHAIN_ID),
