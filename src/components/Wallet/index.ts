008c885032e3d5cfe097bb77d9ce52c3d7e0c81c
Jørgen <jorgen.hovind@gmail.com>
2026-01-13 12:31:12 +0100

    feat: add hl hyperliquid names resolution for connect wallet (#2168)
    Co-authored-by: jorgen <jorgen@umaproject.org>

--- src/components/Wallet/index.ts
+++ src/components/Wallet/index.ts
@@ -1,2 +1,9 @@
 import Wallet from "./Wallet";
+export { ConnectedAccount } from "./Wallet";
+export {
+  ConnectButton,
+  BalanceButton,
+  WalletWrapper,
+  Separator,
+} from "./Wallet.styles";
 export default Wallet;
