1a7e48a66f17f50805955f15aba3e571a0f992c4
Paul <108695806+pxrl@users.noreply.github.com>
2026-01-20 11:23:11 +0000

    Drop weird comment change

--- api/_pool/uma/utils.ts
+++ api/_pool/uma/utils.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 import { BigNumber, BigNumberish } from "ethers";
 
-// Re-export types for use throughout api/_pool
+// Re-export types and utilities for use throughout api/_pool
 export type { BigNumber, BigNumberish } from "ethers";
 
 // check if a value is not null or undefined, useful for numbers which could be 0.
