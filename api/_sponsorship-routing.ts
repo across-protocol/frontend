e798c66654cf2da297a5e3f6d37fd82d71fd7289
Dong-Ha Kim <dongha.kim210@gmail.com>
2026-01-23 17:38:39 +0700

    refactor: sponsorship routing rules

--- api/_sponsorship-routing.ts
+++ api/_sponsorship-routing.ts
@@ -15,11 +15,6 @@ import {
 } from "./_bridges/types";
 import { TOKEN_EQUIVALENCE_REMAPPING } from "./_constants";
 import {
-  CHAINS,
-  CCTP_NO_DOMAIN,
-  TOKEN_EQUIVALENCE_REMAPPING,
-} from "./_constants";
-import {
   SponsorshipEligibilityPreChecks,
   getSponsorshipEligibilityPreChecks,
 } from "./_sponsorship-eligibility";
